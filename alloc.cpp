c4c93224b3cb6663cd1e5fdb6768c2a24da3a92d
Alexander Stigsen <astigsen@tightdb.com>
2011-04-16 12:02:06 +0200

    Slab Allocator

--- alloc.cpp
+++ alloc.cpp
@@ -1,67 +1,103 @@
 #include "alloc.h"
+#include <assert.h>
 
-//TODO: what about returning pos zero vs error reporting?
-void* SlabAlloc::Alloc(size_t size) {
+SlabAlloc::SlabAlloc() : m_shared(NULL), m_baseline(0) {}
+
+SlabAlloc::~SlabAlloc() {
+	// Release all allocated memory
+	for (size_t i = 0; i < m_slabs.GetSize(); ++i) {
+		void* p = (void*)(int)m_slabs[i].pointer;
+		free(p);
+	}
+}
+
+MemRef SlabAlloc::Alloc(size_t size) {
 	// Do we have a free space we can reuse?
-	if (!m_freeSpace.IsEmpty()) {
-		FreeSpace::Cursor r = m_freeSpace.Back();
+	for (size_t i = 0; i < m_freeSpace.GetSize(); ++i) {
+		FreeSpace::Cursor r = m_freeSpace[i];
 		if (r.size >= (int)size) {
-			void* location = (void*)(int)r.pos;
+			const size_t location = r.ref;
 			const size_t rest = r.size - size;
 
 			// Update free list
-			// TODO: sorted list
-			if (rest == 0) m_freeSpace.PopBack();
+			if (rest == 0) m_freeSpace.DeleteRow(i);
 			else {
-				r.size = (int)rest;
-				r.pos += (int)size;
+				r.size = rest;
+				r.ref += size;
 			}
 
-			return location;
+			return MemRef(Translate(location), location);
 		}
 	}
 
 	// Else, allocate new slab
 	const size_t multible = 256 * ((size % 256) + 1);
-	const size_t slabsBack = m_slabs.IsEmpty() ? 0 : m_slabs.Back().end;
+	const size_t slabsBack = m_slabs.IsEmpty() ? 0 : m_slabs.Back().offset;
 	const size_t doubleLast = m_slabs.IsEmpty() ? 0 :
-		                                          (slabsBack - (m_slabs.GetSize() == 1) ? 0 : m_slabs[-2].end) * 2;
+		                                          (slabsBack - (m_slabs.GetSize() == 1) ? 0 : m_slabs[-2].offset) * 2;
 	const size_t newsize = multible > doubleLast ? multible : doubleLast;
 
-	// Alloc and add to slab table
+	// Allocate memory 
 	void* slab = malloc(newsize);
-	if (!slab) return NULL;
+	if (!slab) return MemRef(NULL, 0);
+
+	// Add to slab table
 	Slabs::Cursor s = m_slabs.Add();
-	s.end = (int)(slabsBack + newsize) + (slabsBack ? 0 : 1); // slabs count from 1
+	s.offset = slabsBack + newsize;
 	s.pointer = (int)slab;
 
 	// Update free list
 	const size_t rest = newsize - size;
 	FreeSpace::Cursor f = m_freeSpace.Add();
-	f.size = (int)rest;
-	f.pos = (int)(slabsBack + newsize);
+	f.ref = slabsBack + newsize;
+	f.size = rest;
 
-	return (void*)slabsBack;
+	return MemRef((void*)slab, slabsBack);
 }
 
-void SlabAlloc::Free(void* p) {
+// Support function
+size_t GetSizeFromHeader(void* p) {
+	// parse the capacity part of 8byte header
+	const uint8_t* const header = (uint8_t*)p;
+	return (header[4] << 16) + (header[5] << 8) + header[6];
+}
+
+void SlabAlloc::Free(size_t ref, void* p) {
 	// Get size from segment
+	const size_t size = GetSizeFromHeader(p);
 
 	// Add to freelist
+	m_freeSpace.Add(ref, size);
 
 	// Consolidate freelist
 }
 
-void* SlabAlloc::ReAlloc(void* p, size_t size) {
+MemRef SlabAlloc::ReAlloc(size_t ref, void* p, size_t size) {
+	//TODO: Check if we can extend current space
+
 	// Allocate new space
-	void* space = Alloc(size);
-	if (!space) return NULL;
+	const MemRef space = Alloc(size);
+	if (!space.pointer) return space;
+
+	// Get size of old segment
+	const size_t oldsize = GetSizeFromHeader(p);
 
 	// Copy existing segment
-	//memcpy(space, p, oldsize);
+	memcpy(space.pointer, p, oldsize);
 
 	// Add old segment to freelist
-	Free(p);
+	Free(ref, p);
 
 	return space;
+}
+
+void* SlabAlloc::Translate(size_t ref) const {
+	if (ref < m_baseline) return m_shared + ref;
+	else {
+		const size_t ndx = m_slabs.offset.Find(ref); //FindPos
+		assert(ndx != -1);
+
+		const size_t offset = ndx ? m_slabs[ndx-1].offset : 0;
+		return (int64_t*)(int)m_slabs[ndx].pointer + (ref - offset);
+	}
 }
--- alloc.h
+++ alloc.h
@@ -3,31 +3,44 @@
 
 #include <stdlib.h>
 #include "tightdb.h"
+#include "stdint.h"
+
+struct MemRef {
+	void* pointer;
+	size_t ref;
+	MemRef(void* p, size_t r) : pointer(p), ref(r) {}
+};
 
 class Allocator {
 public:
 	void* Alloc(size_t size) {return malloc(size);}
 	void* ReAlloc(void* p, size_t size) {return realloc(p, size);}
 	void Free(void* p) {return free(p);}
-	void* Translate(void* p) {return p;}
+	void* Translate(size_t ref) const {return (void*)ref;}
 };
 
 class SlabAlloc : public Allocator {
 public:
-	void* Alloc(size_t size);
-	void* ReAlloc(void* p, size_t size);
-	void Free(void* p);
+	SlabAlloc();
+	~SlabAlloc();
+
+	MemRef Alloc(size_t size);
+	MemRef ReAlloc(size_t ref, void* p, size_t size);
+	void Free(size_t ref, void* p);
+	void* Translate(size_t ref) const;
 
 private:
 	// Define internal tables
 	TDB_TABLE_2(Slabs,
-				Int, end,
+				Int, offset,
 				Int, pointer)
 	TDB_TABLE_2(FreeSpace,
-				Int, size,
-				Int, pos)
+				Int, ref,
+				Int, size)
 
 	// Member variables
+	int64_t* m_shared;
+	size_t m_baseline;
 	Slabs m_slabs;
 	FreeSpace m_freeSpace;
 };
