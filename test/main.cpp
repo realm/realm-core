df2795510bf9591d5e42950c2a83ac65ef87c8d8
Karl Jacobi Ekström <karl@tightdb.com>
2011-06-23 17:39:10 -0700

    Another solution for the VC getchar...

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,8 @@
 int main() {
 	const int res = UnitTest::RunAllTests();
 
+#ifdef _MSC_VER
 	getchar(); // wait for key
+#endif
 	return res;
 }
--- test/tightdb-tests.cpp
+++
@@ -1,8 +0,0 @@
-#include "UnitTest++.h"
-
-int main() {
-	const int res = UnitTest::RunAllTests();
-
-	//getchar(); // wait for key
-	return res;
-}
