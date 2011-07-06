190fc0e52547333d84cc1fe72c71f491d2d7326a
Alexander Stigsen <astigsen@tightdb.com>
2011-07-06 14:36:20 -0700

    Added missing file

---
+++ src/AllocSlab.h
@@ -0,0 +1,38 @@
+#ifndef __TDB_ALLOC_SLAB__
+#define __TDB_ALLOC_SLAB__
+
+#include "tightdb.h"
+
+#ifdef _MSC_VER
+#include "win32/stdint.h"
+#else
+#include <cstdint> // unint8_t etc
+#endif
+
+class SlabAlloc : public Allocator {
+public:
+	SlabAlloc();
+	~SlabAlloc();
+
+	MemRef Alloc(size_t size);
+	MemRef ReAlloc(size_t ref, void* p, size_t size, bool doCopy);
+	void Free(size_t ref, void* p);
+	void* Translate(size_t ref) const;
+
+private:
+	// Define internal tables
+	TDB_TABLE_2(Slabs,
+				Int, offset,
+				Int, pointer)
+	TDB_TABLE_2(FreeSpace,
+				Int, ref,
+				Int, size)
+
+	// Member variables
+	int64_t* m_shared;
+	size_t m_baseline;
+	Slabs m_slabs;
+	FreeSpace m_freeSpace;
+};
+
+#endif //__TDB_ALLOC_SLAB__
