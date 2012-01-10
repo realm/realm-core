f788dd83861f5f2192d1ebb912f965691eb0d7c2
Alexander Stigsen <astigsen@tightdb.com>
2012-01-10 12:42:03 +0100

    Minor style fixes

--- src/query/QueryEngine.h
+++ src/query/QueryEngine.h
@@ -3,57 +3,56 @@
 
 
 struct EQUAL { 
-	bool operator()(const char *v1, const char *v2) { return strcmp(v1, v2) == 0; }
-	template<class T> bool operator()(T& v1, T& v2) {return v1 == v2;}  
+	bool operator()(const char *v1, const char *v2) const { return strcmp(v1, v2) == 0; }
+	template<class T> bool operator()(const T& v1, const T& v2) const {return v1 == v2;}
 };
 
 struct NOTEQUAL { 
-	bool operator()(const char *v1, const char *v2) { return strcmp(v1, v2) != 0; }
-	template<class T> bool operator()(T& v1, T& v2) { return v1 != v2; }
+	bool operator()(const char *v1, const char *v2) const { return strcmp(v1, v2) != 0; }
+	template<class T> bool operator()(const T& v1, const T& v2) const { return v1 != v2; }
 };
 
 struct GREATER { 
-	template<class T> bool operator()(const T& v1, const T& v2) {return v1 > v2;} 
+	template<class T> bool operator()(const T& v1, const T& v2) const {return v1 > v2;}
 };
 
 struct LESS { 
-	template<class T> bool operator()(const T& v1, const T& v2) {return v1 < v2;} 
+	template<class T> bool operator()(const T& v1, const T& v2) const {return v1 < v2;}
 };
 
 struct LESSEQUAL { 
-	template<class T> bool operator()(const T& v1, const T& v2) {return v1 <= v2;} 
+	template<class T> bool operator()(const T& v1, const T& v2) const {return v1 <= v2;}
 };
 
 struct GREATEREQUAL { 
-	template<class T> bool operator()(const T& v1, const T& v2) {return v1 >= v2;} 
+	template<class T> bool operator()(const T& v1, const T& v2) const {return v1 >= v2;}
 };
 
 
 class ParentNode { 
 public:
+	ParentNode(ParentNode* p) : m_child(p) {}
 	virtual size_t Find(size_t start, size_t end, const Table& table) = 0;
-	ParentNode *m_child;
+protected:
+	ParentNode* m_child;
 };
 
 
 template <class T, class C, class F> class NODE : public ParentNode {
 public:
-	ParentNode *m_child;
-	T m_value;
-	size_t m_column;
+	NODE<T, C, F>(ParentNode *p, T v, size_t column) : ParentNode(p), m_value(v), m_column(column) {}
 
-	NODE<T, C, F>(ParentNode *p, T v, size_t column) : m_child(p), m_value(v), m_column(column) {}
 	size_t Find(size_t start, size_t end, const Table& table) {
 		const C& column = (C&)(table.GetColumnBase(m_column));
-		F function;
-		for(size_t s = start; s < end; s++) {
-			T t = column.Get(s);
-			if(function(t, m_value)) {
-				if(m_child == 0)
+		const F function = {};
+		for (size_t s = start; s < end; ++s) {
+			const T t = column.Get(s);
+			if (function(t, m_value)) {
+				if (m_child == 0)
 					return s;
 				else {
-					size_t a = m_child->Find(s, end, table);
-					if(s == a)
+					const size_t a = m_child->Find(s, end, table);
+					if (s == a)
 						return s;
 					else
 						s = a - 1;
@@ -63,29 +62,28 @@ public:
 		return end;
 	}
 
+protected:
+	T m_value;
+	size_t m_column;
 };
 
 
 class OR_NODE : public ParentNode {
 public:
-	ParentNode *m_cond1;
-	ParentNode *m_cond2;
-	ParentNode *m_child;
+	OR_NODE(ParentNode* p1, ParentNode* p2, ParentNode* c) : ParentNode(c), m_cond1(p1), m_cond2(p2) {};
 
 	size_t Find(size_t start, size_t end, const Table& table) {
-		size_t f1, f2;
-
-		for(size_t s = start; s < end; s++) {
+		for (size_t s = start; s < end; ++s) {
 			// Todo, redundant searches can occur
-			f1 = m_cond2->Find(s, end, table);
-			f2 = m_cond1->Find(s, f1, table);
+			const size_t f1 = m_cond2->Find(s, end, table);
+			const size_t f2 = m_cond1->Find(s, f1, table);
 			s = f1 < f2 ? f1 : f2;
 
-			if(m_child == 0)
+			if (m_child == 0)
 				return s;
 			else {
-				size_t a = m_child->Find(s, end, table);
-				if(s == a)
+				const size_t a = m_child->Find(s, end, table);
+				if (s == a)
 					return s;
 				else
 					s = a - 1;
@@ -94,5 +92,7 @@ public:
 		return end;
 	}
 
-	OR_NODE(ParentNode *p1, ParentNode *p2, ParentNode *c) : m_cond1(p1), m_cond2(p2), m_child(c) {};
+protected:
+	ParentNode* m_cond1;
+	ParentNode* m_cond2;
 };
