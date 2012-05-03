da12464b74c959166c1dd519a393318ce6f23a74
Brian Munkholm <bm@tightdb.com>
2012-05-03 18:13:53 +0200

    Updated .py files with header

--- src/tightdb-c-gen.py
+++ src/tightdb-c-gen.py
@@ -5,6 +5,25 @@ templateDef = """#slurp
 commentStartToken = %%
 directiveStartToken = %
 #end compiler-settings
+/*************************************************************************
+ * 
+ * TIGHTDB CONFIDENTIAL
+ * __________________
+ * 
+ *  [2011] - [2012] TightDB Inc
+ *  All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of TightDB Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to TightDB Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from TightDB Incorporated.
+ *
+ **************************************************************************/
 #ifndef __C_TIGHTDB_H__
 #define __C_TIGHTDB_H__
 
