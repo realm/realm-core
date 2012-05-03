1834d44e17ce7fcbed7b10e18e2622b2399aab5a
Brian Munkholm <bm@tightdb.com>
2012-05-03 18:08:25 +0200

    Added copyright header to .hpp files

--- src/query_engine.hpp
+++ src/query_engine.hpp
@@ -1,3 +1,25 @@
+/*************************************************************************
+ * 
+ * TIGHTDB CONFIDENTIAL
+ * __________________
+ * 
+ *  [2011] - [2012] TightDB Inc
+ *  All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of TightDB Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to TightDB Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from TightDB Incorporated.
+ *
+ **************************************************************************/
+#ifndef __TIGHTDB_ENGINE_HPP
+#define __TIGHTDB_ENGINE_HPP
+
 #include <string>
 #include "table.hpp"
 #include "column_string.hpp"
@@ -353,3 +375,5 @@ public:
 };
 
 }
+
+#endif __TIGHTDB_ENGINE_HPP
--- src/utilities.hpp
+++ src/utilities.hpp
@@ -1,5 +1,24 @@
-#ifndef UTILITIES_HEADER
-#define UTILITIES_HEADER
+/*************************************************************************
+ * 
+ * TIGHTDB CONFIDENTIAL
+ * __________________
+ * 
+ *  [2011] - [2012] TightDB Inc
+ *  All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of TightDB Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to TightDB Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from TightDB Incorporated.
+ *
+ **************************************************************************/
+#ifndef __TIGHTDB_UTILITIES_HPP
+#define __TIGHTDB_UTILITIES_HPP
 
 #include <cstdlib>
 #ifdef _MSC_VER
@@ -48,4 +67,5 @@ void checksum_init(checksum_t* t);
 
 }
 
-#endif
+#endif // __TIGHTDB_UTILITIES_HPP
+
