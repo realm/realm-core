01df912d0e9ab25eb24cdc8e889e30c9b0bcd18e
Alexander Stigsen <astigsen@tightdb.com>
2012-05-07 17:46:51 +0200

    Minor style fixes in query.hpp

--- src/query.hpp
+++ src/query.hpp
@@ -71,23 +71,23 @@ public:
     void Or();
 
     // Searching
-    size_t find_next(Table& table, size_t lastmatch=-1);
+    size_t    find_next(Table& table, size_t lastmatch=-1);
     TableView find_all(Table& table, size_t start=0, size_t end=size_t(-1), size_t limit=size_t(-1));
-    void find_all(Table& table, TableView& tv, size_t start=0, size_t end=size_t(-1), size_t limit=size_t(-1));
+    void      find_all(Table& table, TableView& tv, size_t start=0, size_t end=size_t(-1), size_t limit=size_t(-1));
     
     // Aggregates
-    int64_t sum(const Table& table, size_t column, size_t* resultcount = NULL, size_t start = 0, size_t end = size_t(-1), size_t limit = size_t(-1)) const;
-    int64_t maximum(const Table& table, size_t column, size_t* resultcount=NULL, size_t start = 0, size_t end = size_t(-1), size_t limit = size_t(-1)) const;
-    int64_t minimum(const Table& table, size_t column, size_t* resultcount=NULL, size_t start = 0, size_t end = size_t(-1), size_t limit = size_t(-1)) const;
-    double average(const Table& table, size_t column_ndx, size_t* resultcount = NULL, size_t start = 0, size_t end = size_t(-1), size_t limit = size_t(-1)) const;
-    size_t count(const Table& table, size_t start = 0, size_t end = size_t(-1), size_t limit = size_t(-1)) const;
+    int64_t sum(const Table& table, size_t column, size_t* resultcount=NULL, size_t start=0, size_t end = size_t(-1), size_t limit=size_t(-1)) const;
+    int64_t maximum(const Table& table, size_t column, size_t* resultcount=NULL, size_t start=0, size_t end = size_t(-1), size_t limit=size_t(-1)) const;
+    int64_t minimum(const Table& table, size_t column, size_t* resultcount=NULL, size_t start=0, size_t end = size_t(-1), size_t limit=size_t(-1)) const;
+    double  average(const Table& table, size_t column_ndx, size_t* resultcount=NULL, size_t start=0, size_t end=size_t(-1), size_t limit=size_t(-1)) const;
+    size_t  count(const Table& table, size_t start=0, size_t end=size_t(-1), size_t limit=size_t(-1)) const;
     
     // Deletion
-    size_t remove(Table& table, size_t start = 0, size_t end = size_t(-1), size_t limit = size_t(-1)) const;
+    size_t  remove(Table& table, size_t start=0, size_t end=size_t(-1), size_t limit=size_t(-1)) const;
     
     // Multi-threading
-    void FindAllMulti(Table& table, TableView& tv, size_t start = 0, size_t end = size_t(-1));
-    int SetThreads(unsigned int threadcount);
+    void FindAllMulti(Table& table, TableView& tv, size_t start=0, size_t end=size_t(-1));
+    int  SetThreads(unsigned int threadcount);
 
     std::string Verify();
     
@@ -97,24 +97,25 @@ protected:
     friend class XQueryAccessorInt;
     friend class XQueryAccessorString;
 
-    void Init(const Table& table) const;
-    size_t FindInternal(const Table& table, size_t start = 0, size_t end = size_t(-1)) const;
-    void UpdatePointers(ParentNode* p, ParentNode** newnode);
-    static bool comp(const std::pair<size_t, size_t>& a, const std::pair<size_t, size_t>& b);
+    void   Init(const Table& table) const;
+    size_t FindInternal(const Table& table, size_t start=0, size_t end=size_t(-1)) const;
+    void   UpdatePointers(ParentNode* p, ParentNode** newnode);
+    
+    static bool  comp(const std::pair<size_t, size_t>& a, const std::pair<size_t, size_t>& b);
     static void* query_thread(void* arg);
 
     struct thread_state {
         pthread_mutex_t result_mutex;
-        pthread_cond_t completed_cond;
+        pthread_cond_t  completed_cond;
         pthread_mutex_t completed_mutex;
         pthread_mutex_t jobs_mutex;
-        pthread_cond_t jobs_cond;
+        pthread_cond_t  jobs_cond;
         size_t next_job;
         size_t end_job;
         size_t done_job;
         size_t count;
-        ParentNode *node;
-        Table *table;
+        ParentNode* node;
+        Table* table;
         std::vector<size_t> results;
         std::vector<std::pair<size_t, size_t> > chunks;
     } ts;
