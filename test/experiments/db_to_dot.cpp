1e373534b684cefebf3814079a22b8c987a62493
Kristian Spangsege <kristian.spangsege@gmail.com>
2012-07-19 19:53:07 +0200

    Handy tool to produce a DOT file from a TightDB database file

---
+++ test/experiments/db_to_dot.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <tightdb/group.hpp>
+
+using namespace std;
+using namespace tightdb;
+
+int main(int argc, const char* const argv[])
+{
+    if (argc != 2) {
+        cerr << "Wrong number of command line arguments\n"
+            "Synopsis: " << argv[0] << "  DATABASE-FILE" << endl;
+        return 1;
+    }
+    Group g(argv[1], GROUP_READONLY);
+    if (!g.is_valid()) {
+        cerr << "Failed to open TightDB database '" << argv[1] << "'" << endl;
+        return 1;
+    }
+    g.to_dot(cout);
+    return 0;
+}
