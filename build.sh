678c90c46f8e743aeadf6e5f524853cf6869e714
Kristian Spangsege <kristian.spangsege@gmail.com>
2012-09-29 23:54:21 +0200

    build.sh is not longer so easily fooled when it tries to run 'git descibe' in each extension

--- build.sh
+++ build.sh
@@ -161,22 +161,23 @@ case "$MODE" in
         BRANCH="$(git rev-parse --abbrev-ref HEAD)" || exit 1
         VERSION="$(git describe)" || exit 1
 
-        {
-            if [ -z "$AVAIL_EXTENSIONS" ]; then
-                echo "Continuing with no extensions"
-            else
-                echo "Continuing with these parts:"
-                {
-                    echo "  core  ->  .  $BRANCH  $VERSION"
-                    for x in $AVAIL_EXTENSIONS; do
-                        EXT_HOME="../$(map_ext_name_to_dir "$x")" || exit 1
-                        EXT_BRANCH="$(cd "$EXT_HOME" && git rev-parse --abbrev-ref HEAD)" || exit 1
-                        EXT_VERSION="$(cd "$EXT_HOME" && git describe)" || exit 1
-                        echo "  $x  ->  $EXT_HOME  $EXT_BRANCH  $EXT_VERSION"
-                    done
-                } | column -t || exit 1
-            fi
-        } | tee -a "$LOG_FILE" || exit 1
+        if [ -z "$AVAIL_EXTENSIONS" ]; then
+            echo "Continuing with no extensions" | tee -a "$LOG_FILE"
+        else
+            echo "Continuing with these parts:"
+            {
+                echo "core  ->  .  $BRANCH  $VERSION"
+                for x in $AVAIL_EXTENSIONS; do
+                    EXT_HOME="../$(map_ext_name_to_dir "$x")" || exit 1
+                    EXT_BRANCH="$(cd "$EXT_HOME" && git rev-parse --abbrev-ref HEAD)" || exit 1
+                    EXT_VERSION="$(cd "$EXT_HOME" && git describe --always)" || exit 1
+                    echo "$x  ->  $EXT_HOME  $EXT_BRANCH  $EXT_VERSION"
+                done
+            } >"$TEMP_DIR/continuing_with" || exit 1
+            column -t "$TEMP_DIR/continuing_with" >"$TEMP_DIR/continuing_with2" || exit 1
+            sed 's/^/  /' "$TEMP_DIR/continuing_with2" >"$TEMP_DIR/continuing_with3" || exit 1
+            tee -a "$LOG_FILE" <"$TEMP_DIR/continuing_with3"
+        fi
 
 
         # Setup package directory
