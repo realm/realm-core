1b5087cdd11bd5b8d18a5bc58033888cebc92884
Kristian Spangsege <kristian.spangsege@gmail.com>
2012-10-25 00:35:52 +0200

    Minor update of generic.mk: Improvements in regard to CFLAGS specification

--- config.mk
+++ config.mk
@@ -5,24 +5,26 @@ INSTALL_DEBUG_PROGS = 1
 ifneq ($(TIGHTDB_ENABLE_FAT_BINARIES),)
 ifneq ($(call CC_CXX_AND_LD_ARE,clang),)
 ifeq ($(shell uname),Darwin)
-CFLAGS_DEFAULT  += -arch i386 -arch x86_64
-LDFLAGS_DEFAULT += -arch i386 -arch x86_64
+CFLAGS_ARCH += -arch i386 -arch x86_64
 endif
 endif
 endif
 
-CFLAGS_DEFAULT += -Wextra -ansi -pedantic -Wno-long-long
 # FIXME: '-fno-elide-constructors' currently causes TightDB to fail
 #CFLAGS_DEBUG   += -fno-elide-constructors
 CFLAGS_PTHREAD += -pthread
+CFLAGS_GENERAL += -Wextra -ansi -pedantic -Wno-long-long
+
 
 ifeq ($(TIGHTDB_DISABLE_SSE),)
-CFLAGS_DEFAULT += -msse4.2 -DUSE_SSE42
+PROJECT_CFLAGS = -msse4.2 -DUSE_SSE42
 endif
 
 ifneq ($(TIGHTDB_ENABLE_REPLICATION),)
-CFLAGS_DEFAULT += -DTIGHTDB_ENABLE_REPLICATION
+PROJECT_CFLAGS += -DTIGHTDB_ENABLE_REPLICATION
 endif
 
-CFLAGS_DEBUG += -DTIGHTDB_DEBUG -DMAX_LIST_SIZE=4
-CFLAGS_COVER += -DTIGHTDB_DEBUG -DMAX_LIST_SIZE=4
+PROJECT_CFLAGS_DEBUG = -DTIGHTDB_DEBUG -DMAX_LIST_SIZE=4
+PROJECT_CFLAGS_COVER = -DTIGHTDB_DEBUG -DMAX_LIST_SIZE=4
+
+
