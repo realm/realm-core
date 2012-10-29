42c7ba1853b5fe3d57cdff9268aad5c2dde28329
Kristian Spangsege <kristian.spangsege@gmail.com>
2012-10-29 18:50:35 +0100

    Virtually nothing - honestly!

--- build.sh
+++ build.sh
@@ -188,7 +188,7 @@ case "$MODE" in
 
     "test-installed")
         PREFIX="$1"
-        make -C test-installed test || exit 1
+        make -C "test-installed" test || exit 1
         exit 0
         ;;
 
