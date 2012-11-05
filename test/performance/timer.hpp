e3434f8894ff48f6153c1a0699e1d7711bde29b9
Kristian Spangsege <kristian.spangsege@gmail.com>
2012-11-05 18:47:24 +0100

    Fixes for: Makefile added for test/performance

--- test/performance/timer.hpp
+++ test/performance/timer.hpp
@@ -1,13 +1,27 @@
+/*************************************************************************
+ *
+ * TIGHTDB CONFIDENTIAL
+ * __________________
+ *
+ *  [2011] - [2012] TightDB Inc
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of TightDB Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to TightDB Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from TightDB Incorporated.
+ *
+ **************************************************************************/
 #ifndef TIGHTDB_TIMER_HPP
 #define TIGHTDB_TIMER_HPP
 
-#ifndef _MSC_VER
-#  include <time.h>
-#else
-#  include <windows.h>
-#endif
-
 namespace tightdb {
+
 
 /// Get the number of milliseconds since the system was started (or
 /// since some other arbitrary point in time after the system was
@@ -16,32 +30,6 @@ namespace tightdb {
 /// a 31-bit representation).
 long get_timer_millis();
 
-
-#ifndef _MSC_VER
-struct InitialTime {
-    timespec m_ts;
-    InitialTime()
-    {
-        clock_gettime(CLOCK_MONOTONIC, &m_ts);
-    }
-};
-long get_timer_millis()
-{
-    static const InitialTime init;
-    timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    if (ts.tv_nsec < init.m_ts.tv_nsec) {
-        ts.tv_sec  -= 1;
-        ts.tv_nsec += 1000000000L;
-    }
-    return long(ts.tv_sec - init.m_ts.tv_sec) * 1000 + (ts.tv_nsec - init.m_ts.tv_nsec) / 1000000L;
-}
-#else
-long get_timer_millis()
-{
-    return GetTickCount();
-}
-#endif
 
 } // namespace tightdb
 
