cc6b3d92ed6c45a0257d185ee4ffe33c0bb0f39f
Kenneth Geisshirt <kenneth@geisshirt.dk>
2012-11-29 10:34:46 +0100

    More transaction benchmarking

--- test/benchmark-transaction/transact.cpp
+++ test/benchmark-transaction/transact.cpp
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
 #include <assert.h>
 #include <pthread.h>
 #include <string.h>
@@ -31,9 +32,12 @@ struct thread_info {
     char     *datfile;
 };
 
+
+static bool verbose;
+
+// Shared variables and mutex to protect them
 static bool runnable = true;
 static pthread_mutex_t mtx_runnable = PTHREAD_MUTEX_INITIALIZER;
-
 
 static double dt_readers;
 static long iteration_readers;
@@ -53,7 +57,34 @@ void usage(const char *msg) {
     cout << " -d   : database (tdb or sqlite)" << endl;
     cout << " -t   : duration (in secs)" << endl;
     cout << " -v   : verbose" << endl;
+    cout << " -s   : single run" << endl;
     exit(-1);
+}
+
+
+// minimal version: no error checks!
+void copy(const char *src, const char *dst) {
+    int fd_to, fd_from;
+    char buf[4096];
+    ssize_t nread;
+    int saved_errno;
+
+    fd_from = open(src, O_RDONLY);
+    fd_to = open(dst, O_WRONLY | O_CREAT | O_EXCL, 0666);
+    while ((nread = read(fd_from, buf, sizeof(buf))) > 0) {
+        char *out_ptr = buf;
+        ssize_t nwritten;
+        do {
+            nwritten = write(fd_to, out_ptr, nread);
+            if (nwritten >= 0) {
+                nread -= nwritten;
+                out_ptr += nwritten;
+            }
+        } while (nread > 0);
+    }
+
+    close(fd_from);
+    close(fd_to);
 }
 
 double delta_time(struct timespec ts_1, struct timespec ts_2) {
@@ -78,6 +109,12 @@ void update_writer(struct timespec ts_1, struct timespec ts_2) {
 }
 
 
+
+int db_retry(void *data, int i) {
+    return 1;
+}
+
+
 static void *sqlite_reader(void *arg) {
     struct timespec ts_1, ts_2;
     sqlite3 *db;
@@ -88,6 +125,7 @@ static void *sqlite_reader(void *arg) {
     struct thread_info *tinfo = (struct thread_info *)arg;
     srandom(tinfo->thread_num);
     sqlite3_open(tinfo->datfile, &db);
+    sqlite3_busy_handler(db, &db_retry, NULL);
     while (true) {
         pthread_mutex_lock(&mtx_runnable);
         bool local_runnable = runnable;
@@ -102,7 +140,9 @@ static void *sqlite_reader(void *arg) {
         randy = random() % 1000;
         sprintf(sql, "SELECT COUNT(*) FROM test WHERE y = %ld", randy);
         sqlite3_exec(db, sql, NULL, NULL, &errmsg);
-        sqlite3_exec(db, "END TRANSACTION", NULL, NULL, &errmsg);
+        if (sqlite3_exec(db, "END TRANSACTION", NULL, NULL, &errmsg) == SQLITE_BUSY) {
+            cout << "Oops" << endl;
+        }
 
         // update statistics
         clock_gettime(CLOCK_REALTIME, &ts_2);
@@ -140,7 +180,7 @@ static void *tdb_reader(void *arg) {
     }
     return NULL;
 }
- 
+
 static void *sqlite_writer(void *arg) {
     sqlite3 *db;
     long int randx, randy;
@@ -152,6 +192,7 @@ static void *sqlite_writer(void *arg) {
     srandom(tinfo->thread_num);
 
     sqlite3_open(tinfo->datfile, &db);
+    sqlite3_busy_handler(db, &db_retry, NULL);
     while (true) {
         pthread_mutex_lock(&mtx_runnable);
         bool local_runnable = runnable;
@@ -162,16 +203,17 @@ static void *sqlite_writer(void *arg) {
         clock_gettime(CLOCK_REALTIME, &ts_1);
 
         // execute transaction        
-        sqlite3_exec(db, "BEGIN TRANSACTION", NULL, NULL, &errmsg);
+        sqlite3_exec(db, "BEGIN EXCLUSIVE TRANSACTION", NULL, NULL, &errmsg);
         randx = random() % 1000;
         randy = random() % 1000;
         sprintf(sql, "UPDATE test VALUES (%ld, %ld) WHERE y = %ld", randx, randy, randy);
         sqlite3_exec(db, sql, NULL, NULL, &errmsg);
-        sqlite3_exec(db, "END TRANSACTION", NULL, NULL, &errmsg);
+        if (sqlite3_exec(db, "END TRANSACTION", NULL, NULL, &errmsg) == SQLITE_BUSY) {
+            cout << "Ooops" << endl;
+        }
 
-        // update statistics
-      
-  clock_gettime(CLOCK_REALTIME, &ts_2);
+        // update statistics    
+        clock_gettime(CLOCK_REALTIME, &ts_2);
         update_writer(ts_1, ts_2);
     }        
     sqlite3_close(db);
@@ -219,6 +261,10 @@ void sqlite_create(const char *f, long n) {
 
     remove(f);
     srandom(1);
+
+    if (sqlite3_config(SQLITE_CONFIG_MULTITHREAD, 1) == SQLITE_ERROR) {
+        cout << "SQLite has no multi-threading support" << endl;
+    }
     sqlite3_open(f, &db);
     sqlite3_exec(db, "CREATE TABLE test (x INT, y INT)", NULL, NULL, &errmsg);
     sqlite3_exec(db, "BEGIN TRANSACTION", NULL, NULL, &errmsg);
@@ -249,25 +295,98 @@ void tdb_create(const char *f, long n) {
 }
 
 
-int main(int argc, char *argv[]) {
-    int i, j, c;
-    int database = -1;
-    long n_readers = -1, n_writers = -1, n_records = -1;
-    unsigned int duration = 0;
-    bool verbose = false;
-    extern char *optarg;
-    void *res;
-    char *datfile = NULL;
+void benchmark(int database, const char *datfile, long n_readers, long n_writers, unsigned int duration, 
+                double& tps_readers, double& tps_writers) {
     pthread_attr_t attr;
     struct thread_info *tinfo;
-    double tps_readers = 0.0, tps_writers = 0.0;
+    void *res;
 
-    dt_readers = 0.0;
-    dt_writers = 0.0;
+    dt_readers        = 0.0;
     iteration_readers = 0;
+    dt_writers        = 0.0;
     iteration_writers = 0;
+    runnable          = true;
 
-    while ((c = getopt(argc, argv, "hr:w:f:n:t:d:v")) != EOF) {
+    if (verbose) cout << "Copying file" << endl;
+    unlink(("tmp"+string(datfile)).c_str());
+    unlink(("tmp"+string(datfile)+".lock").c_str());
+    copy(datfile, ("tmp"+string(datfile)).c_str());
+
+    assert(pthread_attr_init(&attr) == 0);
+    assert((tinfo = (struct thread_info *)calloc(sizeof(struct thread_info), n_readers+n_writers)) != NULL);
+   
+    for(int i=0; i<(n_readers+n_writers); ++i) {
+        tinfo[i].thread_num = i+1;
+        tinfo[i].datfile = strdup(("tmp"+string(datfile)).c_str());
+    }
+
+    if (verbose) cout << "Starting threads" << endl;
+    for(int i=0; i<n_readers; ++i) {
+        switch (database) {
+        case 0:
+            pthread_create(&tinfo[i].thread_id, &attr, &tdb_reader, &tinfo[i]);
+            break;
+        case 1:
+            pthread_create(&tinfo[i].thread_id, &attr, &sqlite_reader, &tinfo[i]);
+            break;
+        }
+    }
+    for(int i=0; i<n_writers; ++i) {
+        int j = i+n_readers;
+        switch (database) {
+        case 0:
+            pthread_create(&tinfo[j].thread_id, &attr, &tdb_writer, &tinfo[j]);
+            break;
+        case 1:
+            pthread_create(&tinfo[j].thread_id, &attr, &sqlite_writer, &tinfo[j]);
+            break;
+        }
+    }
+
+    if (verbose) cout << "Waiting for " << duration << " seconds" << endl;
+    sleep(duration);
+
+    if (verbose) cout << "Cancelling threads" << endl;
+    pthread_mutex_lock(&mtx_runnable);
+    runnable = false;
+    pthread_mutex_unlock(&mtx_runnable);
+ 
+    if (verbose)
+        cout << "Waiting for threads" << endl;
+    for(int i=0; i<n_readers; ++i) {
+        pthread_join(tinfo[i].thread_id, &res);
+    }
+
+    if (n_readers > 0) 
+        tps_readers = (double)(iteration_readers)/dt_readers;
+    else
+        tps_readers = 0.0;
+
+    for(int i=0; i<n_writers; ++i) {
+        int j = i+n_readers;
+        pthread_join(tinfo[j].thread_id, &res);
+    }
+    if (n_writers > 0)
+        tps_writers = (double)(iteration_writers)/dt_writers;    
+    else
+        tps_writers = 0.0;
+
+    unlink(("tmp"+string(datfile)).c_str());
+    unlink(("tmp"+string(datfile)+".lock").c_str());
+    free((void *)tinfo); 
+}
+
+int main(int argc, char *argv[]) {
+    int c;
+    long n_readers = -1, n_writers = -1, n_records = -1;
+    unsigned int duration = 0;
+    int database = -1;
+    bool single  = false;
+    extern char *optarg;
+    char *datfile = NULL;
+
+    verbose = false;
+    while ((c = getopt(argc, argv, "hr:w:f:n:t:d:vs")) != EOF) {
         switch (c) {
         case 'h':
             usage("");
@@ -287,10 +406,18 @@ int main(int argc, char *argv[]) {
             duration = atoi(optarg);
             break;
         case 'd':
-            database = (strcmp(optarg, "tdb") == 0);
+            if (strcmp(optarg, "tdb") == 0) {
+                database = 0;
+            }
+            else {
+                database = 1;
+            }
             break;
         case 'v':
             verbose = true;
+            break;
+        case 's':
+            single = true;
             break;
         default:
             usage("Wrong option");
@@ -305,68 +432,52 @@ int main(int argc, char *argv[]) {
         usage("-n missing");
     if (duration < 1)
         usage("-t missing");
-    if (database == -1)
+    if (database == -1 && single)
         usage("-d missing");
-    if (datfile == NULL)
+    if (datfile == NULL && single)
         usage("-f missing");
 
 
-    assert(pthread_attr_init(&attr) == 0);
-    assert((tinfo = (struct thread_info *)calloc(sizeof(struct thread_info), n_readers+n_writers)) != NULL);
-   
-    if (verbose)
-        std::cout << "Creating test data" << std::endl;
-    if (database) 
-        tdb_create(datfile, n_records);
-    else 
-        sqlite_create(datfile, n_records);
-
-    for(i=0; i<(n_readers+n_writers); ++i) {
-        tinfo[i].thread_num = i+1;
-        tinfo[i].datfile = strdup(datfile);
+    if (verbose) cout << "Creating test data" << endl;
+    if (single) {
+        switch (database) {
+        case 0: 
+            tdb_create(datfile, n_records);
+            break;
+        case 1:
+            sqlite_create(datfile, n_records);
+            break;
+        }
+    }
+    else {
+        tdb_create("test.tdb", n_records);
+        sqlite_create("test.sqlite", n_records);
     }
 
-    if (verbose)
-        std::cout << "Starting threads" << std::endl;
-    for(i=0; i<n_readers; ++i) {
-        if (database)
-            pthread_create(&tinfo[i].thread_id, &attr, &tdb_reader, &tinfo[i]);
-        else
-            pthread_create(&tinfo[i].thread_id, &attr, &sqlite_reader, &tinfo[i]);
+    if (single) {
+        double tps_readers, tps_writers;
+        benchmark(database, datfile, n_readers, n_writers, duration, tps_readers, tps_writers);
+        cout << tps_readers << " " << tps_writers << endl;
     }
-    for(i=0; i<n_writers; ++i) {
-        j = i+n_readers;
-        if (database)
-            pthread_create(&tinfo[j].thread_id, &attr, &tdb_writer, &tinfo[j]);
-        else
-            pthread_create(&tinfo[j].thread_id, &attr, &sqlite_writer, &tinfo[j]);
-    }
-
-
-    if (verbose)
-        cout << "Waiting for " << duration << " seconds" << endl;
-    sleep(duration);
-
-    if (verbose)
-        cout << "Cancelling threads" << endl;
-    pthread_mutex_lock(&mtx_runnable);
-    runnable = false;
-    pthread_mutex_unlock(&mtx_runnable);
- 
-    if (verbose)
-        cout << "Waiting for threads" << endl;
-    for(i=0; i<n_readers; ++i) {
-        pthread_join(tinfo[i].thread_id, &res);
-    }
-    if (n_readers > 0) 
-        tps_readers = (double)(iteration_readers)/dt_readers;
-
-    for(i=0; i<n_writers; ++i) {
-        j = i+n_readers;
-        pthread_join(tinfo[j].thread_id, &res);
-    }
-    if (n_writers > 0)
-        tps_writers = (double)(iteration_writers)/dt_writers;    
-
-    std::cout << tps_readers << " " << tps_writers << std::endl;
+    else {
+        for(int i=0; i<=n_readers; ++i) {
+            for(int j=0; j<=n_writers; ++j) {
+                double tps_readers_sqlite, tps_writers_sqlite;
+                double tps_readers_tdb, tps_writers_tdb;
+                benchmark(0, "test.tdb", i, j, duration, tps_readers_tdb, tps_writers_tdb);
+                benchmark(1, "test.sqlite", i, j, duration, tps_readers_sqlite, tps_writers_sqlite);
+                cout << i << " " << j << " ";
+                if (i == 0)
+                    cout << "0.0";
+                else
+                    cout << tps_readers_tdb/tps_readers_sqlite;
+                cout << " ";
+                if (j == 0)
+                    cout << "0.0";
+                else
+                    cout << tps_writers_tdb/tps_writers_sqlite;
+                cout << endl;
+            }
+        }
+    }       
 }
