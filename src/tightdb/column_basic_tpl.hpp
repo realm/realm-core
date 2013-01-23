da591a764f19de7d8fd73ce0ba0c94ce08944111
Brian Munkhol <bm@tightdb.com>
2013-01-23 22:00:01 +0100

    float: added header to a file.

--- src/tightdb/column_basic_tpl.hpp
+++ src/tightdb/column_basic_tpl.hpp
@@ -1,3 +1,25 @@
+/*************************************************************************
+ *
+ * TIGHTDB CONFIDENTIAL
+ * __________________
+ *
+ *  [2011] - [2012] TightDB Inc
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of TightDB Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to TightDB Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from TightDB Incorporated.
+ *
+ **************************************************************************/
+#ifndef TIGHTDB_COLUMN_BASIC_TPL_HPP
+#define TIGHTDB_COLUMN_BASIC_TPL_HPP
+
 #include <tightdb/query_engine.hpp>
 
 
@@ -321,7 +343,9 @@ void ColumnBasic<T>::sort(size_t start, size_t end)
 
 #else
 
-// TODO: test performance of column aggregates, remove slowest
+// Alternative 'naive' implementation:
+// TODO: test performance of column aggregates
+
 template<typename T>
 size_t ColumnBasic<T>::count(T target) const
 {
@@ -438,4 +462,6 @@ T ColumnBasic<T>::maximum(size_t start, size_t end) const
 
 #endif
 
-}
+} // namespace tightdb
+
+#endif // TIGHTDB_COLUMN_BASIC_TPL_HPP
