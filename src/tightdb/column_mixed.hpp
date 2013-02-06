8d8f41e81bce1b325f3e5725603b915ca2142fea
Brian Munkholm <bm@tightdb.com>
2013-02-06 12:46:09 +0100

    minor cleanup after review comments.

--- src/tightdb/column_mixed.hpp
+++ src/tightdb/column_mixed.hpp
@@ -144,21 +144,22 @@ private:
     void InitDataColumn();
 
     enum MixedColType {
+        // NOTE: below numbers must be kept in sync with ColumnType
         // Column types used in Mixed
         MIXED_COL_INT         =  0,
         MIXED_COL_BOOL        =  1,
         MIXED_COL_STRING      =  2,
-        MIXED_COL_STRING_ENUM =  3, // double refs
+                              // 3, used for STRING_ENUM in ColumnType
         MIXED_COL_BINARY      =  4,
         MIXED_COL_TABLE       =  5,
         MIXED_COL_MIXED       =  6,
         MIXED_COL_DATE        =  7,
-        MIXED_COL_RESERVED1   =  8, // DateTime
+                              // 8, used for RESERVED1 in ColumnType
         MIXED_COL_FLOAT       =  9, // Float
         MIXED_COL_DOUBLE      = 10, // Positive Double
         MIXED_COL_DOUBLE_NEG  = 11, // Negative Double
-        MIXED_COL_INT_NEG     = 12 // Negative Integers
-        // Preserve values above for backward compability
+        MIXED_COL_INT_NEG     = 12  // Negative Integers
+
     };
 
     void clear_value(size_t ndx, MixedColType newtype);
