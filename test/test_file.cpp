893b777de8c048d584aba9d322e87c46097bd58d
Kristian Spangsege <kristian.spangsege@gmail.com>
2013-06-04 17:56:44 +0200

    Fixes for: New method File::is_same_file() added

--- test/test_file.cpp
+++ test/test_file.cpp
@@ -15,15 +15,16 @@ TEST(File_ExistsAndRemove)
 
 TEST(File_IsSame)
 {
-    File f1("test_file_1", File::mode_Write);
-    File f2("test_file_1", File::mode_Read);
-    File f3("test_file_2", File::mode_Write);
+    {
+        File f1("test_file_1", File::mode_Write);
+        File f2("test_file_1", File::mode_Read);
+        File f3("test_file_2", File::mode_Write);
 
-    CHECK(f1.is_same_file(f1));
-    CHECK(f1.is_same_file(f2));
-    CHECK(!f1.is_same_file(f3));
-    CHECK(!f2.is_same_file(f3));
-
+        CHECK(f1.is_same_file(f1));
+        CHECK(f1.is_same_file(f2));
+        CHECK(!f1.is_same_file(f3));
+        CHECK(!f2.is_same_file(f3));
+    }
     File::try_remove("test_file_1");
     File::try_remove("test_file_2");
 }
