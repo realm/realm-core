4d638f143a23ea1015750e7a9b76411c31ac7c54
Kenneth Geisshirt <kenneth@geisshirt.dk>
2013-06-20 15:45:28 +0200

    Adding benchmark of Group.

--- test/benchmark-insert-add/add_insert.cpp
+++ test/benchmark-insert-add/add_insert.cpp
@@ -2,9 +2,12 @@
 #include <tightdb/group_shared.hpp>
 #include <unistd.h>
 #include <time.h>
+#include <cstdio>
 
 using namespace tightdb;
 using namespace std;
+
+enum Mode {USE_SHARED, USE_GROUP, USE_TABLE};
 
 TIGHTDB_TABLE_5(TestTable,
                 x,  Int,
@@ -18,11 +21,23 @@ double delta_time(struct timespec ts_1, struct timespec ts_2) {
 }
 
 
+void usage(void) {
+    cout << "Usage: add_insert [-h] [-s mem|full] [-a]" << endl;
+    cout << "  -h : this text" << endl;
+    cout << "  -s : use shared group (default: no)" << endl;
+    cout << "  -i : insert at front (defalut: no - append)" << endl;
+    cout << "  -N : number of rows to add" << endl;
+    cout << "  -n : rows between print outs" << endl;
+    cout << "  -g : use group (default: no)" << endl;
+    cout << "  -r : rows/commit (default: 1)" << endl;
+    exit(-1);
+}
 
 
 int main(int argc, char *argv[]) {
     size_t N = 100000000;
     size_t n = 50000;
+    size_t rows_per_commit = 1;
     struct timespec ts_begin, ts_now;
     TestTable t;
 
@@ -32,17 +47,12 @@ int main(int argc, char *argv[]) {
     bool use_shared = false;
     SharedGroup::DurabilityLevel dlevel;
     bool do_insert  = false;
+    bool use_group  = false;
 
-    while ((c = getopt(argc, argv, "hs:iN:n:")) != EOF) {
+    while ((c = getopt(argc, argv, "hs:iN:n:r:g")) != EOF) {
         switch (c) {
             case 'h':
-                cout << "Usage: add_insert [-h] [-s mem|full] [-a]" << endl;
-                cout << "  -h : this text" << endl;
-                cout << "  -s : use shared group (default: no)" << endl;
-                cout << "  -i : insert at front (defalut: no - append)" << endl;
-                cout << "  -N : number of rows to add" << endl;
-                cout << "  -n : rows between print outs" << endl;
-                exit(-1);
+                usage();
                 break;
             case 's':
                 use_shared = true;
@@ -68,53 +78,118 @@ int main(int argc, char *argv[]) {
             case 'n':
                 n = size_t(atol(optarg));
                 break;
+            case 'g':
+                use_group = true;
+                break;
+            case 'r':
+                rows_per_commit = size_t(atol(optarg));
+                break;
             default:
                 cout << "Unknown option" << endl;
-                exit(-1);
+                usage();
         }
     }
+
+    if (use_group && use_shared) {
+        cout << "You cannot specify -g and -s at the same time." << endl;
+        usage();
+    }
+
+    Mode m;
+    if (use_group) {
+        m = USE_GROUP;
+    }
+    else {
+        if (use_shared) {
+            m = USE_SHARED;
+        }
+        else {
+            m = USE_TABLE;
+        }
+    }
+
+    cout << "# Parameters: " << endl;
+    cout << "#  number of rows    : " << N << endl;
+    cout << "#  rows per commit   : " << rows_per_commit << endl;
+    cout << "#  output frequency  : " << n << endl;
+    cout << "#  mode              : " << m << endl;
+    if (do_insert) {
+        cout << "# do inserts" << endl;
+    }
+
+    File::try_remove("test.tightdb");
+    File::try_remove("test.tightdb.lock");
+    File::try_remove("gtest.tightdb");
 
     SharedGroup sg = SharedGroup("test.tightdb", false, dlevel);
 
-    if (use_shared) {
-        File::try_remove("test.tightdb");
-        File::try_remove("test.tightdb.lock");
-        {
-            WriteTransaction wt(sg);
-            BasicTableRef<TestTable> t = wt.get_table<TestTable>("test");
-            wt.commit();
-        }
-    }
-
-
-    clock_gettime(CLOCK_REALTIME, &ts_begin);
-    for(size_t i=0; i<N; ++i) {
-        if (use_shared) {
+    switch (m) {
+        case USE_SHARED:
             {
                 WriteTransaction wt(sg);
-                BasicTableRef<TestTable> t1 = wt.get_table<TestTable>("test");
-                if (do_insert) {
-                    t1->insert(0, N, "Hello", i%2, "World", "Smurf");
-                }
-                else {
-                    t1->add(N, "Hello", i%2, "World", "Smurf");
-                }
+                BasicTableRef<TestTable> t = wt.get_table<TestTable>("test");
                 wt.commit();
             }
-        }
-        else {
-            if (do_insert) {
-                t.insert(0, N, "Hello", i%2, "World", "Smurf");
+            break;
+        case USE_GROUP:
+            Group g("gtest.tightdb");
+            BasicTableRef<TestTable> t = g.get_table<TestTable>("test");
+            break;
+    }
+
+    clock_gettime(CLOCK_REALTIME, &ts_begin);
+    for(size_t i=0; i<N/rows_per_commit; ++i) {
+        switch (m) {
+            case USE_SHARED: {
+                WriteTransaction wt(sg);
+                BasicTableRef<TestTable> t1 = wt.get_table<TestTable>("test");
+                {
+                    for(size_t j=0; j<rows_per_commit; ++j) {
+                        if (do_insert) {
+                            t1->insert(0, N, "Hello", i%2, "World", "Smurf");
+                        }
+                        else {
+                            t1->add(N, "Hello", i%2, "World", "Smurf");
+                        }
+                    }
+                }
+                wt.commit();
+                break;
             }
-            else {
-                t.add(N, "Hello", i%2, "World", "Smurf");
+            case USE_GROUP: {
+                {
+                    Group g("gtest.tightdb");
+                    BasicTableRef<TestTable> t1 = g.get_table<TestTable>("test");
+                    for(size_t j=0; j<rows_per_commit; ++j) {
+                        if (do_insert) {
+                            t1->insert(0, N, "Hello", i%2, "World", "Smurf");
+                        }
+                        else {
+                            t1->add(N, "Hello", i%2, "World", "Smurf");
+                        }
+                    }
+                    File::try_remove("tmp.tightdb");
+                    g.write("tmp.tightdb");
+                }
+                rename("tmp.tightdb", "gtest.tightdb");
+                break;
             }
+            case USE_TABLE:
+                for(size_t j=0; j<rows_per_commit; ++j) {
+                    if (do_insert) {
+                        t.insert(0, N, "Hello", i%2, "World", "Smurf");
+                    }
+                    else {
+                        t.add(N, "Hello", i%2, "World", "Smurf");
+                    }
+                }
+                break;
         }
 
-        if ((i % n) == 0 && i > 0) {
+        if (((i*rows_per_commit) % n) == 0 && i > 0) {
             clock_gettime(CLOCK_REALTIME, &ts_now);
             double dt = delta_time(ts_begin, ts_now);
-            cout << i << ";" << dt << ";" << double(i)/dt << endl;
+            cout << i*rows_per_commit << ";" << dt << ";" << double(i*rows_per_commit)/dt << ";" << dt/double(i*rows_per_commit) << endl;
         }
     }
     return 0;
