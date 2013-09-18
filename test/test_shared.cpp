300fb9964e3597dafe1a79bd3026fc46dec760f4
Kristian Spangsege <kristian.spangsege@gmail.com>
2013-09-18 16:06:23 +0200

    Reenabling failing string index unit test

--- test/test_shared.cpp
+++ test/test_shared.cpp
@@ -1181,7 +1181,6 @@ TEST(StringIndex_Bug2)
 }
 
 
-/* DISABLED DUE TO BUG https://github.com/Tightdb/tightdb/pull/145
 namespace {
 void rand_str(char* res, size_t len) {
     for (size_t i = 0; i < len; ++i) {
@@ -1242,7 +1241,6 @@ TEST(StringIndex_Bug3)
         }
     }
 }
-*/
 
 
 TEST(Shared_MixedWithNonShared)
