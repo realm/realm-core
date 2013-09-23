4a27383b63c6e76694004940e26e51f349dd1d70
Finn Schiermer Andersen <fsa@tightdb.com>
2013-09-23 17:52:48 +0200

    bugfix: doing append->insert

--- test/benchmark-insert-add/run.sh
+++ test/benchmark-insert-add/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 PATH=/bin:/usr/bin
-N=500000
-n=5000
+N=5000
+n=50
 
 ./add_insert         -N $(($N*10)) -n $(($n*10)) > append_table_inmem.dat
 ./add_insert -i      -N $(($N*10)) -n $(($n*10)) > insert_table_inmem.dat
 ./add_insert -s mem  -N $N         -n $n         > append_transact_inmem.dat
-./add_insert -s mem  -N $N         -n $n         > insert_transact_inmem.dat
+./add_insert -i -s mem  -N $N      -n $n         > insert_transact_inmem.dat
 ./add_insert -g      -N $N         -n $n         > append_group.dat
 ./add_insert -g -i   -N $N         -n $n         > insert_group.dat
 ./add_insert -s full -N $N         -n $n         > append_transact_full.dat
