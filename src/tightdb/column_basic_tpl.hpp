ee85d79e44ebeaa119e63b35ef2e23de5ef29de0
Lasse Reinhold <rrrlasse@hotmail.com>
2013-10-03 11:15:18 +0200

    Made aggregates on float and double use query_engine.hpp (now that it's public) again for speed, instead of manual loop.

--- src/tightdb/column_basic_tpl.hpp
+++ src/tightdb/column_basic_tpl.hpp
@@ -20,6 +20,13 @@
 #ifndef TIGHTDB_COLUMN_BASIC_TPL_HPP
 #define TIGHTDB_COLUMN_BASIC_TPL_HPP
 
+// It's bad design (headers are entangled) that a Column uses query_engine.hpp which again uses Column.
+#define TIGHTDB_DEPEND_ON_QUERYENGINE
+
+#ifdef TIGHTDB_DEPEND_ON_QUERYENGINE
+    #include <tightdb/query_engine.hpp>
+#endif
+
 namespace tightdb {
 
 // Predeclarations from query_engine.hpp
@@ -255,21 +262,23 @@ void BasicColumn<T>::find_all(Array &result, T value, std::size_t start, std::si
 template<class T>
 std::size_t BasicColumn<T>::count(T target) const
 {
+#ifdef TIGHTDB_DEPEND_ON_QUERYENGINE
+    // Slow method that doesn't depend on query_engine
     size_t cnt = 0;
     for(size_t i = 0; i < size(); i++)
         if(get(i) == target)
             cnt++;
 
     return cnt;
-// Above because we need to get rid of #include query_engine dependency in this header. Todo, 
-// we must put below call inside a .cpp file and #include query_engine there and use that instead
-// because it's faster than above loop.
-// return std::size_t(ColumnBase::aggregate<T, int64_t, act_Count, Equal>(target, 0, size(), 0));
+#else
+    return std::size_t(ColumnBase::aggregate<T, int64_t, act_Count, Equal>(target, 0, size(), 0));
+#endif
 }
 
 template<class T>
 typename BasicColumn<T>::SumType BasicColumn<T>::sum(std::size_t start, std::size_t end, size_t limit) const
 {
+#ifdef TIGHTDB_DEPEND_ON_QUERYENGINE
     if (end == std::size_t(-1))
         end = size();
 
@@ -281,10 +290,9 @@ typename BasicColumn<T>::SumType BasicColumn<T>::sum(std::size_t start, std::siz
         sum += get(i);
 
     return sum;
-// Above because we need to get rid of #include query_engine dependency in this header. Todo, 
-// we must put below call inside a .cpp file and #include query_engine there and use that instead
-// because it's faster than above loop.
-// return ColumnBase::aggregate<T, SumType, act_Sum, None>(0, start, end, 0);
+#else
+    return ColumnBase::aggregate<T, SumType, act_Sum, None>(0, start, end, 0);
+#endif
 }
 
 template<class T>
@@ -305,6 +313,7 @@ double BasicColumn<T>::average(std::size_t start, std::size_t end, size_t limit)
 template<class T>
 T BasicColumn<T>::minimum(std::size_t start, std::size_t end, size_t limit) const
 {
+#ifdef TIGHTDB_DEPEND_ON_QUERYENGINE
     if (end == std::size_t(-1))
         end = size();
 
@@ -317,15 +326,15 @@ T BasicColumn<T>::minimum(std::size_t start, std::size_t end, size_t limit) cons
             min = get(i);
 
     return min;
-// Above because we need to get rid of #include query_engine dependency in this header. Todo, 
-// we must put below call inside a .cpp file and #include query_engine there and use that instead
-// because it's faster than above loop.
-// return ColumnBase::aggregate<T, T, act_Min, None>(0, start, end, 0);
+#else
+    return ColumnBase::aggregate<T, T, act_Min, None>(0, start, end, 0);
+#endif
 }
 
 template<class T>
 T BasicColumn<T>::maximum(std::size_t start, std::size_t end, size_t limit) const
 {
+#ifdef TIGHTDB_DEPEND_ON_QUERYENGINE
     if (end == std::size_t(-1))
         end = size();
 
@@ -338,10 +347,9 @@ T BasicColumn<T>::maximum(std::size_t start, std::size_t end, size_t limit) cons
             max = get(i);
 
     return max;
-// Above because we need to get rid of #include query_engine dependency in this header. Todo, 
-// we must put below call inside a .cpp file and #include query_engine there and use that instead
-// because it's faster than above loop.
-// return ColumnBase::aggregate<T, T, act_Max, None>(0, start, end, 0);
+#else
+    return ColumnBase::aggregate<T, T, act_Max, None>(0, start, end, 0);
+#endif
 }
 
 
