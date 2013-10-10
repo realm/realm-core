5c6eff7f0ede84442f9209c7a9a738aab78bd1dd
Lasse Reinhold <rrrlasse@hotmail.com>
2013-10-10 17:10:06 +0200

    Fixed delete() bug in c++ example. All examples pass now

--- test/test_table.cpp
+++ test/test_table.cpp
@@ -30,27 +30,21 @@ TEST(DeleteCrash)
     Group group;
     TableRef table = group.get_table("test");
 
-    Spec& s = table->get_spec();
-    s.add_column(type_String, "name");
-    s.add_column(type_Int,    "age");
-    table->update_from_spec();
+    table->add_column(type_String, "name");
+    table->add_column(type_Int,    "age");
 
-    table->add_empty_row();
+    table->add_empty_row(3);
     table->set_string(0, 0, "Alice");
-    table->insert_int(1, 0, 27);
+    table->set_int(1, 0, 27);
 
-    table->add_empty_row();
     table->set_string(0, 1, "Bob");
-    table->insert_int(1, 1, 50);
+    table->set_int(1, 1, 50);
 
-    table->add_empty_row();
     table->set_string(0, 2, "Peter");
-    table->insert_int(1, 2, 44);
+    table->set_int(1, 2, 44);
 
-    // Here it thinks second column has 6 rows?!
     table->remove(0);
 
-    // This crashes. It thinks that the second column has 5 rows now
     table->remove(1);
 }
 
