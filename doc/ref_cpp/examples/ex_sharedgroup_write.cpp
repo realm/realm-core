180476815583d465bbde17f8b6d3c08c28993682
Finn Schiermer Andersen <fsa@tightdb.com>
2013-11-05 17:22:55 +0100

    reorganized shared group and added examples

---
+++ doc/ref_cpp/examples/ex_sharedgroup_write.cpp
@@ -0,0 +1,59 @@
+// @@Example: ex_cpp_shared_group_write @@
+// @@Fold@@
+#include <cassert>
+#include <tightdb.hpp>
+#include <tightdb/file.hpp>
+
+using namespace tightdb;
+
+// Define schema for main table
+TIGHTDB_TABLE_3(PeopleTable,
+                  name,   String,
+                  age,    Int,
+                  hired,  Bool)
+
+// @@EndFold@@
+void some_function_making_it_impossible_to_use_RAII(SharedGroup& g, bool should_commit)
+{
+    if (should_commit)
+        db.commit();
+    else
+        db.rollback();
+}
+
+void func()
+{
+    // Create a new shared group
+    SharedGroup db("shared_db.tightdb");
+
+    // Do a write transaction
+    {
+        Group& g = db.begin_write();
+
+        try {
+
+            // Get table (creating it if it does not exist)
+            PeopleTable::Ref employees = g.get_table<PeopleTable>("employees");
+
+            // Add initial rows (with sub-tables)
+            if (employees->is_empty()) {
+                employees->add("joe", 42, false);
+                employees->add("jessica", 22, true);
+            }
+            some_function_making_it_impossible_to_use_RAII(db, true);
+        }
+        catch (...) {
+            db.rollback();
+            throw;
+        }
+    }
+}
+
+// @@Fold@@
+int main()
+{
+    func();
+    File::remove("shared_db.tightdb");
+}
+// @@EndFold@@
+// @@EndExample@@
