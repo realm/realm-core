c07b58ca1b406136937ab3bcc383c0efcd1bbbc2
Finn Schiermer Andersen <fsa@tightdb.com>
2013-11-06 11:08:26 +0100

    throwing LockFileButNoData

--- src/tightdb/group_shared.cpp
+++ src/tightdb/group_shared.cpp
@@ -424,7 +424,12 @@ void SharedGroup::open(const string& path, bool no_create_file,
             bool read_only = false;
             bool no_create = true;
             bool skip_validate = true; // To avoid race conditions
-            alloc.attach_file(path, is_shared, read_only, no_create, skip_validate); // Throws
+            try {
+                alloc.attach_file(path, is_shared, read_only, no_create, skip_validate); // Throws
+            } 
+            catch (File::NotFound) {
+                throw LockFileButNoData(path);
+            }
 
         }
 
