7cb2a5a03735457afccf8066146914849b401e8f
Kenneth Geisshirt <kenneth@geisshirt.dk>
2013-11-11 23:31:16 +0100

    One-line installer: fix a syntax error.

--- build.sh
+++ build.sh
@@ -864,7 +864,7 @@ if [ \$# -gt 0 -a "\$1" = "interactive" ]; then
                 cp -a tightdb_\$x/examples/* \$HOME/tightdb_examples/\$x
             fi
         done
-        if [ \$(echo \$EXT | grep -c c++) -eq 1]; then
+        if [ \$(echo \$EXT | grep -c c++) -eq 1 ]; then
             cp -a tightdb/examples/* \$HOME/tightdb_examples/c++
         fi
         if [ \$(echo \$EXT | grep -c java) -eq 1 ]; then
