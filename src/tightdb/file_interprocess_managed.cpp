9c6bd887854f15605f187dc23f3736135f48371b
Finn Schiermer Andersen <fsa@tightdb.com>
2013-11-15 16:16:57 +0100

    open mostly done

--- src/tightdb/file_interprocess_managed.cpp
+++ src/tightdb/file_interprocess_managed.cpp
@@ -34,6 +34,7 @@ struct IPMFileSharedInfo {
     enum State { Unitialized = 0, Ready, Stale };
     Atomic<State> m_state;
     Atomic<uint32_t> m_transition_count;
+    Atomic<uint32_t> m_activity_count;
 };
 
 struct IPMFile::IPMFileImplementation {
@@ -94,14 +95,16 @@ void* IPMFile::open(bool& is_exclusive, size_t size, int msec_timeout)
         m_impl->m_file.open(m_impl->m_path, need_init);
 
         // Try to get a lock - preferably an exclusive lock,
-        // but if not be contend with a shared lock. It is important to try
+        // but if not be contend with a shared lock for now. It is important to try
         // (and potentially retry) in order to ensure that ONE of the clients
         // gets exclusive access if possible.
         got_exclusive = m_impl->m_file.try_lock_exclusive();
-        if (got_exclusive)
+        if (got_exclusive) {
             got_shared = false;
-        else
+        }
+        else {
             got_shared = m_impl->m_file.try_lock_shared();
+        }
         if (!got_exclusive && !got_shared) {
 
             // someone else has exclusive access, backout and retry
@@ -109,92 +112,72 @@ void* IPMFile::open(bool& is_exclusive, size_t size, int msec_timeout)
             msec_timeout--;
             micro_sleep(1000);
             continue;
-
         }
 
-        // Regardless of lock, if the file is marked as stale, try to remove it (non-faulting way)
-        // and start all over again. Unfortunately, to get access to the State field, we need to
-        // map it, and to map it we must be sure that the file is large enough, hence first some
-        // checks for file size
+        // If the file is smaller than a size limit we trust the exclusive lock and initialize
+        // the file. The file is only smaller than the size limit before initialization.
+        // If the file is too small and we didn't get the exclusive lock, we back out and retry.
         if (int_cast_with_overflow_detect(m_impl->m_file.get_size(), file_size)) {
 
             m_impl->m_file.unlock();
             m_impl->m_file.close();
-            // File::try_remove(m_impl->m_path); <-- don't remove it, require operator intervention
+            // don't remove it, require operator intervention:
             throw std::runtime_error("Lock file too large");
         }
         if (file_size < size+sizeof(IPMFileMap)) {
 
-            m_impl->m_file.unlock();
-            m_impl->m_file.close();
-            File::try_remove(m_impl->m_path);
-        }
-        // now we can safely map the file and then check if it is marked Stale.
-        // (Finding a file marked stale is very unlikely. We mark files Stale just prior to closing
-        // and deleting them, so the only way to get a stay file is if a thread crashes after deciding
-        // to remove the file, but before actually removing it.
+            if (got_exclusive) {
 
+                // initialize the file. zero transition count, zero activity count:
+                file_size = sizeof (IPMFileMap) + size;
+                char* empty_buf = new char[file_size];
+                std::fill(empty_buf, empty_buf+file_size, 0);
+                m_impl->m_file.write(empty_buf, file_size);
+                delete[] empty_buf;
 
-
-        // Coming here, we have a lock AND we have an initialized file
-
-
-        // coming here we have a lock, either exclusive or shared.
-        // we now do a set of refined checks
-        // 1- if the file is too small to be mapped and we have exclusive access, initialise it
-        //    (otherwise backout and retry)
-        // 2- if the file is ok in size, but marked as stale, delete it and retry
-        // 3- if we have exclusive access, the file isn't stale and the transition count is non zero,
-        //    backout and retry. We assume that some other process is transitioning the state of the file.
-
-        // File Initialization step
-        if (need_init) {
-
-            // complete zero-initialization of entire mapped area
-            file_size = sizeof (IPMFileMap) + size;
-            char* empty_buf = new char[file_size];
-            std::fill(empty_buf, empty_buf+file_size, 0);
-            m_impl->m_file.write(empty_buf, file_size);
-            delete[] empty_buf;
-
-        } 
-        else {
-
-            // wait for file to be large enough to be mapped
-            File::CloseGuard fcg(m_impl->m_file);
-            if (int_cast_with_overflow_detect(m_impl->m_file.get_size(), file_size)) {
-
-                throw std::runtime_error("Lock file too large");
             }
-            while (msec_timeout >= 0 && file_size < size+sizeof(IPMFileMap)) {
+            if (got_shared) {
 
+                // we need exclusive access to init the file, so back out and retry:
+                m_impl->m_file.close();
                 msec_timeout--;
                 micro_sleep(1000);
-                if (int_cast_with_overflow_detect(m_impl->m_file.get_size(), file_size)) {
-                    throw std::runtime_error("Lock file too large");
-                }
+                continue;
             }
-            if (msec_timeout <= 0) {
-
-                throw PresumablyStaleFile(m_impl->m_path);
-            }
-            fcg.release();
         }
 
+        // The file has proper size and we can map it!
         File::CloseGuard fcg(m_impl->m_file);
-        // File is now known to be at least large enough to be mapped
-
-        // File mapping step
         m_impl->m_file_map.map(m_impl->m_file, File::access_ReadWrite, 
                                sizeof (IPMFileSharedInfo), File::map_NoSync);
         File::UnmapGuard fug_1(m_impl->m_file_map);
 
-        // Transition to fully initialized
+        // for files of proper size, having an exclusive lock does not alone guarantee exclusivity.
+        // - other threads may have held the exclusive lock and are now asking to enter shared state,
+        //   we just beat them (race) and got the exclusive lock
+        // - other threads may be trying to get the exclusive lock to detect if they are alone,
+        //   and to do that, they have to release their shared lock. But we beat them (race) and
+        //   got the exclusive lock.
+        // - other threads may have released their locks and are exiting (possibly with one of them
+        //   trying to delete the file in a moment)
+        // All the above scenarios are detected by inspecting a transition count. The transition
+        // count is incremented whenever a thread intiates a state transition and decremented when
+        // the transition is complete. A non-zero transition count indicates that the exclusive lock
+        // should not be trusted.
+        //
+        // A program crashing mid-transition will leave the lock file permanently damaged, in the
+        // sense that no one can obtain exclusive access until the lock file is removed. The state
+        // transitions are implemented such that they minimize the frequency.
+        //
+        // As long as programs are mid-transition, they must increment an activity counter within
+        // a very short time frame. When a file with non-zero transition count is encountered, the
+        // activity counter can be sampled and if it doesn't change within a short time frame, the
+        // file may be considered to be in indeterminate state, and an exception is thrown to
+        // indicate that operator intervention is required.
 
-
-    } while (timeleft);
-
-    return NULL;
+        // TODO: Look for transition changes
+    };
+    throw PresumablyStaleFile(m_impl->m_path);
 }
 
 
