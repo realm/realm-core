fd0a47d8ef8628e0e8782c464b471991978b2325
Finn Schiermer Andersen <fsa@tightdb.com>
2014-03-12 16:59:59 +0100

    bugfix: added proper initialization

--- src/tightdb/column_basic_tpl.hpp
+++ src/tightdb/column_basic_tpl.hpp
@@ -247,6 +247,11 @@ public:
     ref_type create_leaf(std::size_t size) TIGHTDB_OVERRIDE
     {
         MemRef mem = BasicArray<T>::create_array(size, m_alloc); // Throws
+        T* tp = reinterpret_cast<T*>(Array::get_data_from_header(mem.m_addr));
+        while (size) {
+            *tp++ = 0.0;
+            --size;
+        }
         return mem.m_ref;
     }
 private:
