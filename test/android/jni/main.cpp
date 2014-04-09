ac6ef7bdc8198549c155309a02d3a5b7c1acf5cd
Emanuele Zattin <ez@tightdb.com>
2014-04-09 13:00:52 +0200

    Fix indentation

--- test/android/jni/main.cpp
+++ test/android/jni/main.cpp
@@ -18,55 +18,55 @@
 
 void android_main(struct android_app* state) {
 
-	// Make sure glue isn't stripped.
-	app_dummy();
+    // Make sure glue isn't stripped.
+    app_dummy();
 
-	ANativeActivity* nativeActivity = state->activity;
-	const char* externalDataPath = nativeActivity->externalDataPath;
-	const char* internalDataPath = nativeActivity->internalDataPath;
-	std::string inDataPath(internalDataPath);
-	std::string exDataPath(externalDataPath);
+    ANativeActivity* nativeActivity = state->activity;
+    const char* externalDataPath = nativeActivity->externalDataPath;
+    const char* internalDataPath = nativeActivity->internalDataPath;
+    std::string inDataPath(internalDataPath);
+    std::string exDataPath(externalDataPath);
 
-	LOGI("Copying asset files...");
-	const int buffer_size = 255;
-	AAssetManager* assetManager = state->activity->assetManager;
-	AAssetDir* assetDir = AAssetManager_openDir(assetManager, "");
+    LOGI("Copying asset files...");
+    const int buffer_size = 255;
+    AAssetManager* assetManager = state->activity->assetManager;
+    AAssetDir* assetDir = AAssetManager_openDir(assetManager, "");
     const char* filename = (const char*)NULL;
     while ((filename = AAssetDir_getNextFileName(assetDir)) != NULL) {
-    	LOGI("Asset file: %s", filename);
-		AAsset* asset = AAssetManager_open(assetManager, filename, AASSET_MODE_STREAMING);
-		char buf[buffer_size];
-		int nb_read = 0;
-		std::string filename_str(filename);
-		std::string filepath = inDataPath + "/" + filename_str;
-		FILE* out = fopen(filepath.c_str(), "w");
-		while ((nb_read = AAsset_read(asset, buf, buffer_size)) > 0)
-		    fwrite(buf, nb_read, 1, out);
-		fclose(out);
-		AAsset_close(asset);
+        LOGI("Asset file: %s", filename);
+        AAsset* asset = AAssetManager_open(assetManager, filename, AASSET_MODE_STREAMING);
+        char buf[buffer_size];
+        int nb_read = 0;
+        std::string filename_str(filename);
+        std::string filepath = inDataPath + "/" + filename_str;
+        FILE* out = fopen(filepath.c_str(), "w");
+        while ((nb_read = AAsset_read(asset, buf, buffer_size)) > 0)
+            fwrite(buf, nb_read, 1, out);
+        fclose(out);
+        AAsset_close(asset);
     }
     AAssetDir_close(assetDir);
     LOGI("Copying of asset files completed");
-	
-	tightdb::test_util::PlatformConfig* platform_config = tightdb::test_util::PlatformConfig::Instance();
-	platform_config->set_path(inDataPath + "/");
-	platform_config->set_resource_path(inDataPath + "/");
 
-	LOGI("Starting unit tests...");
+    tightdb::test_util::PlatformConfig* platform_config = tightdb::test_util::PlatformConfig::Instance();
+    platform_config->set_path(inDataPath + "/");
+    platform_config->set_resource_path(inDataPath + "/");
 
-	test_all(0,0);
+    LOGI("Starting unit tests...");
 
-	LOGI("Done running unit tests...");
+    test_all(0,0);
 
-	LOGI("Copying the test results in the external storage");
-	std::string source = inDataPath + "/unit-test-report.xml";
-	std::string destination = exDataPath + "/unit-test-report.xml";
-	std::ifstream src(source.c_str(), std::ios::binary);
+    LOGI("Done running unit tests...");
+
+    LOGI("Copying the test results in the external storage");
+    std::string source = inDataPath + "/unit-test-report.xml";
+    std::string destination = exDataPath + "/unit-test-report.xml";
+    std::ifstream src(source.c_str(), std::ios::binary);
     std::ofstream dst(destination.c_str(), std::ios::binary);
-	dst << src.rdbuf();
-	LOGI("The XML file is located in %s", destination.c_str());
+    dst << src.rdbuf();
+    LOGI("The XML file is located in %s", destination.c_str());
 
-	ANativeActivity_finish(nativeActivity);
+    ANativeActivity_finish(nativeActivity);
 
-	return;
+    return;
 }
