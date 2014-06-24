96983cebeceb5aa986fe60ebadc8559bd415eedf
Finn Schiermer Andersen <fsa@tightdb.com>
2014-06-24 16:14:30 +0200

    changed Table::m_cols to std::vector

--- src/tightdb/table.hpp
+++ src/tightdb/table.hpp
@@ -717,11 +717,7 @@ private:
     // and ~Table(), and functiones called on behalf of
     // Group::advance_transact().
     //
-    // FIXME: Change to `std::vector<ColumnBase*> m_col_accessors`. When this is
-    // done, it is expected that it will be able to contain null-entries in
-    // other cases than during Group::advance_transact(), but only after a
-    // memory allocation failure.
-    Array m_cols;
+    std::vector<ColumnBase*> m_cols;
 
     mutable std::size_t m_ref_count;
     mutable const StringIndex* m_search_index;
