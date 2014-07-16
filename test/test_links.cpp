44c63ea5624758bac005fa8b7148e762d144cd1a
Lasse Reinhold <lr@tightdb.com>
2014-07-17 01:15:20 +0200

    lr_linktest: Increased average number of tables created by Links_FuzzyTest from 2 to 10

--- test/test_links.cpp
+++ test/test_links.cpp
@@ -780,10 +780,10 @@ ONLY(Links_FuzzyTest)
         size_t tablecount = 0;
         vector<vector<size_t>> tables;
 
-        for (size_t inner_iter = 0; inner_iter < 20; inner_iter++) {
+        for (size_t inner_iter = 0; inner_iter < 30; inner_iter++) {
             int action = rnd.draw_int_mod(100);
 
-            if (action < 40 && tables.size() > 0) {
+            if (action < 33 && tables.size() > 0) {
                 // create link
                 size_t from = rnd.draw_int_mod(tables.size());
                 size_t to = rnd.draw_int_mod(tables.size());
@@ -795,7 +795,7 @@ ONLY(Links_FuzzyTest)
                 else
                     refs[from]->add_column_link(type_LinkList, "link", *refs[to]);
             }
-            else if (action < 80 && tables.size() > 0) {
+            else if (action < 66 && tables.size() > 0) {
                 // delete link
                 size_t from = rnd.draw_int_mod(tables.size());
 
