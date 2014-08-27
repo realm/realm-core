16cbb2858825f8acca8200b11d97e3851e679eca
Thomas Goyne <tg@realm.io>
2014-08-27 14:09:03 -0700

    Detach table accessors before attaching to a new table

--- src/tightdb/row.cpp
+++ src/tightdb/row.cpp
@@ -17,10 +17,10 @@ void RowBase::attach(Table* table, size_t row_ndx)
 void RowBase::reattach(Table* table, size_t row_ndx)
 {
     if (m_table.get() != table) {
-        if (table)
-            table->register_row_accessor(this); // Throws
         if (m_table)
             m_table->unregister_row_accessor(this);
+        if (table)
+            table->register_row_accessor(this); // Throws
         m_table.reset(table);
     }
     m_row_ndx = row_ndx;
