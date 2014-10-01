8c50a8fe364caf922b4bbcde8d366581e6717804
Thomas Goyne <tg@realm.io>
2014-10-01 15:10:48 -0700

    Print a backtrace before terminating on supported platforms

--- src/tightdb/util/terminate.cpp
+++ src/tightdb/util/terminate.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#ifdef __APPLE__
+#include <execinfo.h>
+#endif
+
 #include <tightdb/util/terminate.hpp>
 
 using namespace std;
@@ -11,6 +15,17 @@ namespace util {
 TIGHTDB_NORETURN void terminate(string message, const char* file, long line) TIGHTDB_NOEXCEPT
 {
     cerr << file << ":" << line << ": " << message << endl;
+
+#ifdef __APPLE__
+    void* callstack[128];
+    int frames = backtrace(callstack, 128);
+    char** strs = backtrace_symbols(callstack, frames);
+    for (int i = 0; i < frames; ++i) {
+        cerr << strs[i] << endl;
+    }
+    free(strs);
+#endif
+
     abort();
 }
 
