4e557f87f7e4889ac2619f9ab26118e570d9c378
Thomas Goyne <tg@realm.io>
2014-11-14 10:31:26 -0800

    Also run unit tests with encryption enabled on PRs

--- build.sh
+++ build.sh
@@ -2717,6 +2717,7 @@ EOF
         TIGHTDB_MAX_BPNODE_SIZE_DEBUG="4" TIGHTDB_ENABLE_ENCRYPTION="yes" sh build.sh config "$WORKSPACE/install" || exit 1
         sh build.sh build-iphone || exit 1
         sh build.sh build-android || exit 1
+        sh build.sh check || exit 1
 
         TIGHTDB_MAX_BPNODE_SIZE_DEBUG="4" sh build.sh config "$WORKSPACE/install" || exit 1
         sh build.sh build-iphone || exit 1
