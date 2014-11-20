9d83520dbf082a252bd65897142e58a5794f267e
Finn Schiermer Andersen <fsa@realm.io>
2014-11-20 18:08:30 +0100

    improved testcase (less errors from valgrind drd)

--- test/test_shared.cpp
+++ test/test_shared.cpp
@@ -1792,25 +1792,39 @@ TEST_IF(Shared_AsyncMultiprocess, allow_async)
 
 static const int num_threads = 3;
 static int shared_state[num_threads];
-
+static Mutex muu;
 void waiter(string path, int i)
 {
     SharedGroup sg(path, true, SharedGroup::durability_Full);
-    shared_state[i] = 1;
+    {
+        LockGuard l(muu);
+        shared_state[i] = 1;
+    }
     sg.wait_for_change();
-    shared_state[i] = 2; // this state should not be observed by the writer
+    {
+        LockGuard l(muu);
+        shared_state[i] = 2; // this state should not be observed by the writer
+    }
     sg.wait_for_change(); // we'll fall right through here, because we haven't advanced our readlock
-    shared_state[i] = 3;
+    {
+        LockGuard l(muu);
+        shared_state[i] = 3;
+    }
     sg.begin_read();
     sg.end_read();
     sg.wait_for_change(); // this time we'll wait because state hasn't advanced since we did.
-    shared_state[i] = 4;
-
+    {
+        LockGuard l(muu);
+        shared_state[i] = 4;
+    }
     // works within a read transaction as well
     sg.begin_read();
     sg.wait_for_change();
     sg.end_read();
-    shared_state[i] = 5;
+    {
+        LockGuard l(muu);
+        shared_state[i] = 5;
+    }
 }
 
 TEST(Shared_WaitForChange)
@@ -1823,27 +1837,32 @@ TEST(Shared_WaitForChange)
     for (int j=0; j < num_threads; j++)
         threads[j].start(bind(&waiter, string(path), j));
     sleep(1);
-    for (int j=0; j < num_threads; j++)
+    for (int j=0; j < num_threads; j++) {
+        LockGuard l(muu);
         CHECK_EQUAL(1, shared_state[j]);
+    }
 
     sg.begin_write();
     sg.commit();
     sleep(1);
-    for (int j=0; j < num_threads; j++)
+    for (int j=0; j < num_threads; j++) {
+        LockGuard l(muu);
         CHECK_EQUAL(3, shared_state[j]);
-
+    }
     sg.begin_write();
     sg.commit();
     sleep(1);
-    for (int j=0; j < num_threads; j++)
+    for (int j=0; j < num_threads; j++) {
+        LockGuard l(muu);
         CHECK_EQUAL(4, shared_state[j]);
-
+    }
     sg.begin_write();
     sg.commit();
     sleep(1);
-    for (int j=0; j < num_threads; j++)
+    for (int j=0; j < num_threads; j++) {
+        LockGuard l(muu);
         CHECK_EQUAL(5, shared_state[j]);
-
+    }
     for (int j=0; j < num_threads; j++)
         threads[j].join();
 }
