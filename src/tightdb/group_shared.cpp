c786c330fcfe72083f825c62cbaeef841947d7ee
Finn Schiermer Andersen <fsa@realm.io>
2014-12-11 16:37:32 +0100

    hotfix

--- src/tightdb/group_shared.cpp
+++ src/tightdb/group_shared.cpp
@@ -672,6 +672,11 @@ void SharedGroup::open(const string& path, bool no_create_file,
                         // the database was written by shared group, so it has versioning info
                         TIGHTDB_ASSERT(top.size() == 7);
                         version = top.get(6) / 2;
+                        // In case this was written by an older version of shared group, it
+                        // will have version 0. Version 0 is not a legal initial version, so
+                        // it has to be set to 1 instead.
+                        if (version == 0)
+                            version = 1;
                     }
                 }
                 else {
