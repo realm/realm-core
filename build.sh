7b4e4c8e8db48f6c2c7570501e4c212c1b9842aa
Kenneth Geisshirt <kg@realm.io>
2015-01-05 11:28:04 +0100

    Adding support for encryption when building using Jenkins.

--- build.sh
+++ build.sh
@@ -2764,7 +2764,7 @@ EOF
             echo "Bad check mode '$check_mode'" 1>&2
             exit 1
         fi
-        TIGHTDB_MAX_BPNODE_SIZE_DEBUG="4" sh build.sh config || exit 1
+        TIGHTDB_MAX_BPNODE_SIZE_DEBUG="4" TIGHTDB_ENABLE_ENCRYPTION="$2" sh build.sh config || exit 1
         UNITTEST_SHUFFLE="1" UNITTEST_REANDOM_SEED="random" UNITTEST_XML="1" sh build.sh "$check_mode" || exit 1
         exit 0
         ;;
