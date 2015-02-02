13580d741c16d52a1f0f8d29357e8ae82fedbc1e
Kristian Spangsege <kristian.spangsege@gmail.com>
2015-02-02 11:03:17 +0100

    merge_proto.py: Work with a changeset per history entry, rather than with single operations

--- merge_proto.py
+++ merge_proto.py
@@ -8,135 +8,196 @@ def next_timestamp():
     return current_timestamp
 
 
+def add_changeset(_self, changeset, timestamp, remote_version, remote_peer_id):
+    assert((remote_version == None) == (remote_peer_id == None))
+    new_version = _self['version'] + 1
+    _self['version'] = new_version
+    for operation in changeset:
+        name  = operation['name']
+        index = operation['index']
+        value = operation['value']
+        if name == 'set':
+            _self['list'][index] = value
+        elif name == 'insert':
+            _self['list'].insert(index, value)
+        else:
+            raise Exception("Unknown operation '%s'" % (name))
+    _self['history'].append({
+        'changeset': changeset,
+        'version': new_version,
+        'remote_version': remote_version,
+        'remote_peer_id': remote_peer_id,
+        'timestamp': timestamp
+    })
+
+
+def add_remote_changeset(_self, remote):
+    assert(_self['peer_id'] != remote['peer_id'])
+
+    # Find the last remote version already integrated
+    last_remote_version_integrated = 0
+    for i in range(len(_self['history']), 0, -1):
+        entry = _self['history'][i-1]
+        if entry['remote_peer_id'] == remote['peer_id']:
+            last_remote_version_integrated = entry['remote_version']
+            break
+
+    # Find next remote version to integrate
+    next_remote_version_to_intergate = last_remote_version_integrated + 1
+    while True:
+        remote_entry = remote['history'][next_remote_version_to_intergate-1]
+        if remote_entry['remote_peer_id'] != _self['peer_id']:
+            break
+        next_remote_version_to_intergate = next_remote_version_to_intergate + 1
+
+    # Find the last local version already integrated into the next remote
+    # version to be intergated
+    last_local_version_integrated = 0
+    for i in range(next_remote_version_to_intergate-1, 0, -1):
+        remote_entry_2 = remote['history'][i-1]
+        if remote_entry_2['remote_peer_id'] == _self['peer_id']:
+            last_local_version_integrated = remote_entry_2['remote_version']
+            break
+
+    # Rebuild the operation transformation map
+    last_local_version = _self['version']
+    map = []
+    for version in range(last_local_version_integrated+1, last_local_version+1):
+        entry = _self['history'][version-1]
+        for operation in entry['changeset']:
+            if operation['name'] != 'insert':
+                continue
+            index = operation['index']
+            i = 0
+            while i < len(map):
+                begin, diff, _, _ = map[i]
+                if index < begin + diff:
+                    break
+                i = i + 1
+            num = 1 # Number of inserted list elements
+            if entry['remote_peer_id'] != remote['peer_id']:
+                for j in range(i, len(map)):
+                    map[j][1] = map[j][1] + num
+                diff = map[i-1][1] if i > 0 else 0
+                peer_id = entry['remote_peer_id']
+                if peer_id == None:
+                    peer_id = _self['peer_id']
+                map.insert(i, [index-diff, diff+num, entry['timestamp'], peer_id])
+            else:
+                for j in range(i, len(map)):
+                    map[j][0] = map[j][0] + num
+
+    # Use map to transform incoming operations
+    new_changeset = []
+    remote_entry_peer_id = remote_entry['remote_peer_id']
+    if remote_entry_peer_id == None:
+        remote_entry_peer_id = remote['peer_id']
+    for operation in remote_entry['changeset']:
+        i = len(map)
+        while i > 0:
+            begin, _, _, _ = map[i-1]
+            if operation['index'] >= begin:
+                break
+            i = i - 1
+        if operation['name'] == 'insert':
+            while i > 0:
+                begin, diff, timestamp, peer_id = map[i-1]
+                if operation['index'] > begin:
+                    break
+                if operation['index'] == begin:
+                    if remote_entry['timestamp'] > timestamp:
+                        break
+                    if remote_entry['timestamp'] == timestamp:
+                        if remote_entry_peer_id > peer_id:
+                            break
+                        assert(remote_entry_peer_id < peer_id)
+                i = i - 1
+        diff = map[i-1][1] if i > 0 else 0
+        new_changeset.append({
+            'name':  operation['name'],
+            'index': operation['index'] + diff,
+            'value': operation['value']
+        })
+
+    add_changeset(_self, new_changeset, remote_entry['timestamp'],
+                  remote_entry['version'], remote['peer_id'])
+
+
+def set(peer, index, value, reuse_prev_timestamp = False):
+    if index < 0 or index >= len(peer['list']):
+        raise Exception("Index out of range")
+    changeset = [ { 'name': 'set', 'index': index, 'value': value } ]
+    timestamp = next_timestamp() if not reuse_prev_timestamp else current_timestamp
+    add_changeset(peer, changeset, timestamp, None, None)
+
+def insert(peer, index, value, reuse_prev_timestamp = False):
+    if index < 0 or index > len(peer['list']):
+        raise Exception("Index out of range")
+    changeset = [ { 'name': 'insert', 'index': index, 'value': value } ]
+    timestamp = next_timestamp() if not reuse_prev_timestamp else current_timestamp
+    add_changeset(peer, changeset, timestamp, None, None)
+
+
+
+# Example that sorts a foreign insert in the middle of a sequence of consecutive
+# inserts due to timing
+
+server = { 'peer_id': 0, 'version': 0, 'list': [], 'history': [] }
+client = { 'peer_id': 1, 'version': 0, 'list': [], 'history': [] }
+
+insert(server, index=0, value=100)
+insert(server, index=1, value=101)
+
+insert(client, index=0, value=200)
+
+insert(server, index=2, value=102)
+insert(server, index=3, value=103)
+
+add_remote_changeset(server, client)
+add_remote_changeset(client, server)
+add_remote_changeset(client, server)
+add_remote_changeset(client, server)
+add_remote_changeset(client, server)
+
+print '---'
+print "Server:", server['list']
+print "Client:", client['list']
+
+
+
+# Example that demonstrates that non-end insertions are handled correctly
+# (intuitively)
+
+server = { 'peer_id': 0, 'version': 0, 'list': [], 'history': [] }
+client = { 'peer_id': 1, 'version': 0, 'list': [], 'history': [] }
+
+insert(server, index=0, value=100)
+insert(server, index=0, value=101)
+
+insert(client, index=0, value=200)
+
+insert(server, index=0, value=102)
+insert(server, index=0, value=103)
+
+add_remote_changeset(server, client)
+add_remote_changeset(client, server)
+add_remote_changeset(client, server)
+add_remote_changeset(client, server)
+add_remote_changeset(client, server)
+
+print '---'
+print "Server:", server['list']
+print "Client:", client['list']
+
+
+
+# Example that demonstrates 3-way merge with server in the middle (star-shaped
+# topology)
+
 server   = { 'peer_id': 0, 'version': 0, 'list': [], 'history': [] }
 client_A = { 'peer_id': 1, 'version': 0, 'list': [], 'history': [] }
 client_B = { 'peer_id': 2, 'version': 0, 'list': [], 'history': [] }
 client_C = { 'peer_id': 3, 'version': 0, 'list': [], 'history': [] }
-
-
-def add_operation(peer, operation, index, value, timestamp, remote_version, peer_id):
-    new_version = peer['version'] + 1
-    peer['version'] = new_version
-    if operation == 'set':
-        peer['list'][index] = value
-    elif operation == 'insert':
-        peer['list'].insert(index, value)
-    else:
-        raise Exception("Unknown operation")
-    assert((remote_version == None) == (peer_id == None))
-    if peer_id == None:
-        peer_id = peer['peer_id']
-    peer['history'].append({
-        'operation': operation,
-        'index': index,
-        'value': value,
-        'version': new_version,
-        'remote_version': remote_version,
-        'timestamp': timestamp,
-        'peer_id': peer_id
-    })
-
-
-def sync_one(origin, destination):
-    assert(origin['peer_id'] != destination['peer_id'])
-
-    # Find the last origin version already integrated into the destination
-    last_origin_version_integrated = 0
-    for i in range(len(destination['history']), 0, -1):
-        entry = destination['history'][i-1]
-        if entry['peer_id'] == origin['peer_id']:
-            last_origin_version_integrated = entry['remote_version']
-            break
-
-    # Find next origin version to integrate into the destination
-    next_origin_version_to_intergate = last_origin_version_integrated + 1
-    while True:
-        origin_entry = origin['history'][next_origin_version_to_intergate-1]
-        if origin_entry['peer_id'] != destination['peer_id']:
-            break
-        next_origin_version_to_intergate = next_origin_version_to_intergate + 1
-
-    # Find the last destination version already integrated into the next origin
-    # version to be intergated into the destination
-    last_destination_version_integrated = 0
-    for i in range(next_origin_version_to_intergate-1, 0, -1):
-        entry = origin['history'][i-1]
-        if entry['peer_id'] == destination['peer_id']:
-            last_destination_version_integrated = entry['remote_version']
-            break
-
-    # Rebuild the operation transformation map
-    last_destination_version = destination['version']
-    map = []
-    for ver in range(last_destination_version_integrated, last_destination_version):
-        produced_ver = ver + 1
-        entry = destination['history'][produced_ver-1]
-        if entry['operation'] != 'insert':
-            continue
-        index = entry['index']
-        i = 0
-        while i < len(map):
-            begin, diff, _, _ = map[i]
-            if index < begin + diff:
-                break
-            i = i + 1
-        num = 1 # Number of inserted list elements
-        is_foreign_to_origin = (entry['peer_id'] != origin['peer_id'])
-        if is_foreign_to_origin:
-            for j in range(i, len(map)):
-                map[j][1] = map[j][1] + num
-            diff = map[i-1][1] if i > 0 else 0
-            map.insert(i, [index-diff, diff+num, entry['timestamp'], entry['peer_id']])
-        else:
-            for j in range(i, len(map)):
-                map[j][0] = map[j][0] + num
-
-    # Use map to transform incoming operation
-    i = len(map)
-    while i > 0:
-        begin, _, _, _ = map[i-1]
-        if origin_entry['index'] >= begin:
-            break
-        i = i - 1
-    if origin_entry['operation'] == 'insert':
-        while i > 0:
-            begin, diff, timestamp, peer_id = map[i-1]
-            if origin_entry['index'] > begin:
-                break
-            if origin_entry['index'] == begin:
-                if origin_entry['timestamp'] > timestamp:
-                    break
-                if origin_entry['timestamp'] == timestamp:
-                    if origin_entry['peer_id'] > peer_id:
-                        break
-                    assert(origin_entry['peer_id'] < peer_id)
-            i = i - 1
-    diff = map[i-1][1] if i > 0 else 0
-
-    add_operation(destination,
-                  origin_entry['operation'],
-                  origin_entry['index'] + diff,
-                  origin_entry['value'],
-                  origin_entry['timestamp'],
-                  origin_entry['version'],
-                  origin['peer_id'])
-
-
-def set(peer, index, value, timestamp = None):
-    if index < 0 or index >= len(peer['list']):
-        raise Exception("Index out of range")
-    if timestamp == None:
-        timestamp = next_timestamp()
-    add_operation(peer, 'set', index, value, timestamp, None, None)
-
-def insert(peer, index, value, timestamp = None):
-    if index < 0 or index > len(peer['list']):
-        raise Exception("Index out of range")
-    if timestamp == None:
-        timestamp = next_timestamp()
-    add_operation(peer, 'insert', index, value, timestamp, None, None)
-
-
 
 insert(client_A, index=0, value=100)
 insert(client_B, index=0, value=200)
@@ -144,55 +205,39 @@ insert(client_B, index=0, value=200)
 insert(client_A, index=1, value=101)
 insert(client_B, index=1, value=201)
 
-sync_one(client_A, server)
-sync_one(server, client_B)
-sync_one(server, client_C)
+add_remote_changeset(server, client_A)
+
+add_remote_changeset(client_B, server)
+add_remote_changeset(client_C, server)
 
 insert(client_C, index=0, value=300)
 
-sync_one(client_A, server)
-sync_one(server, client_B)
+add_remote_changeset(server, client_A)
+add_remote_changeset(client_B, server)
 
-sync_one(client_B, server)
-sync_one(server, client_A)
+add_remote_changeset(server, client_B)
+add_remote_changeset(client_A, server)
 
-sync_one(client_C, server)
-sync_one(server, client_A)
-sync_one(server, client_B)
+add_remote_changeset(server, client_C)
+add_remote_changeset(client_A, server)
+add_remote_changeset(client_B, server)
 
 insert(client_A, index=3, value=102)
-sync_one(client_A, server)
-sync_one(server, client_B)
+add_remote_changeset(server, client_A)
+add_remote_changeset(client_B, server)
 
-sync_one(server, client_C)
-sync_one(server, client_C)
+add_remote_changeset(client_C, server)
+add_remote_changeset(client_C, server)
 
-sync_one(client_B, server)
-sync_one(server, client_A)
+add_remote_changeset(server, client_B)
+add_remote_changeset(client_A, server)
 
-sync_one(server, client_C)
-sync_one(server, client_C)
+add_remote_changeset(client_C, server)
+add_remote_changeset(client_C, server)
 
+print '---'
+print "Server:  ",   server['list']
+print "Client-A:", client_A['list']
+print "Client-B:", client_B['list']
+print "Client-C:", client_C['list']
 
-#insert(server, index=0, value=100, timestamp=1)
-#insert(server, index=0, value=101, timestamp=3)
-#insert(server, index=0, value=102, timestamp=5)
-#insert(server, index=3, value=103, timestamp=7)
-
-#insert(client, index=0, value=200, timestamp=4)
-
-#sync_one(client, server)
-#sync_one(server, client)
-#sync_one(server, client)
-#sync_one(server, client)
-#sync_one(server, client)
-
-
-
-print "S:",   server['list']
-print "A:", client_A['list']
-print "B:", client_B['list']
-print "C:", client_C['list']
-
-
-# Deletions
