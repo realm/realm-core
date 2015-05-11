852d6c7347ccb467728b091610927935c1c5bc36
Simon Ask Ulsnes <su@realm.io>
2015-05-11 17:34:55 +0200

    Run unit tests on iOS.

---
+++ test/unit-tests-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  unit-tests-ios
+//
+//  Created by Simon Ask Ulsnes on 11/05/15.
+//  Copyright (c) 2015 Realm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/unit-tests-ios/ViewController.mm
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  unit-tests-ios
+//
+//  Created by Simon Ask Ulsnes on 11/05/15.
+//  Copyright (c) 2015 Realm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#include "test_path.hpp"
+#include "test_all.hpp"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSURL* tmpUrl = [[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask].firstObject;
+    NSString* tmpDir = tmpUrl.path;
+    std::string tmp_dir{tmpDir.UTF8String, tmpDir.length};
+    realm::test_util::set_test_path_prefix(tmp_dir + '/');
+    test_all(0, NULL);
+    exit(0);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
