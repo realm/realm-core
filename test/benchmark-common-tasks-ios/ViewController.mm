55430c939b8288723f7513ecb6f5af54f45aa698
Simon Ask Ulsnes <su@realm.io>
2015-05-12 13:04:58 +0200

    Remove unnecessary trailing slash.

--- test/benchmark-common-tasks-ios/ViewController.mm
+++ test/benchmark-common-tasks-ios/ViewController.mm
@@ -15,7 +15,7 @@ extern "C" int benchmark_common_tasks_main(int, const char**);
     
     NSString* tmpDir = NSTemporaryDirectory();
     std::string tmp_dir{tmpDir.UTF8String, tmpDir.length};
-    realm::test_util::set_test_path_prefix(tmp_dir + '/');
+    realm::test_util::set_test_path_prefix(tmp_dir);
     
     [[NSProcessInfo processInfo] performActivityWithOptions:NSActivityLatencyCritical reason:@"benchmark-common-tasks" usingBlock:^{
         benchmark_common_tasks_main(0, NULL);
--- test/unit-tests-ios/ViewController.mm
+++ test/unit-tests-ios/ViewController.mm
@@ -13,7 +13,7 @@
     [super viewDidLoad];
     NSString* tmpDir = NSTemporaryDirectory();
     std::string tmp_dir{tmpDir.UTF8String, tmpDir.length};
-    realm::test_util::set_test_path_prefix(tmp_dir + '/');
+    realm::test_util::set_test_path_prefix(tmp_dir);
     test_all(0, NULL);
     exit(0);
 }
