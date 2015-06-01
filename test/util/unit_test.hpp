7c7b721b3385336fe0f3cf65e1bb1751d7c6c9ea
Kristian Spangsege <kristian.spangsege@gmail.com>
2015-06-01 17:57:25 +0200

    Move TEST_TYPES macro to test_types.hpp (cast it as a plugin)

--- test/util/unit_test.hpp
+++ test/util/unit_test.hpp
@@ -33,8 +33,6 @@
 #include <realm/util/safe_int_ops.hpp>
 #include <realm/util/bind_ptr.hpp>
 
-#include "demangle.hpp"
-
 
 #define TEST(name) TEST_IF(name, true)
 
@@ -58,23 +56,6 @@
         realm_unit_test_reg__##name((list), realm_unit_test__##name, \
                                       "DefaultSuite", #name, __FILE__, __LINE__); \
     void Realm_UnitTest__##name::test_run()
-
-#define TEST_TYPES(name, ...) \
-    TEST_TYPES_IF(name, true, __VA_ARGS__)
-
-#define TEST_TYPES_IF(name, enabled, ...) \
-    TEST_TYPES_EX(name, realm::test_util::unit_test::get_default_test_list(), enabled, __VA_ARGS__)
-
-#define TEST_TYPES_EX(name, list, enabled, ...) \
-    template<class> struct Realm_UnitTest__##name: realm::test_util::unit_test::Test { \
-        bool test_enabled() const { return bool(enabled); } \
-        void test_run();                                    \
-    }; \
-    realm::test_util::unit_test::TestCons<Realm_UnitTest__##name, __VA_ARGS__> realm_unit_test__##name; \
-    realm::test_util::unit_test::RegisterTest                        \
-        realm_unit_test_reg__##name((list), realm_unit_test__##name, \
-                                      "DefaultSuite", #name, __FILE__, __LINE__); \
-    template<class TEST_TYPE> void Realm_UnitTest__##name<TEST_TYPE>::test_run()
 
 
 #define CHECK(cond) \
@@ -186,17 +167,6 @@
 namespace realm {
 namespace test_util {
 namespace unit_test {
-
-
-template<template<class> class Test, class...> class TestCons;
-
-template<template<class> class Test, class Type, class... Types>
-class TestCons<Test, Type, Types...> {
-public:
-    Test<Type> head;
-    TestCons<Test, Types...> tail;
-};
-template<template<class> class Test> class TestCons<Test> {};
 
 
 class Test;
@@ -468,32 +438,10 @@ struct RegisterTest {
     {
         register_test(list, test, suite, name, file, line);
     }
-    template<template<class> class Test, class... Types>
-    RegisterTest(TestList& list, TestCons<Test, Types...>& tests, const char* suite,
-                 const char* name, const char* file, long line)
-    {
-        register_tests(list, tests, suite, name, file, line);
-    }
     static void register_test(TestList& list, Test& test, const char* suite,
                               const std::string& name, const char* file, long line)
     {
         list.add(test, suite, name, file, line);
-    }
-    template<template<class> class Test, class Type, class... Types>
-    static void register_tests(TestList& list, TestCons<Test, Type, Types...>& tests,
-                               const char* suite, const char* name, const char* file, long line)
-    {
-        std::string name_2 = name;
-        name_2 += '<';
-        name_2 += get_type_name<Type>();
-        name_2 += '>';
-        register_test(list, tests.head, suite, name_2, file, line);
-        register_tests(list, tests.tail, suite, name, file, line);
-    }
-    template<template<class> class Test>
-    static void register_tests(TestList&, TestCons<Test>&,
-                               const char*, const char*, const char*, long)
-    {
     }
 };
 
