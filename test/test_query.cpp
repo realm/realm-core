033f6267dfb9ac69808f91bbecd330098cd81296
Thomas Goyne <tg@realm.io>
2015-06-16 09:49:30 -0700

    Test the other comparison operators for good measure

--- test/test_query.cpp
+++ test/test_query.cpp
@@ -6059,7 +6059,7 @@ TEST(Query_BinaryNull)
 
 #endif
 
-TEST(Query_LongLongLessThan)
+TEST(Query_64BitValues)
 {
     Group g;
     TableRef table = g.add_table("table");
@@ -6067,13 +6067,35 @@ TEST(Query_LongLongLessThan)
 
     const int64_t start = 4485019129LL;
     const int64_t count = 20; // First 16 SSE-searched, four fallback
+    const int64_t min = std::numeric_limits<int64_t>::min();
+    const int64_t max = std::numeric_limits<int64_t>::max();
     table->add_empty_row(count);
     for (int64_t i = 0; i < count; ++i)
         table->set_int(0, i, start + i);
 
+    CHECK_EQUAL(0, table->where().less(0, min).count());
     CHECK_EQUAL(0, table->where().less(0, start).count());
     CHECK_EQUAL(1, table->where().less(0, start + 1).count());
     CHECK_EQUAL(count, table->where().less(0, start + count).count());
+    CHECK_EQUAL(count, table->where().less(0, max).count());
+
+    CHECK_EQUAL(0, table->where().less_equal(0, min).count());
+    CHECK_EQUAL(1, table->where().less_equal(0, start).count());
+    CHECK_EQUAL(count, table->where().less_equal(0, start + count).count());
+    CHECK_EQUAL(count, table->where().less_equal(0, max).count());
+
+    CHECK_EQUAL(count, table->where().greater(0, min).count());
+    CHECK_EQUAL(count - 1, table->where().greater(0, start).count());
+    CHECK_EQUAL(1, table->where().greater(0, start + count - 2).count());
+    CHECK_EQUAL(0, table->where().greater(0, start + count - 1).count());
+    CHECK_EQUAL(0, table->where().greater(0, max).count());
+
+    CHECK_EQUAL(count, table->where().greater_equal(0, min).count());
+    CHECK_EQUAL(count, table->where().greater_equal(0, start).count());
+    CHECK_EQUAL(count - 1, table->where().greater_equal(0, start + 1).count());
+    CHECK_EQUAL(1, table->where().greater_equal(0, start + count - 1).count());
+    CHECK_EQUAL(0, table->where().greater_equal(0, start + count).count());
+    CHECK_EQUAL(0, table->where().greater_equal(0, max).count());
 }
 
 #endif // TEST_QUERY
