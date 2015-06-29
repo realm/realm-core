4ca26ca6003994191f833157b9703d7b46fda4f7
Finn Schiermer Andersen <fsa@realm.io>
2015-06-29 13:42:41 +0200

    compile error on some platforms

--- src/realm/util/file_mapper.cpp
+++ src/realm/util/file_mapper.cpp
@@ -702,7 +702,7 @@ void* mremap(int fd, void* old_addr, size_t old_size, File::AccessMode a, size_t
     int err = errno; // Eliminate any risk of clobbering
     throw std::runtime_error(get_errno_msg("mremap(): failed: ", err));
 #else
-    void* new_addr = mmap(fd, new_size, a, nullptr);
+    void* new_addr = mmap(fd, new_size, a, nullptr, 0);
     if(::munmap(old_addr, old_size) != 0) {
         int err = errno;
         throw std::runtime_error(get_errno_msg("munmap() failed: ", err));
