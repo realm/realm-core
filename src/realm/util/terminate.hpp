4d33b4666d9ba8274d6aabc8ac153d28e4b7574e
Sebastian Lauwers <sl@realm.io>
2015-09-01 12:49:55 +0200

    Added REALM_VER_CHUNK to terminate()

--- src/realm/util/terminate.hpp
+++ src/realm/util/terminate.hpp
@@ -25,6 +25,7 @@
 #include <string>
 
 #include <realm/util/features.h>
+#include <realm/version.hpp>
 
 #define REALM_TERMINATE(msg) realm::util::terminate((msg), __FILE__, __LINE__)
 
@@ -34,28 +35,28 @@ REALM_NORETURN void terminate_internal(std::stringstream&) noexcept;
 
 REALM_NORETURN inline void terminate(const char* message, const char* file, long line) noexcept {
     std::stringstream ss;
-    ss << file << ":" << line << ": " << message << "\n";
+    ss << file << ":" << line << ": " REALM_VER_CHUNK " " << message << "\n";
     terminate_internal(ss);
 }
 
 template <typename T1, typename T2>
 REALM_NORETURN void terminate(const char* message, const char* file, long line, T1 info1, T2 info2) noexcept {
     std::stringstream ss;
-    ss << file << ":" << line << ": " << message << " [" << info1 << ", " << info2 << "]\n";
+    ss << file << ":" << line << ": " REALM_VER_CHUNK " " << message << " [" << info1 << ", " << info2 << "]\n";
     terminate_internal(ss);
 }
 
 template <typename T1, typename T2, typename T3, typename T4>
 REALM_NORETURN void terminate(const char* message, const char* file, long line, T1 info1, T2 info2, T3 info3, T4 info4) noexcept {
     std::stringstream ss;
-    ss << file << ":" << line << ": " << message << " [" << info1 << ", " << info2 << ", " << info3 << ", " << info4 << "]\n";
+    ss << file << ":" << line << ": " REALM_VER_CHUNK " " << message << " [" << info1 << ", " << info2 << ", " << info3 << ", " << info4 << "]\n";
     terminate_internal(ss);
 }
 
 template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
 REALM_NORETURN void terminate(const char* message, const char* file, long line, T1 info1, T2 info2, T3 info3, T4 info4, T5 info5, T6 info6) noexcept {
     std::stringstream ss;
-    ss << file << ":" << line << ": " << message << " [" << info1 << ", " << info2 << ", " << info3 << ", " << info4 << ", " << info5 << ", " << info6 << "]\n";
+    ss << file << ":" << line << ": " REALM_VER_CHUNK " " << message << " [" << info1 << ", " << info2 << ", " << info3 << ", " << info4 << ", " << info5 << ", " << info6 << "]\n";
     terminate_internal(ss);
 }
 
