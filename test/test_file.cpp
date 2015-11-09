5ed8f1a837cabc293b19e5876ce219dafce58675
Daniel Povlsen <daniel.povlsen@gmail.com>
2015-11-09 13:29:55 +0100

    Removed reference to fixed bug

--- test/test_file.cpp
+++ test/test_file.cpp
@@ -249,7 +249,6 @@ TEST(File_SetEncryptionKey)
 #if REALM_ENABLE_ENCRYPTION
     f.set_encryption_key(key); // should not throw
 #else
-    // FIXME: https://ci.realm.io/job/core_android/784/testReport/(root)/DefaultSuite/File_SetEncryptionKey/
     CHECK_THROW(f.set_encryption_key(key), std::runtime_error);
 #endif
 }
