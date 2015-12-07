497268caaff1e559334e480cd9f8d787a374df36
Lasse Reinhold <lr@realm.io>
2015-12-07 12:34:57 +0100

    Update unit_test.cpp

--- test/util/unit_test.cpp
+++ test/util/unit_test.cpp
@@ -244,7 +244,7 @@ class TestList::ExecContext {
 public:
     SharedContext* m_shared;
     Mutex m_mutex;
-	std::atomic<long long> m_num_checks;
+    std::atomic<long long> m_num_checks;
     long long m_num_failed_checks;
     long m_num_failed_tests;
     bool m_errors_seen;
