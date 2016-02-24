c62f7dac0bf882c346d4ce5a84f52513c41cf109
Daniel Povlsen <daniel.povlsen@gmail.com>
2016-02-24 13:12:53 +0100

    Initial column_datetime

---
+++ src/realm/column_datetime.hpp
@@ -0,0 +1,31 @@
+/*************************************************************************
+ *
+ * REALM CONFIDENTIAL
+ * __________________
+ *
+ *  [2011] - [2015] Realm Inc
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Realm Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Realm Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Realm Incorporated.
+ *
+ **************************************************************************/
+#ifndef REALM_COLUMN_DATETIME_HPP
+#define REALM_COLUMN_DATETIME_HPP
+
+namespace realm {
+
+class DateTimeColumn {
+public:
+};
+
+} // namespace realm
+
+#endif // REALM_COLUMN_DATETIME_HPP
