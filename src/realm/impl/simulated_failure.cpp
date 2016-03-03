95f4f4909b41dc2c0572f458ba6ace5aeceef769
Simon Ask Ulsnes <su@realm.io>
2016-03-03 13:26:03 +0100

    Fixed compilation in release mode.

--- src/realm/impl/simulated_failure.cpp
+++ src/realm/impl/simulated_failure.cpp
@@ -2,10 +2,14 @@
 
 using namespace realm::_impl;
 
+#if REALM_DEBUG
+
 void SimulatedFailure::do_fail(type event, void* userdata)
 {
     static_cast<void>(userdata);
     unprime(event);
     throw SimulatedFailure();
 }
+
+#endif // REALM_DEBUG
 
