b7e30b43e8ec470870e1619a7c3c1394e0adea29
Daniel Povlsen <daniel.povlsen@gmail.com>
2016-04-26 14:15:43 +0200

    Forgot to use the introduced `nullable` (TEST_TYPE).

--- test/test_column_timestamp.cpp
+++ test/test_column_timestamp.cpp
@@ -92,7 +92,7 @@ TEST_TYPES(TimestampColumn_Compare, std::true_type, std::false_type)
     CHECK(c.compare(c));
 
     {
-        ref_type ref = TimestampColumn::create(Allocator::get_default(), 0, true /*nullable*/);
+        ref_type ref = TimestampColumn::create(Allocator::get_default(), 0, nullable);
         TimestampColumn c2(Allocator::get_default(), ref);
         CHECK_NOT(c.compare(c2));
         c2.destroy();
