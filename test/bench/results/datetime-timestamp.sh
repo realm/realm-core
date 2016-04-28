4f1c247d59c4a363c20fc18238046312ca380fd8
Oleksandr Shturmov <oleks@oleks.info>
2016-04-28 16:26:21 +0200

    A simple script to compare datetime and timestamp

---
+++ test/bench/results/datetime-timestamp.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+rm -rf results.*
+cat <<EOF
+##############
+## Baseline ##
+##############
+EOF
+../datetime/_build/benchmark # Baseline
+
+cat <<EOF
+##############
+## Datetime ##
+##############
+EOF
+../datetime/_build/benchmark
+
+cat <<EOF
+###############
+## Timestamp ##
+###############
+EOF
+../timestamp/_build/benchmark
