6dde107c8d7b16e99ceb1287d26eac374cda8437
Daniel Povlsen <daniel.povlsen@gmail.com>
2016-05-10 15:50:01 +0200

    Initial version of "kill AFL instances and convert findings to cpp"-script.

---
+++ test/fuzzy/kill_parallel_fuzzer.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+if [ "$#" -ne 1 ]; then
+    echo "Usage sh $0 executable_path (e.g. ./fuzz-group-dbg)"
+    exit 1
+fi
+EXECUTABLE_PATH=$1
+UNIT_TESTS_PATH="findings/generated_unit_tests/"
+
+# Kill all running fuzzers
+pkill afl-fuzz
+
+mkdir -p $UNIT_TESTS_PATH
+
+# Find all interesting cases
+files=`find findings \( -path "*hang/id:s*" -or -path "*crashes/id:*" \)`
+
+# Run executable for each and capture the output
+for file in $files
+do
+    cppfile=$UNIT_TESTS_PATH$(basename $file).cpp
+    $EXECUTABLE_PATH $file --log > $cppfile
+done
