f35d00c0ca69aa175cd219b49944bd11015602e9
Finn Schiermer Andersen <fsa@realm.io>
2016-05-19 14:03:53 +0200

    fix: failure during open could yield stale mmaps

--- src/realm/group_shared.cpp
+++ src/realm/group_shared.cpp
@@ -949,6 +949,12 @@ void SharedGroup::do_open(const std::string& path, bool no_create_file, Durabili
             catch (SlabAlloc::Retry&) {
                 continue;
             }
+            // If we fail in any way, we must detach the allocator. Failure to do so
+            // will retain memory mappings in the mmap cache shared between allocators.
+            // This would allow other SharedGroups to reuse the mappings even in
+            // situations, where the database has been re-initialised (e.g. through
+            // compact()). This could render the mappings (partially) undefined.
+            SlabAlloc::DetachGuard alloc_detach_guard(alloc);
 
             // Determine target file format version for session (upgrade
             // required if greater than file format version of attached file).
@@ -1082,6 +1088,7 @@ void SharedGroup::do_open(const std::string& path, bool no_create_file, Durabili
             m_wait_for_change_enabled = true;
 
             // Keep the mappings and file open:
+            alloc_detach_guard.release();
             fug_2.release(); // Do not unmap
             fug_1.release(); // Do not unmap
             fcg.release(); // Do not close
