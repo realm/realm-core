47e0502cabc62cd248d739962afb962203e378fe
Finn Schiermer Andersen <fsa@realm.io>
2016-05-25 15:06:41 +0200

    experiment: enabling emulation on all platforms

--- src/realm/util/interprocess_mutex.hpp
+++ src/realm/util/interprocess_mutex.hpp
@@ -22,9 +22,11 @@
 #define REALM_UTIL_INTERPROCESS_MUTEX
 
 // Enable this only on platforms where it might be needed
-#if REALM_PLATFORM_APPLE || REALM_PLATFORM_ANDROID
+//#if REALM_PLATFORM_APPLE || REALM_ANDROID
+// FIXME: Just testing if unittest suite will run with emulation
+// on all platforms..
 #define REALM_ROBUST_MUTEX_EMULATION
-#endif
+//#endif
 
 #include <realm/util/features.h>
 #include <realm/util/thread.hpp>
