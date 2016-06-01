7df92fb392628665be8285f04670059c761ba8f7
Oleksandr Shturmov <oleks@oleks.info>
2016-06-01 10:58:28 +0200

    Put the documentation under --help

--- test/util/build-core.sh
+++ test/util/build-core.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
-
-# build-core.sh
 #
-# This script builds a given version of core in a dedicated directory of core
-# builds under the current working directory. This enables, for instance,
-# comparing the performance of various of versions of core on the same device.
-#
-# Examples:
-#
-# $ ./util/build-core.sh master # master assumed by default.
-# $ ./util/build-core.sh tags/v0.97.3
-# $ ./util/build-core.sh ea310804
-# $ ./util/build-core.sh 32b3b79d2ab90e784ad5f14f201d682be9746781
-#
-# This results in directories:
-#
-# $ ./core-builds/master
-# $ ./core-builds/tags/v0.97.3
-# $ ./core-builds/ea310804
-# $ ./core-builds/32b3b79d2ab90e784ad5f14f201d682be9746781
+# See ./util/build-core.sh --help for documentation.
 
 set -euo pipefail
 
@@ -35,12 +17,25 @@ showHelp () {
   showUsage
   echo ""
   cat <<EOF
-Builds core at given <branch>, <commit>, or <tag> under
-${builddir} in the current working directory.
+./util/build-core.sh
 
-By default, the master branch gets built.
-The commit can be the 7-character commit ID.
-A tag must begin with tags/ (e.g., tags/v0.97.3).
+This script builds the given version of core (branch, commit, or tag) in a
+dedicated ${builddir} directory. This enables, for instance, comparing the
+performance of various of versions of core on the same machine.
+
+Examples:
+
+$ ./util/build-core.sh master # master assumed by default.
+$ ./util/build-core.sh tags/v0.97.3 # Tags must be prefixed with "tag/".
+$ ./util/build-core.sh ea310804 # Can be a short commit ID.
+$ ./util/build-core.sh 32b3b79d2ab90e784ad5f14f201d682be9746781
+
+This results in directories:
+
+$ ./core-builds/master
+$ ./core-builds/tags/v0.97.3
+$ ./core-builds/ea310804
+$ ./core-builds/32b3b79d2ab90e784ad5f14f201d682be9746781
 EOF
 }
 
