b48b992210783dad88acd457d67d8bcbf227b2e9
Kenneth Geisshirt <kg@realm.io>
2016-06-23 14:14:49 +0200

    makedepend interprets -mandroid as -m which leads to many warnings

--- build.sh
+++ build.sh
@@ -941,7 +941,11 @@ EOF
                     $MAKE clean
                 ) || exit 1
 
-                $MAKE -C "openssl" DEPFLAG="-I$temp_dir/sysroot/usr/include -I$temp_dir/sysroot/usr/include/linux -I$temp_dir/include/c++/4.9/tr1 -I$temp_dir/include/c++/4.9" depend || exit 1
+                # makedepend interpret -mandroid as -m
+                (cd openssl && cp Makefile Makefile.dep && sed -i '' -e 's/\-mandroid//g' Makefile) || exit 1
+                DEPFLAGS="$(grep DEPFLAG= Makefile | head -1 | cut -f2 -d=)"
+                $MAKE -C "openssl" DEPFLAG="$DEPFLAGS -I$temp_dir/sysroot/usr/include -I$temp_dir/sysroot/usr/include/linux -I$temp_dir/include/c++/4.9/tr1 -I$temp_dir/include/c++/4.9" depend || exit 1
+                (cd openssl && mv Makefile.dep Makefile) || exit 1
                 PATH="$path" CC="$cc" CFLAGS="$cflags_arch" PERL="perl" $MAKE -C "openssl" build_crypto || exit 1
                 cp "openssl/libcrypto.a" "$ANDROID_DIR/$libcrypto_name" || exit 1
             fi
