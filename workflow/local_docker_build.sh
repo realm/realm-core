53e81762cd6c1b681cafe516241679211ef0b209
Adam Lebsack <ajl@realm.io>
2016-11-10 21:50:14 +0100

    Address PR comments

--- workflow/local_docker_build.sh
+++ workflow/local_docker_build.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 # This script can be used to locally check and debug
 # the linux build process outside of CI.
-# This should be run from the root directory `packaging/manual_docker_build.sh`
+# This should be run from the root directory `packaging/local_docker_build.sh`
 
 set -e
+
 rm -rf CMakeCache.txt
-find . -name CMakeFiles -delete
+find . -name CMakeFiles -print0 | xargs -0 rm -rf
 
 docker build -t ci/realm-object-server:build .
 
-docker run -it --rm -v $(pwd):/source -w /source ci/realm-object-server:build ./workflow/test_coverage.sh
-
+docker run --rm \
+  -u $(id -u) \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v $(pwd):/source \
+  -w /source \
+  ci/realm-object-server:build \
+  ./workflow/test_coverage.sh
