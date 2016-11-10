f6df42a032c4148aac2e8436e6d7f39f2a9d4ef8
Adam Lebsack <ajl@realm.io>
2016-11-10 21:49:13 +0100

    Added processor count detection on linux

--- workflow/test_coverage.sh
+++ workflow/test_coverage.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-cmake -DCMAKE_BUILD_TYPE=Coverage .
-# sh realm-sync/build.sh config -DREALM_SYNC_PREFIX=/source/realm-sync/
-make VERBOSE=1 -j2 generate-coverage-cobertura
+nprocs=4
+if [ "$(uname)" = "Linux" ]; then
+  nprocs=$(grep -c ^processor /proc/cpuinfo)
+fi
+
+cmake -DCMAKE_BUILD_TYPE=Coverage . && \
+make VERBOSE=1 -j${nprocs} generate-coverage-cobertura
