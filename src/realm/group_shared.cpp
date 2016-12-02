b611ef7da84cfb839bede328334ad018c4dee763
Finn Schiermer Andersen <fsa@realm.io>
2016-12-02 17:36:30 +0100

    removed a debug print

--- src/realm/group_shared.cpp
+++ src/realm/group_shared.cpp
@@ -1861,7 +1861,6 @@ void SharedGroup::low_level_commit(uint_fast64_t new_version)
     ref_type new_top_ref = out.write_group(); // Throws
     m_free_space = out.get_free_space();
     m_used_space = out.get_file_size() - m_free_space;
-    std::cout << "Free/Used : " << m_free_space << " / " << m_used_space << std::endl;
     // std::cout << "Writing version " << new_version << ", Topptr " << new_top_ref
     //     << " Read lock at version " << oldest_version << std::endl;
     switch (Durability(info->durability)) {
