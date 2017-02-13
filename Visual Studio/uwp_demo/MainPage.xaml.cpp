64e550a009854c8da4269bac4b19f6ab510b72bb
Lasse Reinhold <lr@realm.io>
2017-02-13 12:53:16 +0100

    Lr/uwp arm (#2412)
    * Couple of UWP issues
    
    * * Removed need for _SCL_SECURE_NO_WARNINGS flag in UWP
    * Fixed more project settings (core used debug runtime lib in release mode)
    
    * Removed unneeded directory in sample UWP app
    
    * Windows 10 UWP compiler errors and warnings
    
    * Warning and compile error on linux
    
    * Typo
    
    * to_size_t()
    
    * removed cast
    
    * tab vs space
    
    * Update utilities.hpp
    
    * Fixed typo and explanation in source code
    
    * Simplification
    
    * bad to_size_t
    
    * removed unused non-const method
    
    * Fixed theoretical flaw
    
    * mem leak
    
    * unique_ptr instead
    
    * const warning
    
    * Update file.cpp
    
    * tabs
    
    * ARM support for UWP
    
    * bad #include on ARM
    
    * UWP compiler warning

--- Visual Studio/uwp_demo/MainPage.xaml.cpp
+++ Visual Studio/uwp_demo/MainPage.xaml.cpp
@@ -5,8 +5,9 @@
 
 #include "pch.h"
 #include "MainPage.xaml.h"
-
+#include "realm.hpp"
 using namespace App1;
+using namespace realm;
 
 using namespace Platform;
 using namespace Windows::Foundation;
@@ -19,9 +20,41 @@ using namespace Windows::UI::Xaml::Input;
 using namespace Windows::UI::Xaml::Media;
 using namespace Windows::UI::Xaml::Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// For ARM, only Release mode is currently configured. Build Core in ARM Release mode too.
+
+Table* tbl = nullptr;
 
 MainPage::MainPage()
 {
 	InitializeComponent();
+}
+
+void App1::MainPage::Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+    if(!tbl) {
+        tbl = new Table();
+        tbl->add_column(type_String, "strings");
+        tbl->add_empty_row();
+    }
+    
+    Platform::String^ ps = MyTextBox->Text;
+    std::wstring ws = ps->Data();
+    StringData sd((char*)ws.data(), ws.size()*sizeof(wchar_t));
+    tbl->set_string(0, 0, sd);
+
+}
+
+
+void App1::MainPage::TextBlock_SelectionChanged(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+    
+}
+
+
+void App1::MainPage::Button_Click_1(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+    StringData sd = tbl->get_string(0, 0);
+    std::wstring s = std::wstring((wchar_t*)sd.data());
+    Platform::String^ ps = ref new Platform::String(s.c_str());
+    MyTextBlock->Text = ps;
 }
