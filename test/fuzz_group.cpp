4102b1ec170dafa7b9f5666f8c3a5c12894e80fc
James Stone <js@realm.io>
2017-03-07 10:52:50 -0800

    Add instruction to AFL for setting Mixed types.

--- test/fuzz_group.cpp
+++ test/fuzz_group.cpp
@@ -138,6 +138,88 @@ int32_t get_int32(State& s)
     return v;
 }
 
+std::pair<int64_t, int32_t> get_timestamp_values(State& s) {
+    int64_t seconds = get_int64(s);
+    int32_t nanoseconds = get_int32(s) % 1000000000;
+    // Make sure the values form a sensible Timestamp
+    const bool both_non_negative = seconds >= 0 && nanoseconds >= 0;
+    const bool both_non_positive = seconds <= 0 && nanoseconds <= 0;
+    const bool correct_timestamp = both_non_negative || both_non_positive;
+    if (!correct_timestamp) {
+        nanoseconds = -nanoseconds;
+    }
+    return {seconds, nanoseconds};
+}
+
+Mixed construct_mixed(State& s, util::Optional<std::ostream&> log)
+{
+    // Mixed type has 8 constructors supporting different types.
+    unsigned char type = get_next(s) % 8;
+
+    switch (type) {
+        default:
+        case 0: {
+            bool b = get_next(s) % 2;
+            if (log) {
+                *log << "Mixed mixed(" << (b ? "true" : "false") << ");";
+            }
+            return Mixed(b);
+        }
+        case 1: {
+            int64_t value = get_int64(s);
+            if (log) {
+                *log << "Mixed mixed((int64_t)(" << value << "));";
+            }
+            return Mixed(value);
+        }
+        case 2: {
+            float value = get_next(s);
+            if (log) {
+                *log << "Mixed mixed((float)(" << value << "));";
+            }
+            return Mixed(value);
+        }
+        case 3: {
+            double value = get_next(s);
+            if (log) {
+                *log << "Mixed mixed((double)(" << value << "));";
+            }
+            return Mixed(value);
+        }
+        case 4: {
+            std::string str = create_string(get_next(s));
+            if (log) {
+                *log << "Mixed mixed(StringData(\"" << str << "\"));";
+            }
+            return Mixed(StringData(str));
+        }
+        case 5: {
+            size_t rand_char = get_next(s);
+            size_t blob_size = get_int64(s) % ArrayBlob::max_binary_size;
+            std::string blob(blob_size, static_cast<unsigned char>(rand_char));
+            if (log) {
+                *log << "std::string blob(" << blob_size << ", static_cast<unsigned char>(" << rand_char << "));\n"
+                     << "Mixed mixed(BinaryData(blob));";
+            }
+            return Mixed(BinaryData(blob));
+        }
+        case 6: {
+            int64_t time = get_int64(s);
+            if (log) {
+                *log << "Mixed mixed(OldDateTime(" << time << "));";
+            }
+            return Mixed(OldDateTime(time));
+        }
+        case 7: {
+            std::pair<int64_t, int32_t> values = get_timestamp_values(s);
+            if (log) {
+                *log << "Mixed mixed(Timestamp{" << values.first << ", " << values.second << "});";
+            }
+            return Mixed(Timestamp{values.first, values.second});
+        }
+    }
+}
+
 std::string create_column_name(State& s)
 {
     const size_t length = get_next(s) % (Descriptor::max_column_name_length + 1);
@@ -347,7 +429,7 @@ void parse_and_apply_instructions(std::string& in, const std::string& path, util
                     std::string name = create_column_name(s);
                     if (log) {
                         *log << "g.get_table(" << table_ndx << ")->rename_column("
-                             << col_ndx << ", " << name << ");\n";
+                             << col_ndx << ", \"" << name << "\");\n";
                     }
                     t->rename_column(col_ndx, name);
                 }
@@ -578,21 +660,24 @@ void parse_and_apply_instructions(std::string& in, const std::string& path, util
                             }
                         }
                         else if (type == type_Timestamp) {
-                            int64_t seconds = get_int64(s);
-                            int32_t nanoseconds = get_int32(s) % 1000000000;
-                            // Make sure the values form a sensible Timestamp
-                            const bool both_non_negative = seconds >= 0 && nanoseconds >= 0;
-                            const bool both_non_positive = seconds <= 0 && nanoseconds <= 0;
-                            const bool correct_timestamp = both_non_negative || both_non_positive;
-                            if (!correct_timestamp) {
-                                nanoseconds = -nanoseconds;
-                            }
-                            Timestamp value{seconds, nanoseconds};
+                            std::pair<int64_t, int32_t> values = get_timestamp_values(s);
+                            Timestamp value{values.first, values.second};
                             if (log) {
                                 *log << "g.get_table(" << table_ndx << ")->set_timestamp(" << col_ndx << ", "
                                      << row_ndx << ", " << value << ");\n";
                             }
                             t->set_timestamp(col_ndx, row_ndx, value);
+                        }
+                        else if (type == type_Mixed) {
+                            if (log) {
+                                *log << "{\n";
+                            }
+                            Mixed mixed = construct_mixed(s, log);
+                            if (log) {
+                                *log << "\ng.get_table(" << table_ndx << ")->set_mixed(" << col_ndx << ", "
+                                     << row_ndx << ", mixed);\n}\n";
+                            }
+                            t->set_mixed(col_ndx, row_ndx, mixed);
                         }
                     }
                 }
