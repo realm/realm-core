realm/realm-core

commit 12f00d08ca8f575ffc9b2a7b0debb605cfb3b49c

Allow for history schemas to be upgraded independently of the Realm file format

src/realm/group.cpp
@@ -96,7 +96,8 @@ int Group::get_committed_file_format_version() const noexcept
 int Group::get_target_file_format_version_for_session(int current_file_format_version,
                                                       int requested_history_type) noexcept
 {
-    Replication::HistoryType requested_history_type_2 = Replication::HistoryType(requested_history_type);
+    Replication::HistoryType requested_history_type_2 =
+        Replication::HistoryType(requested_history_type);
 
     // Note: This function is responsible for choosing the target file format
     // for a sessions. If it selects a file format that is different from
@@ -112,9 +113,12 @@ int Group::get_target_file_format_version_for_session(int current_file_format_ve
     // that the file format it is not yet decided (only possible for empty
     // Realms where top-ref is zero).
 
+    // Please see Allocator::get_file_format_version() for information about the
+    // individual file format versions.
+
     static_cast<void>(current_file_format_version);
     static_cast<void>(requested_history_type_2);
-    return Allocator::CURRENT_FILE_FORMAT_VERSION;
+    return 7;
 }
 
 
@@ -125,7 +129,7 @@ void Group::upgrade_file_format(int target_file_format_version)
     // Be sure to revisit the following upgrade logic when a new file foprmat
     // version is introduced. The following assert attempt to help you not
     // forget it.
-    REALM_ASSERT_EX(target_file_format_version == 6, target_file_format_version);
+    REALM_ASSERT_EX(target_file_format_version == 7, target_file_format_version);
 
     int current_file_format_version = get_file_format_version();
     REALM_ASSERT(current_file_format_version < target_file_format_version);
@@ -134,8 +138,8 @@ void Group::upgrade_file_format(int target_file_format_version)
     // following upgrade logic when SlabAlloc::validate_buffer() is changed (or
     // vice versa).
     REALM_ASSERT_EX(current_file_format_version == 2 || current_file_format_version == 3 ||
-                        current_file_format_version == 4 || current_file_format_version == 5,
-                    current_file_format_version);
+                    current_file_format_version == 4 || current_file_format_version == 5 ||
+                    current_file_format_version == 6, current_file_format_version);
 
     // Upgrade from 2 to 3
     if (current_file_format_version <= 2 && target_file_format_version >= 3) {
@@ -163,6 +167,18 @@ void Group::upgrade_file_format(int target_file_format_version)
         for (size_t t = 0; t < m_tables.size(); t++) {
             TableRef table = get_table(t);
             table->upgrade_file_format(target_file_format_version);
+        }
+    }
+
+    // Upgrade from 6 to 7 (new history schema version in top array)
+    if (current_file_format_version <= 6 && target_file_format_version >= 7) {
+        // If top array size is 9, then add the missing 10th element containing
+        // the history schema version.
+        std::size_t top_size = m_top.size();
+        REALM_ASSERT(top_size <= 9);
+        if (top_size == 9) {
+            int initial_history_schema_version = 0;
+            m_top.add(initial_history_schema_version); // Throws
         }
     }
 
@@ -296,10 +312,10 @@ void Group::attach(ref_type top_ref, bool create_group_when_missing)
         static_cast<void>(top_size);
 
         if (top_size < 8) {
-            REALM_ASSERT_11(top_size, ==, 3, ||, top_size, ==, 5, ||, top_size, ==, 7);
+            REALM_ASSERT_EX(top_size == 3 || top_size == 5 || top_size == 7, top_size);
         }
         else {
-            REALM_ASSERT_3(top_size, ==, 9);
+            REALM_ASSERT_EX(top_size == 10, top_size);
         }
 
         m_table_names.init_from_parent();
@@ -1860,6 +1876,34 @@ void Group::advance_transact(ref_type new_top_ref, size_t new_file_size, _impl::
 }
 
 
+void Group::prepare_history_parent(Array& history_root, int history_type,
+                                   int history_schema_version)
+{
+    REALM_ASSERT(m_alloc.get_file_format_version() >= 7);
+    if (m_top.size() < 10) {
+        REALM_ASSERT(m_top.size() <= 7);
+        while (m_top.size() < 7) {
+            m_top.add(0); // Throws
+        }
+        ref_type history_ref = 0; // No history yet
+        m_top.add(RefOrTagged::make_tagged(history_type)); // Throws
+        m_top.add(RefOrTagged::make_ref(history_ref)); // Throws
+        m_top.add(RefOrTagged::make_tagged(history_schema_version)); // Throws
+    }
+    else {
+        int stored_history_type = int(m_top.get_as_ref_or_tagged(7).get_as_int());
+        int stored_history_schema_version = int(m_top.get_as_ref_or_tagged(9).get_as_int());
+        if (stored_history_type != Replication::hist_None) {
+            REALM_ASSERT(stored_history_type == history_type);
+            REALM_ASSERT(stored_history_schema_version == history_schema_version);
+        }
+        m_top.set(7, RefOrTagged::make_tagged(history_type)); // Throws
+        m_top.set(9, RefOrTagged::make_tagged(history_schema_version)); // Throws
+    }
+    set_history_parent(history_root);
+}
+
+
 #ifdef REALM_DEBUG // LCOV_EXCL_START ignore debug functions
 
 namespace {
@@ -1994,7 +2038,9 @@ void Group::verify() const
         if (_impl::History* hist = repl->get_history()) {
             _impl::History::version_type version = 0;
             int history_type = 0;
-            get_version_and_history_type(m_top, version, history_type);
+            int history_schema_version = 0;
+            get_version_and_history_info(m_top, version, history_type, history_schema_version);
+            REALM_ASSERT(history_type != Replication::hist_None || history_schema_version == 0);
             hist->update_from_parent(version);
             hist->verify();
         }
@@ -2015,8 +2061,8 @@ void Group::verify() const
     // marked as free before the file was opened.
     MemUsageVerifier mem_usage_2(ref_begin, immutable_ref_end, mutable_ref_end, baseline);
     {
-        REALM_ASSERT(m_top.size() == 3 || m_top.size() == 5 || m_top.size() == 7 ||
-                     (get_file_format_version() >= 4 && m_top.size() == 9));
+        REALM_ASSERT_EX(m_top.size() == 3 || m_top.size() == 5 || m_top.size() == 7 ||
+                        m_top.size() == 10, m_top.size());
         Allocator& alloc = m_top.get_alloc();
         ArrayInteger pos(alloc), len(alloc), ver(alloc);
         size_t pos_ndx = 3, len_ndx = 4, ver_ndx = 5;

src/realm/group.hpp
@@ -532,32 +532,33 @@ private:
     ///
     /// <pre>
     ///
-    ///   slot  value
-    ///   -----------------------
-    ///   1st   m_table_names
-    ///   2nd   m_tables
-    ///   3rd   Logical file size
-    ///   4th   GroupWriter::m_free_positions (optional)
-    ///   5th   GroupWriter::m_free_lengths   (optional)
-    ///   6th   GroupWriter::m_free_versions  (optional)
-    ///   7th   Transaction number / version  (optional)
-    ///   8th   In-Realm history type         (optional)
-    ///   9th   In-Realm history ref          (optional)
+    ///                                                     Introduced in file
+    ///   Slot  Value                                       format version
+    ///   ---------------------------------------------------------------------
+    ///    1st   m_table_names
+    ///    2nd   m_tables
+    ///    3rd   Logical file size
+    ///    4th   GroupWriter::m_free_positions (optional)
+    ///    5th   GroupWriter::m_free_lengths   (optional)
+    ///    6th   GroupWriter::m_free_versions  (optional)
+    ///    7th   Transaction number / version  (optional)
+    ///    8th   History type         (optional)             4
+    ///    9th   History ref          (optional)             4
+    ///   10th   History version      (optional)             7
     ///
     /// </pre>
     ///
-    /// The 'in-Realm history type' slot stores a value of
-    /// Replication::HistoryType, although never
-    /// Replication::hist_OutOfRealm. For more information about that, see
-    /// Replication::get_history_type().
+    /// The 'History type' slot stores a value of type
+    /// Replication::HistoryType. The 'History version' slot stores a history
+    /// schema version as returned by Replication::get_history_schema_version().
     ///
     /// The first three entries are mandatory. In files created by
     /// Group::write(), none of the optional entries are present and the size of
     /// `m_top` is 3. In files updated by Group::commit(), the 4th and 5th entry
-    /// is present, and the size of `m_top` is 5. In files updated by way of a
-    /// transaction (SharedGroup::commit()), the 4th, 5th, 6th, and 7th entry is
-    /// present, and the size of `m_top` is 7. In files that contain a changeset
-    /// history, the 8th and 9th entry is present.
+    /// are present, and the size of `m_top` is 5. In files updated by way of a
+    /// transaction (SharedGroup::commit()), the 4th, 5th, 6th, and 7th entry
+    /// are present, and the size of `m_top` is 7. In files that contain a
+    /// changeset history, the 8th, 9th, and 10th entry are present.
     ///
     /// When a group accessor is attached to a newly created file or an empty
     /// memory buffer where there is no top array yet, `m_top`, `m_tables`, and
@@ -684,11 +685,13 @@ private:
     void send_cascade_notification(const CascadeNotification& notification) const;
     void send_schema_change_notification() const;
 
-    static void get_version_and_history_type(const Array& top, _impl::History::version_type& version,
-                                             int& history_type) noexcept;
+    static void get_version_and_history_info(const Array& top, _impl::History::version_type& version,
+                                             int& history_type, int& history_schema_version) noexcept;
     static ref_type get_history_ref(const Array& top) noexcept;
+    static int get_history_schema_version(const Array& top) noexcept;
+    void set_history_schema_version(int version);
     void set_history_parent(Array& history_root) noexcept;
-    void prepare_history_parent(Array& history_root, int history_type);
+    void prepare_history_parent(Array& history_root, int history_type, int history_schema_version);
 
     friend class Table;
     friend class GroupWriter;
@@ -956,19 +959,22 @@ inline void Group::send_schema_change_notification() const
         m_schema_change_handler();
 }
 
-inline void Group::get_version_and_history_type(const Array& top, _impl::History::version_type& version,
-                                                int& history_type) noexcept
+inline void Group::get_version_and_history_info(const Array& top, _impl::History::version_type& version,
+                                                int& history_type, int& history_schema_version) noexcept
 {
-    _impl::History::version_type version_2 = 0;
+    using version_type = _impl::History::version_type;
+    version_type version_2 = 0;
     int history_type_2 = 0;
+    int history_schema_version_2 = 0;
     if (top.is_attached()) {
         if (top.size() >= 6) {
             REALM_ASSERT(top.size() >= 7);
-            version_2 = _impl::History::version_type(top.get(6) / 2);
+            version_2 = version_type(top.get_as_ref_or_tagged(6).get_as_int());
         }
         if (top.size() >= 8) {
-            REALM_ASSERT(top.size() >= 9);
-            history_type_2 = int(top.get(7) / 2);
+            REALM_ASSERT(top.size() >= 10);
+            history_type_2           = int(top.get_as_ref_or_tagged(7).get_as_int());
+            history_schema_version_2 = int(top.get_as_ref_or_tagged(9).get_as_int());
         }
     }
     // Version 0 is not a legal initial version, so it has to be set to 1
@@ -977,33 +983,40 @@ inline void Group::get_version_and_history_type(const Array& top, _impl::History
         version_2 = 1;
     version = version_2;
     history_type = history_type_2;
+    history_schema_version = history_schema_version_2;
 }
 
 inline ref_type Group::get_history_ref(const Array& top) noexcept
 {
     if (top.is_attached()) {
         if (top.size() >= 8) {
-            REALM_ASSERT(top.size() >= 9);
+            REALM_ASSERT(top.size() >= 10);
             return top.get_as_ref(8);
         }
     }
     return 0;
 }
 
+inline int Group::get_history_schema_version(const Array& top) noexcept
+{
+    if (top.is_attached()) {
+        if (top.size() >= 8) {
+            REALM_ASSERT(top.size() >= 10);
+            return int(top.get_as_ref_or_tagged(9).get_as_int());
+        }
+    }
+    return 0;
+}
+
+inline void Group::set_history_schema_version(int version)
+{
+    REALM_ASSERT(m_top.size() >= 10);
+    m_top.set(9, RefOrTagged::make_tagged(unsigned(version))); // Throws
+}
+
 inline void Group::set_history_parent(Array& history_root) noexcept
 {
     history_root.set_parent(&m_top, 8);
-}
-
-inline void Group::prepare_history_parent(Array& history_root, int history_type)
-{
-    REALM_ASSERT(m_alloc.get_file_format_version() >= 4);
-    // Ensure that there are slots for both the history type and the history
-    // ref.
-    while (m_top.size() < 9)
-        m_top.add(0);                                     // Throws
-    m_top.set(7, RefOrTagged::make_tagged(history_type)); // Throws
-    set_history_parent(history_root);
 }
 
 class Group::TableWriter {
@@ -1154,13 +1167,15 @@ public:
             group.create_empty_group(); // Throws
     }
 
-    static void get_version_and_history_type(Allocator& alloc, ref_type top_ref,
-                                             _impl::History::version_type& version, int& history_type) noexcept
+    static void get_version_and_history_info(Allocator& alloc, ref_type top_ref,
+                                             _impl::History::version_type& version,
+                                             int& history_type,
+                                             int& history_schema_version) noexcept
     {
         Array top(alloc);
         if (top_ref != 0)
             top.init_from_ref(top_ref);
-        Group::get_version_and_history_type(top, version, history_type);
+        Group::get_version_and_history_info(top, version, history_type, history_schema_version);
     }
 
     static ref_type get_history_ref(const Group& group) noexcept
@@ -1176,14 +1191,33 @@ public:
         return Group::get_history_ref(top);
     }
 
+    static int get_history_schema_version(const Group& group) noexcept
+    {
+        return Group::get_history_schema_version(group.m_top);
+    }
+
+    static int get_history_schema_version(Allocator& alloc, ref_type top_ref) noexcept
+    {
+        Array top{alloc};
+        if (top_ref != 0)
+            top.init_from_ref(top_ref);
+        return Group::get_history_schema_version(top);
+    }
+
+    static void set_history_schema_version(Group& group, int version)
+    {
+        group.set_history_schema_version(version); // Throws
+    }
+
     static void set_history_parent(Group& group, Array& history_root) noexcept
     {
         group.set_history_parent(history_root);
     }
 
-    static void prepare_history_parent(Group& group, Array& history_root, int history_type)
+    static void prepare_history_parent(Group& group, Array& history_root, int history_type,
+                                       int history_schema_version)
     {
-        group.prepare_history_parent(history_root, history_type); // Throws
+        group.prepare_history_parent(history_root, history_type, history_schema_version); // Throws
     }
 
     static int get_file_format_version(const Group& group) noexcept

src/realm/Makefile
@@ -159,7 +159,6 @@ exceptions.cpp \
 group.cpp \
 group_shared.cpp \
 group_writer.cpp \
-impl/continuous_transactions_history.cpp \
 impl/output_stream.cpp \
 impl/transact_log.cpp \
 impl/simulated_failure.cpp \

src/realm/alloc.cpp
@@ -22,6 +22,8 @@
 #include <algorithm>
 
 #include <realm/alloc_slab.hpp>
+#include <realm/group.hpp>
+#include <realm/replication.hpp>
 
 using namespace realm;
 
@@ -58,7 +60,15 @@ public:
     DefaultAllocator()
     {
         m_baseline = 1; // Zero is not available
-        m_file_format_version = CURRENT_FILE_FORMAT_VERSION;
+
+        // Please see Allocator::get_file_format_version() for information about
+        // the following determination of the file format version.
+        int current_file_format_version = 0; // Undecided
+        int history_type = Replication::hist_None;
+        using gf = _impl::GroupFriend;
+        m_file_format_version =
+            gf::get_target_file_format_version_for_session(current_file_format_version,
+                                                           history_type);
     }
 
     MemRef do_alloc(const size_t size) override

src/realm/alloc.hpp
@@ -80,8 +80,6 @@ private:
 /// \sa SlabAlloc
 class Allocator {
 public:
-    static constexpr int CURRENT_FILE_FORMAT_VERSION = 6;
-
     /// The specified size must be divisible by 8, and must not be
     /// zero.
     ///
@@ -141,14 +139,14 @@ public:
 
     Replication* get_replication() noexcept;
 
-    /// \brief The version of the format of the the node structure (in file or
-    /// in memory) in use by Realm objects associated with this allocator.
+    /// \brief The version of the format of the node structure (in file or in
+    /// memory) in use by Realm objects associated with this allocator.
     ///
     /// Every allocator contains a file format version field, which is returned
     /// by this function. In some cases (as mentioned below) the file format can
     /// change.
     ///
-    /// A value of zero means the the file format is not yet decided. This is
+    /// A value of zero means that the file format is not yet decided. This is
     /// only possible for empty Realms where top-ref is zero.
     ///
     /// For the default allocator (get_default()), the file format version field
@@ -205,6 +203,8 @@ public:
     ///     logs into the Realm file. Changes to the transaction log format
     ///     including reshuffling instructions. This is the format used in
     ///     milestone 2.0.0.
+    ///
+    ///   7 Introduced "history schema version" as 10th entry in top array.
     ///
     /// IMPORTANT: When introducing a new file format version, be sure to review
     /// the file validity checks in AllocSlab::validate_buffer(), the file

src/realm/alloc_slab.cpp
@@ -973,25 +973,29 @@ void SlabAlloc::validate_buffer(const char* data, size_t size, const std::string
     else if (is_shared) {
         // In shared mode (Realm file opened via a SharedGroup instance) this
         // version of the core library is able to open Realms using file format
-        // versions 2, 3, 4, 5, and 6. Version 2, 3, 4, and 5 files need to be
-        // upgraded.
+        // versions 2, 3, 4, 5, 6, and 7. Version 2, 3, 4, 5, and 6 files need
+        // to be upgraded. Please see Allocator::get_file_format_version() for
+        // information about the individual file format verions.
         switch (file_format_version) {
             case 2:
             case 3:
             case 4:
             case 5:
             case 6:
+            case 7:
                 bad_file_format = false;
         }
     }
     else {
         // In non-shared mode (Realm file opened via a Group instance) this
         // version of the core library is only able to open Realms using file
-        // format version 6. Since a Realm file cannot be upgraded when opened
+        // format version 7. Since a Realm file cannot be upgraded when opened
         // in this mode (we may be unable to write to the file), no earlier
-        // versions can be opened.
+        // versions can be opened. Please see
+        // Allocator::get_file_format_version() for information about the
+        // individual file format verions.
         switch (file_format_version) {
-            case 6:
+            case 7:
                 bad_file_format = false;
         }
     }

src/realm/exceptions.cpp
@@ -81,7 +81,10 @@ const char* LogicError::what() const noexcept
                    "not consistent across the session";
         case mixed_history_type:
             return "History type (as specified by the Replication implementation passed to "
-                   "the SharedGroup constructor) was not consistent across the session";
+                "the SharedGroup constructor) was not consistent across the session";
+        case mixed_history_schema_version:
+            return "History schema version (as specified by the Replication implementation passed "
+                "to the SharedGroup constructor) was not consistent across the session";
         case table_has_no_columns:
             return "Table has no columns";
         case column_does_not_exist:

src/realm/exceptions.hpp
@@ -57,12 +57,12 @@ public:
 };
 
 
-/// The \c FileFormatUpgradeRequired exception can be thrown by the \c
-/// SharedGroup constructor when opening a database that uses a deprecated file
-/// format, and the user has indicated he does not want automatic upgrades to
-/// be performed. This exception indicates that until an upgrade of the file
-/// format is performed, the database will be unavailable for read or write
-/// operations.
+/// The FileFormatUpgradeRequired exception can be thrown by the SharedGroup
+/// constructor when opening a database that uses a deprecated file format
+/// and/or a deprecated history schema, and the user has indicated he does not
+/// want automatic upgrades to be performed. This exception indicates that until
+/// an upgrade of the file format is performed, the database will be unavailable
+/// for read or write operations.
 class FileFormatUpgradeRequired : public std::exception {
 public:
     const char* what() const noexcept override;
@@ -198,6 +198,11 @@ public:
         /// to the SharedGroup constructor) was not consistent across the
         /// session.
         mixed_history_type,
+
+        /// History schema version (as specified by the Replication
+        /// implementation passed to the SharedGroup constructor) was not
+        /// consistent across the session.
+        mixed_history_schema_version,
 
         /// Adding rows to a table with no columns is not supported.
         table_has_no_columns,

commit c1dcd7c6376c868563475535a975b6aebd7e43a6

Allow realm files version 6 to be opened
Fixes problem introduced in commit 12f00d08c

src/realm/group.cpp
@@ -315,7 +315,7 @@ void Group::attach(ref_type top_ref, bool create_group_when_missing)
             REALM_ASSERT_EX(top_size == 3 || top_size == 5 || top_size == 7, top_size);
         }
         else {
-            REALM_ASSERT_EX(top_size == 10, top_size);
+            REALM_ASSERT_EX(top_size == 9 || top_size == 10, top_size);
         }
 
         m_table_names.init_from_parent();

src/realm/group.hpp
@@ -972,8 +972,10 @@ inline void Group::get_version_and_history_info(const Array& top, _impl::History
             version_2 = version_type(top.get_as_ref_or_tagged(6).get_as_int());
         }
         if (top.size() >= 8) {
-            REALM_ASSERT(top.size() >= 10);
+            REALM_ASSERT(top.size() >= 9);
             history_type_2           = int(top.get_as_ref_or_tagged(7).get_as_int());
+        }
+        if (top.size() >= 10) {
             history_schema_version_2 = int(top.get_as_ref_or_tagged(9).get_as_int());
         }
     }