9bea6a788e6a664180c05a2264489e28c8a11f0c
Mark Rowe <mrowe@bdash.net.nz>
2017-06-26 13:17:05 -0700

    Revert the change to handling of RemoveProperty within create_initial_tables.
    Simon mentioned on Slack that he does not recall the original
    motiviation for the change, and since tests pass without it then it
    seems safe to undo it.

--- src/object_store.cpp
+++ src/object_store.cpp
@@ -550,7 +550,7 @@ static void create_initial_tables(Group& group, std::vector<SchemaChange> const&
         // not-quite-correct files produced by other things and has no obvious
         // downside.
         void operator()(AddProperty op) { add_column(group, table(op.object), *op.property); }
-        void operator()(RemoveProperty) { }
+        void operator()(RemoveProperty op) { table(op.object).remove_column(op.property->table_column); }
         void operator()(MakePropertyNullable op) { make_property_optional(group, table(op.object), *op.property); }
         void operator()(MakePropertyRequired op) { make_property_required(group, table(op.object), *op.property); }
         void operator()(ChangePrimaryKey op) { ObjectStore::set_primary_key_for_object(group, op.object->name, op.property ? StringData{op.property->name} : ""); }
