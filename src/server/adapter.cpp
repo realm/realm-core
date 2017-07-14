0dafbc95a82a3d373306873e414a442156846d61
Ari Lazier <alazier@gmail.com>
2017-07-14 06:55:07 -0700

    fixes for tests

--- src/server/adapter.cpp
+++ src/server/adapter.cpp
@@ -166,7 +166,9 @@ public:
                 out_object_schema = &*object_schema;
                 out_table = ObjectStore::table_for_object_type(m_group, object_type);
                 out_primary = out_object_schema->primary_key_property();
-                REALM_ASSERT(out_primary);
+                if (!out_primary) {
+                    throw std::runtime_error("Only object types with primary keys can be used in the adapter");
+                }
             }
         }
     }
