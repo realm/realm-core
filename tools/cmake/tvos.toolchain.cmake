99be142a7c37f0e03816b13dd4fe1d98185a1f3e
Thomas Goyne <tg@realm.io>
2017-07-27 13:12:43 -0700

    Fix building for watchOS/tvOS with cmake 3.9

--- tools/cmake/tvos.toolchain.cmake
+++ tools/cmake/tvos.toolchain.cmake
@@ -8,6 +8,11 @@ set_common_xcode_attributes()
 
 set(REALM_SKIP_SHARED_LIB ON)
 
+# CMake special-cases the compiler detection for iOS in a way that happens
+# to also work for tvOS, so use the iphoneos base SDK to opt-in to that and
+# then override the platform to tvOS below
+set(CMAKE_OSX_SYSROOT "iphoneos")
+
 set(CMAKE_XCODE_ATTRIBUTE_SUPPORTED_PLATFORMS "appletvos appletvsimulator")
 set(CMAKE_XCODE_EFFECTIVE_PLATFORMS "-appletvos;-appletvsimulator")
 set(CMAKE_XCODE_ATTRIBUTE_TVOS_DEPLOYMENT_TARGET "9.0")
