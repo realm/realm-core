4af1c6d301ddfd5cf5fa12a08c7255db07ebe411
Thomas Goyne <tg@realm.io>
2018-01-26 11:38:19 -0800

    Remove client_should_reconnect_immediately()
    This functionality was removed from sync and was unused.

--- src/sync/sync_manager.hpp
+++ src/sync/sync_manager.hpp
@@ -88,10 +88,6 @@ public:
     void set_log_level(util::Logger::Level) noexcept;
     void set_logger_factory(SyncLoggerFactory&) noexcept;
 
-    /// Control whether the sync client attempts to reconnect immediately. Only set this to `true` for testing purposes.
-    void set_client_should_reconnect_immediately(bool reconnect_immediately);
-    bool client_should_reconnect_immediately() const noexcept;
-
     /// Ask all valid sync sessions to perform whatever tasks might be necessary to
     /// re-establish connectivity with the Realm Object Server. It is presumed that
     /// the caller knows that network connectivity has been restored.
