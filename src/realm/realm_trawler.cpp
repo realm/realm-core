5afdea4de47cfb4ed498aae51de4d6eb72734dbd
Jørgen Edelbo <je@realm.io>
2018-10-11 09:43:41 +0200

    Supporting encryption in realm-trawler

--- src/realm/realm_trawler.cpp
+++ src/realm/realm_trawler.cpp
@@ -28,6 +28,8 @@
 #include <cassert>
 #include <cstring>
 #include "array_direct.hpp"
+#include "alloc_slab.hpp"
+#include "array.hpp"
 
 constexpr const int signature = 0x41414141;
 constexpr const int alt_signature = 41414141;
@@ -49,18 +51,6 @@ struct StreamingFooter {
 };
 
 static const uint_fast64_t footer_magic_cookie = 0x3034125237E526C8ULL;
-
-#ifdef REALM_DEBUG
-namespace realm {
-namespace util {
-void terminate(const char* message, const char* file, long line, std::initializer_list<Printable>&&) noexcept
-{
-    std::cerr << file << ":" << line << ": " << message << std::endl;
-    exit(1);
-}
-}
-}
-#endif
 
 template <class T>
 void consolidate_list(std::vector<T>& list)
@@ -123,11 +113,11 @@ public:
     Node()
     {
     }
-    Node(std::ifstream& is, uint64_t ref)
+    Node(realm::Allocator& alloc, uint64_t ref)
     {
-        init(is, ref);
+        init(alloc, ref);
     }
-    void init(std::ifstream& is, uint64_t ref);
+    void init(realm::Allocator& alloc, uint64_t ref);
     bool valid() const
     {
         return m_valid;
@@ -151,11 +141,11 @@ public:
     }
     uint64_t size_in_bytes() const
     {
-        return sizeof(m_header) + length();
+        return 8 + length();
     }
 
 protected:
-    unsigned char m_header[8];
+    char* m_header;
     unsigned m_size = 0;
     bool m_valid = false;
 
@@ -187,22 +177,20 @@ public:
     Array()
     {
     }
-    Array(std::ifstream& is, uint64_t ref)
-        : Node(is, ref)
+    Array(realm::Allocator& alloc, uint64_t ref)
+        : Node(alloc, ref)
     {
-        init(is, ref);
+        init(alloc, ref);
     }
-    void init(std::ifstream& is, uint64_t ref)
+    void init(realm::Allocator& alloc, uint64_t ref)
     {
-        Node::init(is, ref);
-        unsigned len = length();
-        m_data.reset(new char[len]);
-        is.read(m_data.get(), len);
+        Node::init(alloc, ref);
+        m_data = realm::Array::get_data_from_header(m_header);
         m_has_refs = has_refs();
     }
     int64_t get_val(unsigned ndx) const
     {
-        int64_t val = realm::get_direct(m_data.get(), width(), ndx);
+        int64_t val = realm::get_direct(m_data, width(), ndx);
 
         if (m_has_refs) {
             if (val & 1) {
@@ -214,7 +202,7 @@ public:
     uint64_t get_ref(unsigned ndx) const
     {
         REALM_ASSERT(m_has_refs);
-        int64_t val = realm::get_direct(m_data.get(), width(), ndx);
+        int64_t val = realm::get_direct(m_data, width(), ndx);
 
         if (val & 1)
             val = 0;
@@ -228,30 +216,30 @@ public:
             auto sz = size_in_bytes();
             auto w = width();
             REALM_ASSERT(ndx * w < sz);
-            str = m_data.get() + ndx * w;
+            str = m_data + ndx * w;
         }
         return str;
     }
 
-    static void get_nodes(std::ifstream& is, uint64_t ref, std::vector<Entry>&);
+    static void get_nodes(realm::Allocator& alloc, uint64_t ref, std::vector<Entry>&);
 
 private:
-    std::unique_ptr<char> m_data;
+    char* m_data;
     bool m_has_refs = false;
 };
 
 class Group : public Array {
 public:
-    Group(std::ifstream& is, uint64_t ref)
-        : Array(is, ref)
-        , m_is(is)
+    Group(realm::Allocator& alloc, uint64_t ref)
+        : Array(alloc, ref)
+        , m_alloc(alloc)
     {
         if (valid()) {
-            m_table_names.init(is, get_ref(0));
+            m_table_names.init(alloc, get_ref(0));
             m_file_size = get_val(2);
-            m_free_list_positions.init(is, get_ref(3));
-            m_free_list_sizes.init(is, get_ref(4));
-            m_free_list_versions.init(is, get_ref(5));
+            m_free_list_positions.init(alloc, get_ref(3));
+            m_free_list_sizes.init(alloc, get_ref(4));
+            m_free_list_versions.init(alloc, get_ref(5));
         }
     }
     uint64_t get_file_size() const
@@ -260,7 +248,7 @@ public:
     }
     int get_current_version() const
     {
-        return int();
+        return int(get_val(6));
     }
     int get_nb_tables() const
     {
@@ -272,7 +260,7 @@ public:
 
 private:
     friend std::ostream& operator<<(std::ostream& ostr, const Group& g);
-    std::ifstream& m_is;
+    realm::Allocator& m_alloc;
     uint64_t m_file_size;
     Array m_table_names;
     Array m_free_list_positions;
@@ -284,7 +272,7 @@ std::ostream& operator<<(std::ostream& ostr, const Group& g)
 {
     if (g.valid()) {
         ostr << "File size: " << g.get_file_size() << std::endl;
-        ostr << "Current version:" << g.get_current_version() << std::endl;
+        ostr << "Current version: " << g.get_current_version() << std::endl;
         ostr << "Free list size: " << g.m_free_list_positions.size() << std::endl;
         ostr << "Tables: " << std::endl;
 
@@ -300,25 +288,23 @@ std::ostream& operator<<(std::ostream& ostr, const Group& g)
 
 class RealmFile {
 public:
-    RealmFile(std::ifstream& is);
+    RealmFile(const std::string& file_path, const char* encryption_key);
     // Walk the file and check that it consists of valid nodes
     void node_scan();
     void memory_leaks();
     void free_list_info() const;
 
 private:
-    std::ifstream& m_is;
     uint64_t m_top_ref;
     uint64_t m_start_pos;
     int m_file_format_version;
     std::unique_ptr<Group> m_group;
+    realm::SlabAlloc m_alloc;
 };
 
-void Node::init(std::ifstream& is, uint64_t ref)
+void Node::init(realm::Allocator& alloc, uint64_t ref)
 {
-    unsigned char header[8];
-    is.seekg(ref, is.beg);
-    is.read(reinterpret_cast<char*>(&m_header), sizeof(header));
+    m_header = alloc.translate(ref);
 
     if (memcmp(m_header, &signature, 4)) {
     }
@@ -328,26 +314,26 @@ void Node::init(std::ifstream& is, uint64_t ref)
     }
 }
 
-void Array::get_nodes(std::ifstream& is, uint64_t ref, std::vector<Entry>& nodes)
+void Array::get_nodes(realm::Allocator& alloc, uint64_t ref, std::vector<Entry>& nodes)
 {
-    Array arr(is, ref);
+    Array arr(alloc, ref);
     nodes.emplace_back(ref, arr.size_in_bytes());
     if (arr.has_refs()) {
         auto sz = arr.size();
         for (size_t i = 0; i < sz; i++) {
             uint64_t r = arr.get_ref(i);
             if (r)
-                get_nodes(is, r, nodes);
+                get_nodes(alloc, r, nodes);
         }
     }
 }
 
 void Group::get_table_nodes(size_t ndx, std::vector<Entry>& nodes) const
 {
-    Array table_refs(m_is, get_ref(1));
+    Array table_refs(m_alloc, get_ref(1));
     auto ref = table_refs.get_val(ndx);
 
-    Array::get_nodes(m_is, ref, nodes);
+    Array::get_nodes(m_alloc, ref, nodes);
 
     consolidate_list(nodes);
 }
@@ -356,13 +342,13 @@ std::vector<Entry> Group::get_allocated_nodes() const
 {
     std::vector<Entry> all_nodes;
 
-    Array::get_nodes(m_is, get_ref(0), all_nodes); // Table names
+    Array::get_nodes(m_alloc, get_ref(0), all_nodes); // Table names
 
     for (int i = 0; i < get_nb_tables(); i++) {
         get_table_nodes(i, all_nodes);
     }
     if (size() > 8) {
-        Array::get_nodes(m_is, get_ref(8), all_nodes);
+        Array::get_nodes(m_alloc, get_ref(8), all_nodes);
     }
 
     consolidate_list(all_nodes);
@@ -385,43 +371,16 @@ std::vector<FreeListEntry> Group::get_free_list() const
     return list;
 }
 
-RealmFile::RealmFile(std::ifstream& is)
-    : m_is(is)
+RealmFile::RealmFile(const std::string& file_path, const char* encryption_key)
 {
-    m_top_ref = 0;
-    union Buffer {
-        Header file_header;
-        StreamingFooter file_footer;
-        char plain[sizeof(Header)];
-    } buffer;
+    realm::SlabAlloc::Config config;
+    config.encryption_key = encryption_key;
+    config.read_only = true;
+    config.no_create = true;
+    m_top_ref = m_alloc.attach_file(file_path, config);
 
-    is.seekg (0);
-    is.read(buffer.plain, sizeof(Header));
-    if (is) {
-        if (std::memcmp(buffer.file_header.m_mnemonic, "T-DB", 4)) {
-            std::cerr << err_txt << "Not a realm file ?" << std::endl;
-            return;
-        }
-        else {
-            m_top_ref = buffer.file_header.m_top_ref[buffer.file_header.m_flags];
-            m_start_pos = sizeof(Header);
-            if (m_top_ref == 0xFFFFFFFFFFFFFFFFULL && buffer.file_header.m_flags == 0) {
-                // Streaming format
-                m_start_pos = 0;
-                is.seekg(0 - sizeof(StreamingFooter), is.end);
-                is.read(buffer.plain, sizeof(StreamingFooter));
-                if (buffer.file_footer.m_magic_cookie == footer_magic_cookie) {
-                    m_top_ref = unsigned(buffer.file_footer.m_top_ref);
-                }
-                else {
-                    std::cerr << err_txt << "Top ref not found" << std::endl;
-                    return;
-                }
-            }
-        }
-    }
-    m_group = std::make_unique<Group>(is, m_top_ref);
-    m_file_format_version = buffer.file_header.m_file_format[buffer.file_header.m_flags];
+    m_group = std::make_unique<Group>(m_alloc, m_top_ref);
+    m_file_format_version = m_alloc.get_committed_file_format_version();
     std::cout << "Top ref: 0x" << std::hex << m_top_ref << std::dec << std::endl;
     std::cout << "File format version: " << m_file_format_version << std::endl;
     std::cout << *m_group << std::endl;
@@ -440,7 +399,7 @@ void RealmFile::node_scan()
                 ref += free_entry->length;
                 ++free_entry;
             }
-            Node n(m_is, ref);
+            Node n(m_alloc, ref);
             if (n.valid()) {
                 if (searching) {
                     std::cerr << "Resuming from ref: " << ref << std::endl;
@@ -514,11 +473,20 @@ void RealmFile::free_list_info() const
 int main (int argc, const char* argv[])
 {
     if (argc > 1) {
-        std::ifstream is(argv[1], std::ifstream::binary);
-        if (is) {
-            RealmFile rf(is);
-            if (argc > 2) {
-                for (const char* command = argv[2]; *command != '\0'; command++) {
+        try {
+            int curr_arg = 1;
+            const char* key_ptr = nullptr;
+            char key[64];
+            if (strcmp(argv[curr_arg], "--key") == 0) {
+                std::ifstream key_file(argv[curr_arg + 1]);
+                key_file.read(key, sizeof(key));
+                key_ptr = key;
+                curr_arg += 2;
+            }
+            RealmFile rf(argv[curr_arg], key_ptr);
+            curr_arg++;
+            if (argc > curr_arg) {
+                for (const char* command = argv[curr_arg]; *command != '\0'; command++) {
                     switch (*command) {
                         case 'f':
                             rf.free_list_info();
@@ -533,8 +501,8 @@ int main (int argc, const char* argv[])
                 }
             }
         }
-        else {
-            std::cerr << err_txt << "Could not open file '" << argv[1] << "'" << std::endl;
+        catch (const std::exception& e) {
+            std::cout << e.what() << std::endl;
         }
     }
     else {
