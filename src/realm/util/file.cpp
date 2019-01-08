realm/realm-core

commit dcc5337ce189ede65c6ae86c03328593fc3e7298

Fix warnings reported by GCC 8 (#3174)
* Fix shadow warnings

* Fix integer conversion warnings

* Fix fallthrough warnings

* Replace readdir_r() with readdir()

* Update CHANGELOG

src/realm/util/file.cpp
@@ -1391,16 +1391,21 @@ bool DirScanner::next(std::string& name)
     if (!m_dirp)
         return false;
 
-    const size_t min_dirent_size = offsetof(struct dirent, d_name) + NAME_MAX + 1;
-    union {
-        struct dirent m_dirent;
-        char m_strut[min_dirent_size];
-    } u;
-    struct dirent* dirent;
     for (;;) {
-        int err = readdir_r(m_dirp, &u.m_dirent, &dirent);
-        if (err != 0) {
-            throw std::system_error(err, std::system_category(), "readdir_r() failed");
+        // Note: readdir_r() is deprecated. However, all implementations
+        // implement readdir() to be thread-safe when operating on separate
+        // directory streams.
+        // More information: lwn.net/Articles/696475/
+
+        struct dirent* dirent;
+        do {
+            // readdir() does not seem to set errno=0 on success. The manpage
+            // recommends manually zeroing errno before calling it.
+            errno = 0;
+            dirent = readdir(m_dirp);
+        } while (errno == EAGAIN);
+        if (errno != 0) {
+            throw std::system_error(errno, std::system_category(), "readdir() failed");
         }
         if (!dirent)
             return false; // End of stream

src/realm/column_string_enum.cpp
@@ -31,11 +31,11 @@
 using namespace realm;
 using namespace realm::util;
 
-StringEnumColumn::StringEnumColumn(Allocator& alloc, ref_type ref, ref_type keys_ref, bool nullable,
+StringEnumColumn::StringEnumColumn(Allocator& alloc, ref_type ref, ref_type keys_ref, bool is_nullable,
                                    size_t column_ndx)
     : IntegerColumn(alloc, ref, column_ndx)         // Throws
-    , m_keys(alloc, keys_ref, nullable, column_ndx) // Throws
-    , m_nullable(nullable)
+    , m_keys(alloc, keys_ref, is_nullable, column_ndx) // Throws
+    , m_nullable(is_nullable)
 {
 }
 

src/realm/query_engine.hpp
@@ -744,11 +744,11 @@ public:
     {
     }
 
-    void aggregate_local_prepare(Action action, DataType col_id, bool nullable) override
+    void aggregate_local_prepare(Action action, DataType col_id, bool is_nullable) override
     {
         this->m_fastmode_disabled = (col_id == type_Float || col_id == type_Double);
         this->m_action = action;
-        this->m_find_callback_specialized = get_specialized_callback(action, col_id, nullable);
+        this->m_find_callback_specialized = get_specialized_callback(action, col_id, is_nullable);
     }
 
     size_t aggregate_local(QueryStateBase* st, size_t start, size_t end, size_t local_limit,
@@ -813,21 +813,21 @@ public:
 protected:
     using TFind_callback_specialized = typename BaseType::TFind_callback_specialized;
 
-    static TFind_callback_specialized get_specialized_callback(Action action, DataType col_id, bool nullable)
+    static TFind_callback_specialized get_specialized_callback(Action action, DataType col_id, bool is_nullable)
     {
         switch (action) {
             case act_Count:
-                return get_specialized_callback_2_int<act_Count>(col_id, nullable);
+                return get_specialized_callback_2_int<act_Count>(col_id, is_nullable);
             case act_Sum:
-                return get_specialized_callback_2<act_Sum>(col_id, nullable);
+                return get_specialized_callback_2<act_Sum>(col_id, is_nullable);
             case act_Max:
-                return get_specialized_callback_2<act_Max>(col_id, nullable);
+                return get_specialized_callback_2<act_Max>(col_id, is_nullable);
             case act_Min:
-                return get_specialized_callback_2<act_Min>(col_id, nullable);
+                return get_specialized_callback_2<act_Min>(col_id, is_nullable);
             case act_FindAll:
-                return get_specialized_callback_2_int<act_FindAll>(col_id, nullable);
+                return get_specialized_callback_2_int<act_FindAll>(col_id, is_nullable);
             case act_CallbackIdx:
-                return get_specialized_callback_2_int<act_CallbackIdx>(col_id, nullable);
+                return get_specialized_callback_2_int<act_CallbackIdx>(col_id, is_nullable);
             default:
                 break;
         }
@@ -836,15 +836,15 @@ protected:
     }
 
     template <Action TAction>
-    static TFind_callback_specialized get_specialized_callback_2(DataType col_id, bool nullable)
+    static TFind_callback_specialized get_specialized_callback_2(DataType col_id, bool is_nullable)
     {
         switch (col_id) {
             case type_Int:
-                return get_specialized_callback_3<TAction, type_Int>(nullable);
+                return get_specialized_callback_3<TAction, type_Int>(is_nullable);
             case type_Float:
-                return get_specialized_callback_3<TAction, type_Float>(nullable);
+                return get_specialized_callback_3<TAction, type_Float>(is_nullable);
             case type_Double:
-                return get_specialized_callback_3<TAction, type_Double>(nullable);
+                return get_specialized_callback_3<TAction, type_Double>(is_nullable);
             default:
                 break;
         }
@@ -853,19 +853,19 @@ protected:
     }
 
     template <Action TAction>
-    static TFind_callback_specialized get_specialized_callback_2_int(DataType col_id, bool nullable)
+    static TFind_callback_specialized get_specialized_callback_2_int(DataType col_id, bool is_nullable)
     {
         if (col_id == type_Int) {
-            return get_specialized_callback_3<TAction, type_Int>(nullable);
+            return get_specialized_callback_3<TAction, type_Int>(is_nullable);
         }
         REALM_ASSERT(false); // Invalid aggregate source column
         return nullptr;
     }
 
     template <Action TAction, DataType TDataType>
-    static TFind_callback_specialized get_specialized_callback_3(bool nullable)
+    static TFind_callback_specialized get_specialized_callback_3(bool is_nullable)
     {
-        if (nullable) {
+        if (is_nullable) {
             return &BaseType::template find_callback_specialization<TConditionFunction, TAction, TDataType, true>;
         }
         else {

src/realm/string_data.cpp
@@ -194,8 +194,10 @@ uint_least32_t realm::murmur2_32(const unsigned char* data, size_t len) noexcept
     switch (len) {
         case 3:
             h ^= data[2] << 16;
+            REALM_FALLTHROUGH;
         case 2:
             h ^= data[1] << 8;
+            REALM_FALLTHROUGH;
         case 1:
             h ^= data[0];
             h *= m;

src/realm/util/features.h
@@ -71,6 +71,8 @@
 
 #if REALM_HAS_CPP_ATTRIBUTE(clang::fallthrough)
 #define REALM_FALLTHROUGH [[clang::fallthrough]]
+#elif REALM_HAS_CPP_ATTRIBUTE(gnu::fallthrough)
+#define REALM_FALLTHROUGH [[gnu::fallthrough]]
 #elif REALM_HAS_CPP_ATTRIBUTE(fallthrough)
 #define REALM_FALLTHROUGH [[fallthrough]]
 #else

src/realm/util/memory_stream.hpp
@@ -116,11 +116,11 @@ inline MemoryInputStreambuf::~MemoryInputStreambuf() noexcept
 {
 }
 
-inline void MemoryInputStreambuf::set_buffer(const char* begin, const char* end) noexcept
+inline void MemoryInputStreambuf::set_buffer(const char* b, const char* e) noexcept
 {
-    m_begin = begin;
-    m_end = end;
-    m_curr = begin;
+    m_begin = b;
+    m_end = e;
+    m_curr = b;
 }
 
 
@@ -132,9 +132,9 @@ inline MemoryOutputStreambuf::~MemoryOutputStreambuf() noexcept
 {
 }
 
-inline void MemoryOutputStreambuf::set_buffer(char* begin, char* end) noexcept
+inline void MemoryOutputStreambuf::set_buffer(char* b, char* e) noexcept
 {
-    setp(begin, end);
+    setp(b, e);
 }
 
 inline size_t MemoryOutputStreambuf::size() const noexcept
@@ -152,31 +152,31 @@ inline MemoryInputStream::~MemoryInputStream() noexcept
 {
 }
 
-inline void MemoryInputStream::set_buffer(const char* begin, const char* end) noexcept
+inline void MemoryInputStream::set_buffer(const char* b, const char* e) noexcept
 {
-    m_streambuf.set_buffer(begin, end);
+    m_streambuf.set_buffer(b, e);
     clear();
 }
 
 template <size_t N> inline void MemoryInputStream::set_buffer(const char (&buffer)[N]) noexcept
 {
-    const char* begin = buffer;
-    const char* end = begin + N;
-    set_buffer(begin, end);
+    const char* b = buffer;
+    const char* e = b + N;
+    set_buffer(b, e);
 }
 
 inline void MemoryInputStream::set_string(const std::string& str) noexcept
 {
-    const char* begin = str.data();
-    const char* end = begin + str.size();
-    set_buffer(begin, end);
+    const char* b = str.data();
+    const char* e = b + str.size();
+    set_buffer(b, e);
 }
 
 inline void MemoryInputStream::set_c_string(const char* c_str) noexcept
 {
-    const char* begin = c_str;
-    const char* end = begin + traits_type::length(c_str);
-    set_buffer(begin, end);
+    const char* b = c_str;
+    const char* e = b + traits_type::length(c_str);
+    set_buffer(b, e);
 }
 
 
@@ -189,9 +189,9 @@ inline MemoryOutputStream::~MemoryOutputStream() noexcept
 {
 }
 
-inline void MemoryOutputStream::set_buffer(char* begin, char* end) noexcept
+inline void MemoryOutputStream::set_buffer(char* b, char* e) noexcept
 {
-    m_streambuf.set_buffer(begin, end);
+    m_streambuf.set_buffer(b, e);
     clear();
 }
 

test/test_thread.cpp
@@ -865,9 +865,9 @@ NONCONCURRENT_TEST(Thread_CondvarNotifyAllWakeup)
     std::mutex control_mutex;
     std::condition_variable control_cv;
 
-    const int num_waiters = 10;
+    const size_t num_waiters = 10;
     Thread waiters[num_waiters];
-    for (int i = 0; i < num_waiters; ++i) {
+    for (size_t i = 0; i < num_waiters; ++i) {
         waiters[i].start(std::bind(waiter, &mutex, &changed, &control_mutex, &control_cv, &num_threads_holding_lock));
     }
     {
@@ -880,7 +880,7 @@ NONCONCURRENT_TEST(Thread_CondvarNotifyAllWakeup)
     changed.notify_all();
     mutex.unlock();
 
-    for (int i = 0; i < num_waiters; ++i) {
+    for (size_t i = 0; i < num_waiters; ++i) {
         waiters[i].join();
     }
     changed.release_shared_part();

test/test_upgrade_database.cpp
@@ -609,11 +609,13 @@ TEST_IF(Upgrade_Database_Strings_With_NUL, REALM_MAX_BPNODE_SIZE == 4 || REALM_M
         switch (test_num) {
             case 0:
                 t->set_string(0, reserved_row_index, StringData("12345678901234567890")); // length == 20
+                break;
             case 1:
                 t->set_string(
                     0, reserved_row_index,
                     StringData(
                         "1234567890123456789012345678901234567890123456789012345678901234567890")); // length == 70
+                break;
             default:
                 break;
         }

commit 88de7c9e23be4a59c8b8ec0861ae2d266a3fb8a9

Fix potential problem in readdir() error handling (#3209)
This fixes a potential problem introduced in #3174, which could theoretically result in DirScanner skipping entries under certain error conditions.

This is likely the root cause for realm/realm-sync#2699.

The hypothesis is as follows: If readdir() temporarily sets errno, or errno was already set to EAGAIN from a previous system call, the retry loop would skip over valid directory entries even if no error occurred. This is fixed by checking the return value of readdir() before checking errno.

src/realm/util/file.cpp
@@ -1385,6 +1385,10 @@ DirScanner::~DirScanner() noexcept
 
 bool DirScanner::next(std::string& name)
 {
+#if !defined(__linux__) && !REALM_PLATFORM_APPLE && !REALM_WINDOWS && !REALM_UWP && !REALM_ANDROID
+#error "readdir() is not known to be thread-safe on this platform"
+#endif
+
     if (!m_dirp)
         return false;
 
@@ -1396,16 +1400,20 @@ bool DirScanner::next(std::string& name)
 
         struct dirent* dirent;
         do {
-            // readdir() does not seem to set errno=0 on success. The manpage
-            // recommends manually zeroing errno before calling it.
+            // readdir() signals both errors and end-of-stream by returning a
+            // null pointer. To distinguish between end-of-stream and errors,
+            // the manpage recommends setting errno specifically to 0 before
+            // calling it...
             errno = 0;
+
             dirent = readdir(m_dirp);
-        } while (errno == EAGAIN);
-        if (errno != 0) {
-            throw std::system_error(errno, std::system_category(), "readdir() failed");
-        }
-        if (!dirent)
+        } while (!dirent && errno == EAGAIN);
+
+        if (!dirent) {
+            if (errno != 0)
+                throw std::system_error(errno, std::generic_category(), "readdir() failed");
             return false; // End of stream
+        }
         const char* name_1 = dirent->d_name;
         std::string name_2 = name_1;
         if (name_2 != "." && name_2 != "..") {