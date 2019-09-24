6df316455a57a5cdd69b88b63488eabf7d7bb4fd
Finn Schiermer Andersen <fsa@realm.io>
2019-09-24 15:47:37 +0200

    bump to 5.23.5

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 import Foundation
 
-let versionStr = "5.23.4"
+let versionStr = "5.23.5"
 let versionPieces = versionStr.split(separator: "-")
 let versionCompontents = versionPieces[0].split(separator: ".")
 let versionExtra = versionPieces.count > 1 ? versionPieces[1] : ""
