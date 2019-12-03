5ced902928c082e1c5c6a66b178e0cfe53523517
James Stone <js@realm.io>
2019-12-02 16:26:15 -0800

    Attempt to get tests passing on android

--- tests/object.cpp
+++ tests/object.cpp
@@ -580,6 +580,7 @@ TEST_CASE("object") {
             change = c;
             callback_called = true;
         });
+
         advance_and_notify(*r);
 
         // First update unconditionally
@@ -587,6 +588,7 @@ TEST_CASE("object") {
 
         callback_called = false;
         advance_and_notify(*r);
+        coordinator.advance_to_latest(*r); // wait for async callbacks to complete
         REQUIRE(callback_called);
         REQUIRE_INDICES(change.modifications, 0, 1, 2, 3, 4);
 
