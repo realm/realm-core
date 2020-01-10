a5f4f0be8f6d41106a4f2b498ca4d28781514c1f
Jørgen Edelbo <jorgen.edelbo@mongodb.com>
2020-01-10 10:58:22 +0100

    Replication support for Decimal128
    Change-Id: Idafa66c76ef076381498b866c9b5560cec6a8aca

--- src/realm/impl/transact_log.hpp
+++ src/realm/impl/transact_log.hpp
@@ -342,6 +342,8 @@ public:
                                Instruction variant = instr_Set);
     virtual void set_object_id(const Table*, ColKey col_key, ObjKey key, ObjectId value,
                                Instruction variant = instr_Set);
+    virtual void set_decimal(const Table*, ColKey col_key, ObjKey key, Decimal128 value,
+                             Instruction variant = instr_Set);
     virtual void set_link(const Table*, ColKey col_key, ObjKey key, ObjKey value, Instruction variant = instr_Set);
     virtual void set_null(const Table*, ColKey col_key, ObjKey key, Instruction variant = instr_Set);
     virtual void insert_substring(const Table*, ColKey col_key, ObjKey key, size_t pos, StringData);
@@ -355,6 +357,7 @@ public:
     virtual void list_set_binary(const Lst<Binary>& list, size_t list_ndx, BinaryData value);
     virtual void list_set_timestamp(const Lst<Timestamp>& list, size_t list_ndx, Timestamp value);
     virtual void list_set_object_id(const Lst<ObjectId>& list, size_t list_ndx, ObjectId value);
+    virtual void list_set_decimal(const Lst<Decimal128>& list, size_t list_ndx, Decimal128 value);
 
     virtual void list_insert_int(const ConstLstBase& list, size_t list_ndx, int64_t value);
     virtual void list_insert_bool(const ConstLstBase& list, size_t list_ndx, bool value);
@@ -364,6 +367,7 @@ public:
     virtual void list_insert_binary(const Lst<Binary>& list, size_t list_ndx, BinaryData value);
     virtual void list_insert_timestamp(const Lst<Timestamp>& list, size_t list_ndx, Timestamp value);
     virtual void list_insert_object_id(const Lst<ObjectId>& list, size_t list_ndx, ObjectId value);
+    virtual void list_insert_decimal(const Lst<Decimal128>& list, size_t list_ndx, Decimal128 value);
 
     virtual void create_object(const Table*, GlobalKey);
     virtual void create_object(const Table*, ObjKey);
@@ -870,6 +874,12 @@ inline void TransactLogConvenientEncoder::set_object_id(const Table* t, ColKey c
     do_set(t, col_key, key, variant); // Throws
 }
 
+inline void TransactLogConvenientEncoder::set_decimal(const Table* t, ColKey col_key, ObjKey key, Decimal128,
+                                                      Instruction variant)
+{
+    do_set(t, col_key, key, variant); // Throws
+}
+
 inline void TransactLogConvenientEncoder::set_link(const Table* t, ColKey col_key, ObjKey key, ObjKey,
                                                    Instruction variant)
 {
@@ -957,6 +967,12 @@ inline void TransactLogConvenientEncoder::list_set_object_id(const Lst<ObjectId>
     m_encoder.list_set(list_ndx); // Throws
 }
 
+inline void TransactLogConvenientEncoder::list_set_decimal(const Lst<Decimal128>& list, size_t list_ndx, Decimal128)
+{
+    select_list(list);            // Throws
+    m_encoder.list_set(list_ndx); // Throws
+}
+
 inline bool TransactLogEncoder::list_insert(size_t list_ndx)
 {
     append_simple_instr(instr_ListInsert, list_ndx); // Throws
@@ -1007,6 +1023,13 @@ inline void TransactLogConvenientEncoder::list_insert_object_id(const Lst<Object
 
 inline void TransactLogConvenientEncoder::list_insert_timestamp(const Lst<Timestamp>& list, size_t list_ndx,
                                                                 Timestamp)
+{
+    select_list(list);               // Throws
+    m_encoder.list_insert(list_ndx); // Throws
+}
+
+inline void TransactLogConvenientEncoder::list_insert_decimal(const Lst<Decimal128>& list, size_t list_ndx,
+                                                              Decimal128)
 {
     select_list(list);               // Throws
     m_encoder.list_insert(list_ndx); // Throws
