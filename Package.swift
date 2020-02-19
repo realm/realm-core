fc90e503342ebd3028cc839031c8e0a70e0b6273
Jørgen Edelbo <jorgen.edelbo@mongodb.com>
2020-02-19 14:19:32 +0100

    Prepare release 6.0.3

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 import Foundation
 
-let versionStr = "6.0.2"
+let versionStr = "6.0.3"
 let versionPieces = versionStr.split(separator: "-")
 let versionCompontents = versionPieces[0].split(separator: ".")
 let versionExtra = versionPieces.count > 1 ? versionPieces[1] : ""
