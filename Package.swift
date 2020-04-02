555147c6cf40f3638a33b3a881456938c2613ef5
Jørgen Edelbo <jorgen.edelbo@mongodb.com>
2020-04-02 09:01:07 +0200

    Prepare release 10.0.0-alpha.4

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 import Foundation
 
-let versionStr = "10.0.0-alpha.3"
+let versionStr = "10.0.0-alpha.4"
 let versionPieces = versionStr.split(separator: "-")
 let versionCompontents = versionPieces[0].split(separator: ".")
 let versionExtra = versionPieces.count > 1 ? versionPieces[1] : ""
