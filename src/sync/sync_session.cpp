dbed5c5af1b497badd337eecc1b4adc7a1663578
Jørgen Edelbo <jorgen.edelbo@mongodb.com>
2020-04-29 14:03:55 +0200

    Update Core and Sync dependencies (#986)

--- src/sync/sync_session.cpp
+++ src/sync/sync_session.cpp
@@ -520,6 +520,7 @@ void SyncSession::handle_error(SyncError error)
             case ProtocolError::unsupported_session_feature:
             case ProtocolError::transact_before_upload:
             case ProtocolError::partial_sync_disabled:
+            case ProtocolError::user_mismatch:
                 break;
             // Session errors
             case ProtocolError::session_closed:
