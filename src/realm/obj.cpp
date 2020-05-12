cb514196ffe801934443b9b331bdb1d3c400ebb9
James Stone <js@realm.io>
2020-05-11 19:45:09 -0700

    fix warning

--- src/realm/obj.cpp
+++ src/realm/obj.cpp
@@ -79,7 +79,6 @@ Allocator& ConstObj::_get_alloc() const
 {
     // Bypass check of table instance version. To be used only in contexts,
     // where instance version match has already been established (e.g _get<>)
-    bool unused_var = false;
     return m_table.unchecked_ptr()->m_alloc;
 }
 
