d59cbac69bec3ecdcdf7a1562343cb7bbd40e3b1
Simon Ask Ulsnes <su@realm.io>
2020-06-30 14:40:56 +0200

    Obj::get_setbase_ptr

--- src/realm/set.cpp
+++ src/realm/set.cpp
@@ -18,17 +18,87 @@
 
 
 #include "realm/set.hpp"
-#include "realm/array_mixed.hpp"
-#include "realm/array_object_id.hpp"
+#include "realm/array_basic.hpp"
+#include "realm/array_integer.hpp"
+#include "realm/array_bool.hpp"
+#include "realm/array_string.hpp"
+#include "realm/array_binary.hpp"
+#include "realm/array_timestamp.hpp"
 #include "realm/array_decimal128.hpp"
+#include "realm/array_object_id.hpp"
+#include "realm/array_typed_link.hpp"
+#include "realm/array_mixed.hpp"
 #include "realm/replication.hpp"
 
 namespace realm {
 
+// FIXME: This method belongs in obj.cpp.
 SetBasePtr Obj::get_setbase_ptr(ColKey col_key) const
 {
-    REALM_TERMINATE("Not implemented yet");
-    return SetBasePtr{};
+    auto attr = get_table()->get_column_attr(col_key);
+    REALM_ASSERT(attr.test(col_attr_Set));
+    bool nullable = attr.test(col_attr_Nullable);
+
+    switch (get_table()->get_column_type(col_key)) {
+        case type_Int: {
+            if (nullable)
+                return std::make_unique<Set<util::Optional<Int>>>(*this, col_key);
+            else
+                return std::make_unique<Set<Int>>(*this, col_key);
+        }
+        case type_Bool: {
+            if (nullable)
+                return std::make_unique<Set<util::Optional<Bool>>>(*this, col_key);
+            else
+                return std::make_unique<Set<Bool>>(*this, col_key);
+        }
+        case type_Float: {
+            if (nullable)
+                return std::make_unique<Set<util::Optional<Float>>>(*this, col_key);
+            else
+                return std::make_unique<Set<Float>>(*this, col_key);
+        }
+        case type_Double: {
+            if (nullable)
+                return std::make_unique<Set<util::Optional<Double>>>(*this, col_key);
+            else
+                return std::make_unique<Set<Double>>(*this, col_key);
+        }
+        case type_String: {
+            return std::make_unique<Set<String>>(*this, col_key);
+        }
+        case type_Binary: {
+            return std::make_unique<Set<Binary>>(*this, col_key);
+        }
+        case type_Timestamp: {
+            return std::make_unique<Set<Timestamp>>(*this, col_key);
+        }
+        case type_Decimal: {
+            return std::make_unique<Set<Decimal128>>(*this, col_key);
+        }
+        case type_ObjectId: {
+            if (nullable)
+                return std::make_unique<Set<util::Optional<ObjectId>>>(*this, col_key);
+            else
+                return std::make_unique<Set<ObjectId>>(*this, col_key);
+        }
+        case type_TypedLink: {
+            return std::make_unique<Set<ObjLink>>(*this, col_key);
+        }
+        case type_Mixed: {
+            return std::make_unique<Set<Mixed>>(*this, col_key);
+        }
+        case type_LinkList:
+            [[fallthrough]];
+        case type_Link:
+            [[fallthrough]];
+        case type_OldDateTime:
+            [[fallthrough]];
+        case type_OldTable:
+            REALM_ASSERT(false);
+            break;
+    }
+    return {};
 }
 
 } // namespace realm
