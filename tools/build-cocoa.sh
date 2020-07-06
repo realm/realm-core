80d68c1413f60ffafebbfa4f79073926c17853d9
Lee Maguire <lee.maguire@mongodb.com>
2020-07-06 10:13:00 +0100

    Fix issue where arch slices was overriding fat library

--- tools/build-cocoa.sh
+++ tools/build-cocoa.sh
@@ -93,14 +93,14 @@ for bt in "${BUILD_TYPES[@]}"; do
                 watchos) SDK="watch";;
                 tvos) SDK="appletv";;
             esac
-            tar -C core -zxvf "${filename}" "lib/librealm-${SDK}os${suffix}.a"
-            mv "core/lib/librealm-${SDK}os${suffix}.a" "core/librealm-${SDK}os${suffix}.a"
-            tar -C core -zxvf "${filename}" "lib/librealm-parser-${SDK}os${suffix}.a"
-            mv "core/lib/librealm-parser-${SDK}os${suffix}.a" "core/librealm-parser-${SDK}os${suffix}.a"
-            tar -C core -zxvf "${filename}" "lib/librealm-${SDK}simulator${suffix}.a"
-            mv "core/lib/librealm-${SDK}simulator${suffix}.a" "core/librealm-${SDK}simulator${suffix}.a"
-            tar -C core -zxvf "${filename}" "lib/librealm-parser-${SDK}simulator${suffix}.a"
-            mv "core/lib/librealm-parser-${SDK}simulator${suffix}.a" "core/librealm-parser-${SDK}simulator${suffix}.a"
+            tar -C core -zxvf "${filename}" "lib/librealm-${SDK}-device${suffix}.a"
+            mv "core/lib/librealm-${SDK}-device${suffix}.a" "core/librealm-${SDK}-device${suffix}.a"
+            tar -C core -zxvf "${filename}" "lib/librealm-parser-${SDK}-device${suffix}.a"
+            mv "core/lib/librealm-parser-${SDK}-device${suffix}.a" "core/librealm-parser-${SDK}-device${suffix}.a"
+            tar -C core -zxvf "${filename}" "lib/librealm-${SDK}-simulator${suffix}.a"
+            mv "core/lib/librealm-${SDK}-simulator${suffix}.a" "core/librealm-${SDK}-simulator${suffix}.a"
+            tar -C core -zxvf "${filename}" "lib/librealm-parser-${SDK}-simulator${suffix}.a"
+            mv "core/lib/librealm-parser-${SDK}-simulator${suffix}.a" "core/librealm-parser-${SDK}-simulator${suffix}.a"
         fi
         rm -rf core/lib
     done
@@ -119,18 +119,13 @@ if [[ ! -z $BUILD_XCFRAMEWORK ]]; then
                 build_dir="core/librealm-maccatalyst${suffix}.a"
                 make_core_xcframework+=( -library ${build_dir} -headers core/include/)
             elif [[ "$p" == "ios" ]]; then
-                build_dir_device="core/librealm-iphoneos${suffix}.a"
-                build_dir_simulator="core/librealm-iphonesimulator${suffix}.a"
-                make_core_xcframework+=( -library ${build_dir_device} -headers core/include/)
-                make_core_xcframework+=( -library ${build_dir_simulator} -headers core/include/)
-            elif [[ "$p" == "watchos" ]]; then
-                build_dir_device="core/librealm-watchos${suffix}.a"
-                build_dir_simulator="core/librealm-watchsimulator${suffix}.a"
-                make_core_xcframework+=( -library ${build_dir_device} -headers core/include/)
-                make_core_xcframework+=( -library ${build_dir_simulator} -headers core/include/)
-            elif [[ "$p" == "tvos" ]]; then
-                build_dir_device="core/librealm-appletvos${suffix}.a"
-                build_dir_simulator="core/librealm-appletvsimulator${suffix}.a"
+                case "${p}" in
+                    ios) SDK="iphone";;
+                    watchos) SDK="watch";;
+                    tvos) SDK="appletv";;
+                esac
+                build_dir_device="core/librealm-${SDK}-device${suffix}.a"
+                build_dir_simulator="core/librealm-${SDK}-simulator${suffix}.a"
                 make_core_xcframework+=( -library ${build_dir_device} -headers core/include/)
                 make_core_xcframework+=( -library ${build_dir_simulator} -headers core/include/)
             fi
