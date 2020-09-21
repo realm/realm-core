bbb5d0cff857bcb65c8b4d0dc3735025f41643ec
Yavor Georgiev <fealebenpae@gmail.com>
2020-09-21 14:24:40 +0200

    Refactor CMake
    - Remove the Objects and Shared targets for Storage and QueryParser. Everything is now a static library
     - Remove ${PLATFORM_LIBRARIES} and ${PLATFORM_DEFINES}. Those have been replaced with INTERFACE and PUBLIC compiler and linker flags on the relevant targets that flow into consumers. For example, Sync adds an INTERFACE dependency on ZLIB, which removed the need to add ZLIB to ${PLATFORM_LIBRARIES} and use that list in every target tha needs to link Sync
     - Remove the `realm-` alias. Added the namespace alias so that downstream consumers can use the same target names regardless if they add_subdirectory the codebase or use release tarballs with exported targets
     - Change the namespace and release tarball name from RealmCore to just Realm
     - All the targets are exported in the same release tarball now
     - Install locations do not rely on GNUInstallDirs anymore, because we’re never making system-installable packages, but only CMake modules
     - Remove the realm-config executable and its attendant defines in config.h. We do not support system-wide installations.
     - Rename some misc targets to make them more consistent

--- tools/cmake/Findprocps.cmake
+++ tools/cmake/Findprocps.cmake
@@ -1,16 +1,48 @@
-# - try to find procps directories and libraries
-#
-# Once done this will define:
-#
-#  PROCPS_FOUND
-#  PROCPS_INCLUDE_DIR
-#  PROCPS_LIBRARY
-#
+# taken from https://github.com/cvmfs/cvmfs/commit/8ee3a8d7764ae25efdbdadf76a77ad5c23291deb
 
-include (FindPackageHandleStandardArgs)
+# --------------------------------------------------------------------
+# - FindProcPS
+#
+# This module looks for the PROCPS software and defines
+#    PROCPS_FOUND        , set to TRUE if the PROCPS is found
+#    PROCPS_INCLUDE_DIR  , include directories for PROCPS
+#    PROCPS_LIBRARY_DIR  , link directories for PROCPS libraries
+#    PROCPS_LIBRARY      , the PROCPS libraries
+# --------------------------------------------------------------------
 
-if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-  find_path(PROCPS_INCLUDE_DIR proc/readproc.h)
-  find_library(PROCPS_LIBRARY NAMES proc procps)
-  find_package_handle_standard_args(procps DEFAULT_MSG PROCPS_LIBRARY PROCPS_INCLUDE_DIR)
+# Include directory finding process
+find_path(
+    PROCPS_INCLUDE_DIR
+    NAMES          procps.h readproc.h
+    PATH_SUFFIXES  proc
+    PATHS          /usr/local/include /usr/include
+    DOC            "ProcPS library include path"
+)
+
+# Library finding process
+find_library(
+    PROCPS_LIBRARY
+    NAMES  proc
+    PATHS  /lib64 /lib /usr/lib64 /usr/lib /usr/local/lib64 /usr/local/lib
+    DOC    "ProcPS library location"
+)
+
+if(PROCPS_LIBRARY)
+    if(NOT TARGET PROCPS::PROCPS)
+        add_library(PROCPS::PROCPS UNKNOWN IMPORTED)
+        set_target_properties(PROCPS::PROCPS PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${PROCPS_INCLUDE_DIR}"
+            IMPORTED_LOCATION "${PROCPS_LIBRARY}"
+        )
+    endif()
 endif()
+
+# Handle the QUIETLY and REQUIRED arguments and set PROCPS_FOUND to TRUE
+# if all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(PROCPS DEFAULT_MSG
+    PROCPS_INCLUDE_DIR
+    PROCPS_LIBRARY
+)
+
+mark_as_advanced(PROCPS_INCLUDE_DIR PROCPS_LIBRARY)
