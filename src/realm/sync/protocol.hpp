af238fb2133f410943f222f4bc8bfa2e8ca686d7
Simon Ask Ulsnes <simon.ulsnes@mongodb.com>
2020-11-03 12:20:17 +0100

    Bump protocol version

--- src/realm/sync/protocol.hpp
+++ src/realm/sync/protocol.hpp
@@ -18,12 +18,14 @@ namespace sync {
 //   1 Initial version, matching io.realm.sync-30, but not including query-based
 //     sync, serialized transactions, and state realms (async open).
 //
+//   2 Restored erase-always-wins OT behavior.
+//
 //  XX Changes:
 //     - Add support for Mixed and TypedLinks columns.
 //
 constexpr int get_current_protocol_version() noexcept
 {
-    return 1;
+    return 2;
 }
 
 constexpr const char* get_websocket_protocol_prefix() noexcept
