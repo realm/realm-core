a413cdd31c1a10699e47b55584efb307dde7c2d9
Jonathan Reams <jonathan@reamstronics.net>
2020-11-05 11:16:28 -0500

    add comment

--- test/object-store/bson.cpp
+++ test/object-store/bson.cpp
@@ -41,6 +41,8 @@ static inline std::string remove_whitespace(const char* c)
 /**
  ======== BSON CORPUS ========
  */
+// FIXME(BUILD-12151) the argument to the corpus check function is an r-value reference to work around
+// a bug in the version of gcc in the v3 mongodb toolchain.
 template <typename T>
 using CorpusCheck = void (*)(T&&);
 
