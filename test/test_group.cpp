1f031df867825752678f8c29d0fbea03bf17bed1
Dominic Frei <dominic.frei@mongodb.com>
2021-02-15 15:20:37 +0000

    Use CHECK_THROW.

--- test/test_group.cpp
+++ test/test_group.cpp
@@ -1618,7 +1618,7 @@ TEST(Group_ChangeEmbeddedness)
     p2.set(col, obj2.get_key());
 
     // obj2 has no owner, so we can't make the table embedded
-    REQUIRE_THROWS(t->set_embedded(true));
+    CHECK_THROW(t->set_embedded(true));
     CHECK_NOT(t->is_embedded());
 
     // Now it has owner
@@ -1632,7 +1632,7 @@ TEST(Group_ChangeEmbeddedness)
 
     // Now obj2 has 2 parents
     CHECK_EQUAL(obj2.get_backlink_count(), 2);
-    REQUIRE_THROWS(t->set_embedded(true));
+    CHECK_THROW(t->set_embedded(true));
     CHECK_NOT(t->is_embedded());
 }
 
