d1dbad765800049fc59212df8de25a832ad00d2e
James Stone <james.stone@mongodb.com>
2021-03-04 11:50:58 -0800

    version bump to 10.5.4

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 import Foundation
 
-let versionStr = "10.5.3"
+let versionStr = "10.5.4"
 let versionPieces = versionStr.split(separator: "-")
 let versionCompontents = versionPieces[0].split(separator: ".")
 let versionExtra = versionPieces.count > 1 ? versionPieces[1] : ""
