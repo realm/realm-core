3c567c3ffd008c085df35c175237ec0eab9a361e
Jørgen Edelbo <jorgen.edelbo@mongodb.com>
2021-04-21 12:07:43 +0200

    Add failing test

--- test/test_query2.cpp
+++ test/test_query2.cpp
@@ -5847,6 +5847,8 @@ TEST_TYPES(Query_Mixed, std::true_type, std::false_type)
     table->get_object(28).set(col_any, Mixed(BinaryData(str2bin)));
     table->get_object(25).set(col_any, Mixed(3.));
     table->get_object(35).set(col_any, Mixed(Decimal128("3")));
+    table->get_object(80).set(col_any, Mixed("abcdefgh"));
+    table->get_object(81).set(col_any, Mixed(int64_t(0x6867666564636261)));
 
     auto it = table->begin();
     for (int64_t i = 0; i < 10; i++) {
@@ -5902,12 +5904,12 @@ TEST_TYPES(Query_Mixed, std::true_type, std::false_type)
     CHECK_EQUAL(tv.size(), 3);
 
     tv = table->where().contains(col_any, StringData("TRIN"), false).find_all();
-    CHECK_EQUAL(tv.size(), 25);
+    CHECK_EQUAL(tv.size(), 24);
     tv = table->where().contains(col_any, Mixed("TRIN"), false).find_all();
-    CHECK_EQUAL(tv.size(), 25);
+    CHECK_EQUAL(tv.size(), 24);
 
     tv = table->where().like(col_any, StringData("Strin*")).find_all();
-    CHECK_EQUAL(tv.size(), 25);
+    CHECK_EQUAL(tv.size(), 24);
 
     tv = table->where().ends_with(col_any, StringData("4")).find_all(); // 4, 24, 44, 64, 84
     CHECK_EQUAL(tv.size(), 5);
@@ -5929,7 +5931,10 @@ TEST_TYPES(Query_Mixed, std::true_type, std::false_type)
     tv = (table->column<Mixed>(col_any) == 3.).find_all();
     CHECK_EQUAL(tv.size(), 3);
     tv = (table->column<Mixed>(col_any) == table->column<Int>(col_int)).find_all();
-    CHECK_EQUAL(tv.size(), 72);
+    CHECK_EQUAL(tv.size(), 71);
+
+    tv = (table->column<Mixed>(col_any) == StringData("abcdefgh")).find_all();
+    CHECK_EQUAL(tv.size(), 1);
 
     ObjLink link_to_first = table->begin()->get_link();
     tv = (origin->column<Mixed>(col_mixed) == Mixed(link_to_first)).find_all();
