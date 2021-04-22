10144c2abb904a57b5849045ff61db9368030cbb
Nikola Irinchev <irinchev@me.com>
2021-04-22 13:21:42 +0200

    Only disable checks for MSVC

--- src/realm/sync/changeset.cpp
+++ src/realm/sync/changeset.cpp
@@ -58,7 +58,9 @@ InternString Changeset::find_string(StringData string) const noexcept
     return InternString{};
 }
 
+#if _MSC_VER
 #pragma runtime_checks("", off)
+#endif
 PrimaryKey Changeset::get_key(const Instruction::PrimaryKey& key) const noexcept
 {
     const auto& get = overload{
@@ -71,7 +73,9 @@ PrimaryKey Changeset::get_key(const Instruction::PrimaryKey& key) const noexcept
     };
     return mpark::visit(get, key);
 }
-#pragma runtime_checks("", restore)
+#if _MSC_VER
+#pragma runtime_checks("", off)
+#endif
 
 bool Changeset::operator==(const Changeset& that) const noexcept
 {
