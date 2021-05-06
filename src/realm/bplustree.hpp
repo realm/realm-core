0fdba41918f875ce2a5ced3cfa506d2e2fd6c40e
Simon Ask Ulsnes <simon.ulsnes@mongodb.com>
2021-05-06 11:16:16 +0200

    bptree maximum/minimum set return_ndx to not_found

--- src/realm/bplustree.hpp
+++ src/realm/bplustree.hpp
@@ -674,6 +674,8 @@ ColumnMinMaxType<T> bptree_maximum(const BPlusTree<T>& tree, size_t* return_ndx 
     using ResultType = typename AggregateResultType<T, act_Max>::result_type;
     ResultType max = std::numeric_limits<ResultType>::lowest();
     if (tree.size() == 0) {
+        if (return_ndx)
+            *return_ndx = not_found;
         return max;
     }
 
@@ -706,6 +708,8 @@ ColumnMinMaxType<T> bptree_minimum(const BPlusTree<T>& tree, size_t* return_ndx 
     using ResultType = typename AggregateResultType<T, act_Max>::result_type;
     ResultType min = std::numeric_limits<ResultType>::max();
     if (tree.size() == 0) {
+        if (return_ndx)
+            *return_ndx = not_found;
         return min;
     }
 
