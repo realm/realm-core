9366da579b13507fef1359b53b173cfbff544da0
Jørgen Edelbo <jorgen.edelbo@mongodb.com>
2021-05-11 09:58:12 +0200

    Update table_view.hpp

--- src/realm/table_view.hpp
+++ src/realm/table_view.hpp
@@ -406,7 +406,7 @@ protected:
     size_t m_limit = size_t(-1);
 
     // FIXME: This class should eventually be replaced by std::vector<ObjKey>
-    // It implements a vector of ObjKey, where the elements are held in the 
+    // It implements a vector of ObjKey, where the elements are held in the
     // heap (default allocator is the only option)
     class KeyValues : public KeyColumn {
     public:
