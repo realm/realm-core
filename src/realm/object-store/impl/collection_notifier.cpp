e4ccf9508b93988e5c699d74c8fea70ebc3a21ec
James Stone <james.stone@mongodb.com>
2021-05-11 11:48:43 -0700

    null is inclusive of unresolved links

--- src/realm/object-store/impl/collection_notifier.cpp
+++ src/realm/object-store/impl/collection_notifier.cpp
@@ -129,10 +129,10 @@ bool DeepChangeChecker::check_outgoing_links(TableKey table_key, Table const& ta
         if (already_checking(link.col_key))
             return false;
         if (!link.is_list) {
-            if (obj.is_null(ColKey(link.col_key)) || obj.is_unresolved(ColKey(link.col_key)))
+            ObjKey dst_key = obj.get<ObjKey>(ColKey(link.col_key));
+            if (!dst_key) // do not descend into a null or unresolved link
                 return false;
-            auto dst = obj.get<ObjKey>(ColKey(link.col_key)).value;
-            return check_row(*table.get_link_target(ColKey(link.col_key)), dst, depth + 1);
+            return check_row(*table.get_link_target(ColKey(link.col_key)), dst_key.value, depth + 1);
         }
 
         auto& target = *table.get_link_target(ColKey(link.col_key));
