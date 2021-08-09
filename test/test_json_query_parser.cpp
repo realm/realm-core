ec946121987e3a37a0eed764776cb153921ba489
Jacob <JacobOscarGunnarsson>
2021-08-09 14:01:33 +0200

    add tests for sorting, commutativity, logical operations

--- test/test_json_query_parser.cpp
+++ test/test_json_query_parser.cpp
@@ -1,46 +1,70 @@
 
 #include "test.hpp"
+#include <realm.hpp>
 #include <realm/parser/driver.hpp>
 
 using namespace realm;
 using json = nlohmann::json;
 
-auto base = R"(
-{
-  "whereClauses": [],
-  "orderingClauses": []
-}
-)"_json;
-
+//basic consts and props
 json int_const = { {"kind", "constant"}, {"value", 3}, {"type", "int"} };
 json string_const = { {"kind", "constant"}, {"value", "Bob"}, {"type", "string"} };
 json float_const = { {"kind", "constant"}, {"value", 2.22}, {"type", "float"} };
 json int_prop = { {"kind", "property"}, {"name", "age"}, {"type", "int"} };
 json string_prop = { {"kind", "property"}, {"name", "name"}, {"type", "string"} };
 json float_prop = { {"kind", "property"}, {"name", "fee"}, {"type", "float"} };
+
+//string_ops consts
+json begins_with_const = { {"kind", "constant"}, {"value", "Bi"}, {"type", "string"} };
+json ends_with_const = { {"kind", "constant"}, {"value", "e"}, {"type", "string"} };
+json contains_const = { {"kind", "constant"}, {"value", "J"}, {"type", "string"} };
+json like_const = { {"kind", "constant"}, {"value", "*e"}, {"type", "string"} };
+
 //int comparisons
-json int_eq_sample = { {"kind", "eq"}, {"left", int_prop}, {"right", int_const}};
-json int_neq_sample = { {"kind", "neq"}, {"left", int_prop}, {"right", int_const}};
-json int_gt_sample = { {"kind", "gt"}, {"left", int_prop}, {"right", int_const}};
-json int_gte_sample = { {"kind", "gte"}, {"left", int_prop}, {"right", int_const}};
-json int_lt_sample = { {"kind", "lt"}, {"left", int_prop}, {"right", int_const}};
-json int_lte_sample = { {"kind", "lte"}, {"left", int_prop}, {"right", int_const}};
+json int_eq = { {"kind", "eq"}, {"left", int_prop}, {"right", int_const}};
+json int_neq = { {"kind", "neq"}, {"left", int_prop}, {"right", int_const}};
+json int_gt = { {"kind", "gt"}, {"left", int_prop}, {"right", int_const}};
+json int_gte = { {"kind", "gte"}, {"left", int_prop}, {"right", int_const}};
+json int_lt = { {"kind", "lt"}, {"left", int_prop}, {"right", int_const}};
+json int_lte = { {"kind", "lte"}, {"left", int_prop}, {"right", int_const}};
+
 //string comparisons
-json string_eq_sample = { {"kind", "eq"}, {"left", string_prop}, {"right", string_const}};
-json string_neq_sample = { {"kind", "neq"}, {"left", string_prop}, {"right", string_const}};
-json string_gt_sample = { {"kind", "gt"}, {"left", string_prop}, {"right", string_const}};
-json string_gte_sample = { {"kind", "gte"}, {"left", string_prop}, {"right", string_const}};
-json string_lt_sample = { {"kind", "lt"}, {"left", string_prop}, {"right", string_const}};
-json string_lte_sample = { {"kind", "lte"}, {"left", string_prop}, {"right", string_const}};
+json string_eq = { {"kind", "eq"}, {"left", string_prop}, {"right", string_const}};
+json string_neq = { {"kind", "neq"}, {"left", string_prop}, {"right", string_const}};
+json string_gt = { {"kind", "gt"}, {"left", string_prop}, {"right", string_const}};
+json string_gte = { {"kind", "gte"}, {"left", string_prop}, {"right", string_const}};
+json string_lt = { {"kind", "lt"}, {"left", string_prop}, {"right", string_const}};
+json string_lte = { {"kind", "lte"}, {"left", string_prop}, {"right", string_const}};
+
 //float comparisons
-json float_eq_sample = { {"kind", "eq"}, {"left", float_prop}, {"right", float_const}};
-json float_neq_sample = { {"kind", "neq"}, {"left", float_prop}, {"right", float_const}};
-json float_gt_sample = { {"kind", "gt"}, {"left", float_prop}, {"right", float_const}};
-json float_gte_sample = { {"kind", "gte"}, {"left", float_prop}, {"right", float_const}};
-json float_lt_sample = { {"kind", "lt"}, {"left", float_prop}, {"right", float_const}};
-json float_lte_sample = { {"kind", "lte"}, {"left", float_prop}, {"right", float_const}};
+json float_eq = { {"kind", "eq"}, {"left", float_prop}, {"right", float_const}};
+json float_neq = { {"kind", "neq"}, {"left", float_prop}, {"right", float_const}};
+json float_gt = { {"kind", "gt"}, {"left", float_prop}, {"right", float_const}};
+json float_gte = { {"kind", "gte"}, {"left", float_prop}, {"right", float_const}};
+json float_lt = { {"kind", "lt"}, {"left", float_prop}, {"right", float_const}};
+json float_lte = { {"kind", "lte"}, {"left", float_prop}, {"right", float_const}};
 
+//commutative expressions
+json int_commutative_eq = { {"kind", "eq"}, {"left", int_const}, {"right", int_prop}};
+json float_commutative_eq = { {"kind", "eq"}, {"left", float_const}, {"right", float_prop}};
+json string_commutative_eq = { {"kind", "eq"}, {"left", string_const}, {"right", string_prop}};
+json int_commutative_neq = { {"kind", "neq"}, {"left", int_const}, {"right", int_prop}};
+json float_commutative_neq = { {"kind", "neq"}, {"left", float_const}, {"right", float_prop}};
+json string_commutative_neq = { {"kind", "neq"}, {"left", string_const}, {"right", string_prop}};
 
+//string operations
+json string_begins_with = { {"kind", "beginsWith"}, {"left", string_prop}, {"right", begins_with_const}};
+json string_ends_with = { {"kind", "endsWith"}, {"left", string_prop}, {"right", ends_with_const}};
+json string_contains = { {"kind", "contains"}, {"left", string_prop}, {"right", contains_const}};
+json string_like = { {"kind", "like"}, {"left", string_prop}, {"right", like_const}};
+
+//sort operations
+json sort_int_asc {{"isAscending", true}, {"property", "age"}};
+json sort_int_desc {{"isAscending", false}, {"property", "age"}};
+json sort_string_asc {{"isAscending", true}, {"property", "name"}};
+json sort_string_desc {{"isAscending", false}, {"property", "name"}};
+json sort_float_asc {{"isAscending", true}, {"property", "fee"}};
+json sort_float_desc {{"isAscending", false}, {"property", "fee"}};
 
 
 Query verify_query(test_util::unit_test::TestContext& test_context, TableRef table, json json, size_t num_results)
@@ -56,8 +80,7 @@ Query verify_query(test_util::unit_test::TestContext& test_context, TableRef tab
     return q;
 }
 
-
-json construct_simple_query(json comparison){
+json simple_query(json comparison){
     json e = json::object();
     e["expression"] = comparison;
     json j = json::object();
@@ -66,18 +89,15 @@ json construct_simple_query(json comparison){
 }
 
 
-TEST(test_comparisons)
+TEST(test_json_query_parser_simple)
 {
-    //construct table
     Group g;
     std::string table_name = "person";
     TableRef t = g.add_table(table_name);
-    auto int_col_key = t->add_column(type_Int, "age");
+    t->add_column(type_Int, "age");
     t->add_column(type_String, "name");
     t->add_column(type_Float, "fee", true);
-    t->add_search_index(int_col_key);
 
-    //populate table
     std::vector<std::string> names = {"Billy", "Bob", "Joe", "Jane", "Joel"};
     std::vector<double> fees = {2.0, 2.23, 2.22, 2.25, 3.73};
     std::vector<ObjKey> keys;
@@ -86,29 +106,234 @@ TEST(test_comparisons)
         t->get_object(keys[i]).set_all(int(i), StringData(names[i]), float(fees[i]));
     }
 
-    //test
-    verify_query(test_context, t, construct_simple_query(int_eq_sample), 1);
-    verify_query(test_context, t, construct_simple_query(int_neq_sample), 4);
-    verify_query(test_context, t, construct_simple_query(int_gt_sample), 1);
-    verify_query(test_context, t, construct_simple_query(int_gte_sample), 2);
-    verify_query(test_context, t, construct_simple_query(int_lt_sample), 3);
-    verify_query(test_context, t, construct_simple_query(int_lte_sample), 4);
+    verify_query(test_context, t, simple_query(int_eq), 1);
+    verify_query(test_context, t, simple_query(int_neq), 4);
+    verify_query(test_context, t, simple_query(int_gt), 1);
+    verify_query(test_context, t, simple_query(int_gte), 2);
+    verify_query(test_context, t, simple_query(int_lt), 3);
+    verify_query(test_context, t, simple_query(int_lte), 4);    
+    
+    verify_query(test_context, t, simple_query(string_eq), 1);
+    verify_query(test_context, t, simple_query(string_neq), 4);
+    verify_query(test_context, t, simple_query(string_gt), 3);
+    verify_query(test_context, t, simple_query(string_gte), 4);
+    verify_query(test_context, t, simple_query(string_lt), 1);
+    verify_query(test_context, t, simple_query(string_lte), 2);
+    
+    verify_query(test_context, t, simple_query(float_eq), 1);
+    verify_query(test_context, t, simple_query(float_neq), 4);
+    verify_query(test_context, t, simple_query(float_gt), 3);
+    verify_query(test_context, t, simple_query(float_gte), 4);
+    verify_query(test_context, t, simple_query(float_lt), 1);
+    verify_query(test_context, t, simple_query(float_lte), 2);
 
+    verify_query(test_context, t, simple_query(int_commutative_eq), 1);
+    verify_query(test_context, t, simple_query(string_commutative_eq), 1);
+    verify_query(test_context, t, simple_query(float_commutative_eq), 1);
+    verify_query(test_context, t, simple_query(int_commutative_neq), 4);
+    verify_query(test_context, t, simple_query(string_commutative_neq), 4);
+    verify_query(test_context, t, simple_query(float_commutative_neq), 4);
 
-    verify_query(test_context, t, construct_simple_query(string_eq_sample), 1);
-    verify_query(test_context, t, construct_simple_query(string_neq_sample), 4);
-    verify_query(test_context, t, construct_simple_query(string_gt_sample), 3);
-    verify_query(test_context, t, construct_simple_query(string_gte_sample), 4);
-    verify_query(test_context, t, construct_simple_query(string_lt_sample), 1);
-    verify_query(test_context, t, construct_simple_query(string_lte_sample), 2);
-
-    verify_query(test_context, t, construct_simple_query(float_eq_sample), 1);
-    verify_query(test_context, t, construct_simple_query(float_neq_sample), 4);
-    verify_query(test_context, t, construct_simple_query(float_gt_sample), 3);
-    verify_query(test_context, t, construct_simple_query(float_gte_sample), 4);
-    verify_query(test_context, t, construct_simple_query(float_lt_sample), 1);
-    verify_query(test_context, t, construct_simple_query(float_lte_sample), 2);
+    verify_query(test_context, t, simple_query(string_begins_with), 1);
+    verify_query(test_context, t, simple_query(string_ends_with), 2);
+    verify_query(test_context, t, simple_query(string_contains), 3);
+    verify_query(test_context, t, simple_query(string_like), 2);
 }
 
+json logical_query(std::string kind, json pred1, json pred2){
+    json expr = {{"kind", kind}, {"left", pred1}, {"right", pred2}};
+    json e = json::object();
+    e["expression"] = expr;
+    json j = json::object();
+    j["whereClauses"] = json::array({e});
+    return j;
+}
 
-//test commutativity
+json not_query(json pred){
+    json not_sample = {{"kind", "not"}, {"expression", pred}};
+    json e = json::object();
+    e["expression"] = not_sample;
+    json j = json::object();
+    j["whereClauses"] = json::array({e});
+    return j;
+}
+
+TEST(test_json_query_parser_logical){
+    Group g;
+    std::string table_name = "person";
+    TableRef t = g.add_table(table_name);
+    t->add_column(type_Int, "age");
+    t->add_column(type_String, "name");
+    t->add_column(type_Float, "fee", true);
+
+    std::vector<std::string> names = {"Billy", "Bob", "Joe", "Jane", "Joel"};
+    std::vector<double> fees = {2.0, 2.23, 2.22, 2.25, 3.73};
+    std::vector<ObjKey> keys;
+    t->create_objects(5, keys);
+    for (size_t i = 0; i < t->size(); ++i) {
+        t->get_object(keys[i]).set_all(int(i), StringData(names[i]), float(fees[i]));
+    }
+    verify_query(test_context, t, logical_query("and", string_lt, int_lt), 1);
+    verify_query(test_context, t, logical_query("and", float_gt, int_eq), 1);
+    verify_query(test_context, t, logical_query("and", float_eq, string_eq), 0);
+    verify_query(test_context, t, logical_query("or", string_lt, int_lt), 3);
+    verify_query(test_context, t, logical_query("or", int_gte, int_lt), 5);
+    verify_query(test_context, t, logical_query("or", string_neq, float_lte), 4);
+    verify_query(test_context, t, not_query(string_begins_with), 4);
+    verify_query(test_context, t, not_query(string_neq), 1);
+    verify_query(test_context, t, not_query(float_gt), 2);
+    verify_query(test_context, t, not_query(int_lte), 1);
+}
+
+TableView get_sorted_view(TableRef t, json json)
+{
+    JsonQueryParser parser;
+    Query q = parser.query_from_json(t, json);
+    return q.find_all();
+}
+
+json simple_query_sort(json comparison, std::vector<json> sorts){
+    json query = simple_query(comparison);
+    query["orderingClauses"] = json::array();
+    for (json s : sorts){
+        query["orderingClauses"].emplace_back(s);
+    } 
+    return query;
+}
+
+TEST(test_json_query_parser_sorting){
+    Group g;
+    std::string table_name = "person";
+    TableRef t = g.add_table(table_name);
+    ColKey age_col = t->add_column(type_Int, "age");
+    ColKey name_col = t->add_column(type_String, "name");
+    ColKey fee_col = t->add_column(type_Float, "fee", true);
+
+    std::vector<std::string> names = {"Billy", "Bob", "Joe", "Jane", "Joel"};
+    std::vector<double> fees = {2.0, 2.22, 2.25, 2.25, 3.73};
+    std::vector<ObjKey> keys;
+    t->create_objects(5, keys);
+    for (size_t i = 0; i < t->size(); ++i) {
+        t->get_object(keys[i]).set_all(int(i), StringData(names[i]), float(fees[i]));
+    }
+
+    // person:                      
+    // name     age     fee 
+    // Billy     0      2.0 
+    // Bob       1      2.22 
+    // Joe       2      2.25      
+    // Jane      3      2.25 
+    // Joel      4      3.73 
+
+    //single sorts
+    {
+        auto check_tv = [&](TableView tv) {
+            for (size_t row_ndx = 1; row_ndx < tv.size(); ++row_ndx) {
+                CHECK(tv.get(row_ndx - 1).get<Int>(age_col) <= tv.get(row_ndx).get<Int>(age_col));
+            }
+        };
+        std::vector<json> sorts;
+        sorts.emplace_back(sort_int_asc);
+        check_tv(get_sorted_view(t, simple_query_sort(int_neq, sorts)));
+    }
+
+    {
+        auto check_tv = [&](TableView tv) {
+            for (size_t row_ndx = 1; row_ndx < tv.size(); ++row_ndx) {
+                CHECK(tv.get(row_ndx - 1).get<Int>(age_col) >= tv.get(row_ndx).get<Int>(age_col));
+            }
+        };
+        std::vector<json> sorts;
+        sorts.emplace_back(sort_int_desc);
+        check_tv(get_sorted_view(t, simple_query_sort(int_neq, sorts)));
+    }
+    
+    {
+        auto check_tv = [&](TableView tv) {
+            for (size_t row_ndx = 1; row_ndx < tv.size(); ++row_ndx) {
+                CHECK(tv.get(row_ndx - 1).get<float>(fee_col) <= tv.get(row_ndx).get<float>(fee_col));
+            }
+        };
+        std::vector<json> sorts;
+        sorts.emplace_back(sort_float_asc);
+        check_tv(get_sorted_view(t, simple_query_sort(float_neq, sorts)));
+    }
+
+    {
+        auto check_tv = [&](TableView tv) {
+            for (size_t row_ndx = 1; row_ndx < tv.size(); ++row_ndx) {
+                CHECK(tv.get(row_ndx - 1).get<float>(fee_col) >= tv.get(row_ndx).get<float>(fee_col));
+            }
+        };
+        std::vector<json> sorts;
+        sorts.emplace_back(sort_float_desc);
+        check_tv(get_sorted_view(t, simple_query_sort(float_neq, sorts)));
+    }
+
+    {
+        auto check_tv = [&](TableView tv) {
+            for (size_t row_ndx = 1; row_ndx < tv.size(); ++row_ndx) {
+                CHECK(tv.get(row_ndx - 1).get<String>(name_col) <= tv.get(row_ndx).get<String>(name_col));
+            }
+        };
+        std::vector<json> sorts;
+        sorts.emplace_back(sort_string_asc);
+        check_tv(get_sorted_view(t, simple_query_sort(float_neq, sorts)));
+    }
+
+    {
+        auto check_tv = [&](TableView tv) {
+            for (size_t row_ndx = 1; row_ndx < tv.size(); ++row_ndx) {
+                CHECK(tv.get(row_ndx - 1).get<String>(name_col) >= tv.get(row_ndx).get<String>(name_col));
+            }
+        };
+        std::vector<json> sorts;
+        sorts.emplace_back(sort_string_desc);
+        check_tv(get_sorted_view(t, simple_query_sort(float_neq, sorts)));
+    }
+
+    //multiple sorts
+    {
+        auto check_tv = [&](TableView tv) {
+            for (size_t row_ndx = 1; row_ndx < tv.size(); ++row_ndx) {
+                std::string s1 = tv.get(row_ndx - 1).get<String>(name_col);
+                std::string s2 = tv.get(row_ndx).get<String>(name_col);
+                float f1 = tv.get(row_ndx - 1).get<float>(fee_col);
+                float f2 = tv.get(row_ndx).get<float>(fee_col);
+
+                if (f1 == f2){
+                    CHECK(tv.get(row_ndx - 1).get<String>(name_col) >= tv.get(row_ndx).get<String>(name_col));
+                } else {
+                    CHECK(tv.get(row_ndx - 1).get<float>(fee_col) <= tv.get(row_ndx).get<float>(fee_col));
+                }
+            }
+        };
+        std::vector<json> sorts;
+        //different orderings
+        sorts.emplace_back(sort_float_asc);
+        sorts.emplace_back(sort_string_desc);
+        check_tv(get_sorted_view(t, simple_query_sort(float_neq, sorts)));
+    }
+
+    {
+        auto check_tv = [&](TableView tv) {
+            for (size_t row_ndx = 1; row_ndx < tv.size(); ++row_ndx) {
+                std::string s1 = tv.get(row_ndx - 1).get<String>(name_col);
+                std::string s2 = tv.get(row_ndx).get<String>(name_col);
+                float f1 = tv.get(row_ndx - 1).get<float>(fee_col);
+                float f2 = tv.get(row_ndx).get<float>(fee_col);
+
+                if (f1 == f2){
+                    CHECK(tv.get(row_ndx - 1).get<String>(name_col) <= tv.get(row_ndx).get<String>(name_col));
+                } else {
+                    CHECK(tv.get(row_ndx - 1).get<float>(fee_col) <= tv.get(row_ndx).get<float>(fee_col));
+                }
+            }
+        };
+        std::vector<json> sorts;
+        //same orderings
+        sorts.emplace_back(sort_float_asc);
+        sorts.emplace_back(sort_string_asc);
+        check_tv(get_sorted_view(t, simple_query_sort(float_neq, sorts)));
+    }
+}
