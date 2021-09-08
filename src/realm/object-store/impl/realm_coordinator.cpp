cee183eb37dcca0ffe59ccdb0384bcdfc43d9cba
James Stone <james.stone@mongodb.com>
2021-09-08 16:12:53 -0700

    fix thread analysis warnings

--- src/realm/object-store/impl/realm_coordinator.cpp
+++ src/realm/object-store/impl/realm_coordinator.cpp
@@ -100,7 +100,20 @@ void RealmCoordinator::create_sync_session()
     m_config.sync_config->get_fresh_realm_for_path = [&](const std::string& path,
                                                          std::function<void(DBRef, std::exception_ptr)> callback) {
         try {
-            auto task = get_synchronized_realm_at_path(path);
+            // Get a fully downloaded Realm using the current configuration but changing the
+            // on disk path to the one provided. The current sync session is not affected.
+            REALM_ASSERT(!path.empty());
+            REALM_ASSERT(path != m_config.path);
+            REALM_ASSERT(m_config.sync_config);
+            auto copy_config = m_config;
+            copy_config.path = path;
+            // Do not use seamless loss mode on the fresh Realm. Use manual mode so that
+            // any error during the download is propagated back to the original session.
+            // This prevents a cycle if the fresh copy itself experiences a client reset.
+            copy_config.sync_config->client_resync_mode = ClientResyncMode::Manual;
+            std::shared_ptr<RealmCoordinator> rc = get_coordinator(copy_config);
+            REALM_ASSERT(rc);
+            auto task = rc->get_synchronized_realm(copy_config);
             task->start([callback, path](ThreadSafeReference realm_ref, std::exception_ptr err) {
                 if (err) {
                     callback(nullptr, std::move(err));
@@ -362,18 +375,6 @@ std::shared_ptr<AsyncOpenTask> RealmCoordinator::get_synchronized_realm(Realm::C
     set_config(config);
     create_sync_session();
     return std::make_shared<AsyncOpenTask>(shared_from_this(), m_sync_session);
-}
-
-std::shared_ptr<AsyncOpenTask> RealmCoordinator::get_synchronized_realm_at_path(std::string path)
-{
-    if (!m_config.sync_config)
-        throw std::logic_error("This method is only available for fully synchronized Realms.");
-    auto copy_config = m_config;
-    copy_config.path = path;
-    copy_config.sync_config->client_resync_mode = ClientResyncMode::Manual;
-    std::shared_ptr<RealmCoordinator> rc = get_coordinator(copy_config);
-    REALM_ASSERT(rc);
-    return rc->get_synchronized_realm(copy_config);
 }
 
 void RealmCoordinator::create_session(const Realm::Config& config)
--- src/realm/object-store/impl/realm_coordinator.hpp
+++ src/realm/object-store/impl/realm_coordinator.hpp
@@ -68,9 +68,6 @@ public:
     // Timeouts and interruptions are not handled by this method and must be handled by upper layers.
     std::shared_ptr<AsyncOpenTask> get_synchronized_realm(Realm::Config config)
         REQUIRES(!m_realm_mutex, !m_schema_cache_mutex);
-
-    std::shared_ptr<AsyncOpenTask> get_synchronized_realm_at_path(std::string path)
-        REQUIRES(!m_realm_mutex, !m_schema_cache_mutex);
     // Creates the underlying sync session if it doesn't already exists.
     // This is also created as part of opening a Realm, so only use this
     // method if the session needs to exist before the Realm does.
