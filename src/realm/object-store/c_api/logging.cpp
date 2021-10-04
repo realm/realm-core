5110a86b3564319710c56f18a93f01acf316067d
Yavor Georgiev <fealebenpae@gmail.com>
2021-10-04 16:35:44 +0200

    updated according to comments

--- src/realm/object-store/c_api/logging.cpp
+++ src/realm/object-store/c_api/logging.cpp
@@ -16,98 +16,51 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-#include <realm/object-store/c_api/types.hpp>
-#include <realm/object-store/c_api/util.hpp>
+#include "types.hpp"
+#include "util.hpp"
+#include "logging.hpp"
 
 namespace realm::c_api {
 namespace {
 using Logger = realm::util::Logger;
-static inline realm_log_level_e to_capi(Logger::Level level)
-{
-    switch (level) {
-        case Logger::Level::all:
-            return RLM_LOG_LEVEL_ALL;
-        case Logger::Level::trace:
-            return RLM_LOG_LEVEL_TRACE;
-        case Logger::Level::debug:
-            return RLM_LOG_LEVEL_DEBUG;
-        case Logger::Level::detail:
-            return RLM_LOG_LEVEL_DETAIL;
-        case Logger::Level::info:
-            return RLM_LOG_LEVEL_INFO;
-        case Logger::Level::warn:
-            return RLM_LOG_LEVEL_WARNING;
-        case Logger::Level::error:
-            return RLM_LOG_LEVEL_ERROR;
-        case Logger::Level::fatal:
-            return RLM_LOG_LEVEL_FATAL;
-        case Logger::Level::off:
-            return RLM_LOG_LEVEL_OFF;
-    }
-    REALM_TERMINATE("Invalid log level."); // LCOV_EXCL_LINE
-}
 
-static inline Logger::Level from_capi(realm_log_level_e level)
-{
-    switch (level) {
-        case RLM_LOG_LEVEL_ALL:
-            return Logger::Level::all;
-        case RLM_LOG_LEVEL_TRACE:
-            return Logger::Level::trace;
-        case RLM_LOG_LEVEL_DEBUG:
-            return Logger::Level::debug;
-        case RLM_LOG_LEVEL_DETAIL:
-            return Logger::Level::detail;
-        case RLM_LOG_LEVEL_INFO:
-            return Logger::Level::info;
-        case RLM_LOG_LEVEL_WARNING:
-            return Logger::Level::warn;
-        case RLM_LOG_LEVEL_ERROR:
-            return Logger::Level::error;
-        case RLM_LOG_LEVEL_FATAL:
-            return Logger::Level::fatal;
-        case RLM_LOG_LEVEL_OFF:
-            return Logger::Level::off;
-    }
-    REALM_TERMINATE("Invalid log level."); // LCOV_EXCL_LINE
-}
+static_assert(realm_log_level_e(Logger::Level::all) == RLM_LOG_LEVEL_ALL);
+static_assert(realm_log_level_e(Logger::Level::trace) == RLM_LOG_LEVEL_TRACE);
+static_assert(realm_log_level_e(Logger::Level::debug) == RLM_LOG_LEVEL_DEBUG);
+static_assert(realm_log_level_e(Logger::Level::detail) == RLM_LOG_LEVEL_DETAIL);
+static_assert(realm_log_level_e(Logger::Level::info) == RLM_LOG_LEVEL_INFO);
+static_assert(realm_log_level_e(Logger::Level::warn) == RLM_LOG_LEVEL_WARNING);
+static_assert(realm_log_level_e(Logger::Level::error) == RLM_LOG_LEVEL_ERROR);
+static_assert(realm_log_level_e(Logger::Level::fatal) == RLM_LOG_LEVEL_FATAL);
+static_assert(realm_log_level_e(Logger::Level::off) == RLM_LOG_LEVEL_OFF);
 
-class CLogger : public Logger::LevelThreshold, public Logger {
+class CLogger : public realm::util::RootLogger {
 public:
-    CLogger(UserdataPtr userdata, realm_logger_log_func_t log_callback,
-            realm_logger_get_threshold_func_t get_threshold)
-        : Logger::LevelThreshold()
-        , Logger(static_cast<Logger::LevelThreshold&>(*this))
+    CLogger(SharedUserdata userdata, realm_log_func_t log_callback, Logger::Level level)
+        : RootLogger()
         , m_userdata(std::move(userdata))
         , m_log_callback(log_callback)
-        , m_get_threshold(get_threshold)
     {
+        this->set_level_threshold(level);
     }
 
 protected:
     void do_log(Logger::Level level, const std::string& message) final
     {
-        m_log_callback(m_userdata.get(), to_capi(level), message.c_str());
-    }
-
-    Logger::Level get() const noexcept final
-    {
-        return from_capi(m_get_threshold(m_userdata.get()));
+        m_log_callback(m_userdata.get(), realm_log_level_e(level), message.c_str());
     }
 
 private:
-    UserdataPtr m_userdata;
-    realm_logger_log_func_t m_log_callback;
-    realm_logger_get_threshold_func_t m_get_threshold;
+    SharedUserdata m_userdata;
+    realm_log_func_t m_log_callback;
 };
 } // namespace
-} // namespace realm::c_api
 
-using namespace realm::c_api;
-
-RLM_API realm_logger_t* realm_logger_new(realm_logger_log_func_t log_func,
-                                         realm_logger_get_threshold_func_t threshold_func, void* userdata,
-                                         realm_free_userdata_func_t free_func)
+realm::SyncClientConfig::LoggerFactory make_logger_factory(realm_log_func_t logger, void* userdata,
+                                                           realm_free_userdata_func_t free_userdata)
 {
-    return new realm_logger_t(new CLogger(UserdataPtr{userdata, free_func}, log_func, threshold_func));
+    return [logger, userdata = SharedUserdata{userdata, free_userdata}](Logger::Level level) {
+        return std::make_unique<CLogger>(userdata, logger, level);
+    };
 }
+} // namespace realm::c_api
