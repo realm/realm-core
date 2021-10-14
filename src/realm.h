243c12b22e1481b71d3eaeadf890cfe7b1fb88bc
Yavor Georgiev <fealebenpae@users.noreply.github.com>
2021-10-14 23:27:46 +0200

    Update src/realm.h
    Co-authored-by: Mathias Stearn <redbeard0531@gmail.com>

--- src/realm.h
+++ src/realm.h
@@ -2140,7 +2140,7 @@ typedef struct realm_app_user_apikey {
 
 // This type should never be returned from a function.
 // It's only meant as an asynchronous callback argument.
-// Pointers to this struct are only valid inside the scope
+// Pointers to this struct and its pointer members are only valid inside the scope
 // of the callback they were passed to.
 typedef struct realm_app_error {
     realm_app_error_category_e error_category;
