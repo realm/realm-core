ed6461ff7953ae11f99037ba97db0c18802a17f1
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2021-10-28 17:40:34 +0200

    Use originating realm schema for frozen realm (#4965)
    Co-authored-by: Ferdinando Papale <>

--- src/realm/object-store/shared_realm.cpp
+++ src/realm/object-store/shared_realm.cpp
@@ -892,6 +892,8 @@ SharedRealm Realm::freeze()
     auto config = m_config;
     auto version = read_transaction_version();
     config.scheduler = util::Scheduler::make_frozen(version);
+    config.schema = m_schema;
+    config.schema_version = m_schema_version;
     return Realm::get_frozen_realm(std::move(config), version);
 }
 
