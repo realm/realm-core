realm/realm-core

commit a5d82daaf2db02fa4d5cdddf3e0cf22e99f2df65

Remove 'start' and 'end' parameters on Query::find_all
These parameters are not used. Only 'limit' is used through LimitDescriptor.
The original idea was probably to support some kind of query window, but this
has to be supported differently.

src/realm/query.cpp
@@ -1428,21 +1428,18 @@ ObjKey Query::find()
     }
 }
 
-void Query::find_all(TableView& ret, size_t begin, size_t end, size_t limit) const
+void Query::do_find_all(TableView& ret, size_t limit) const
 {
     if (limit == 0)
         return;
-
-    REALM_ASSERT_3(begin, <=, m_table->size());
 
     init();
 
     bool has_cond = has_conditions();
 
     if (m_view) {
-        if (end == size_t(-1))
-            end = m_view->size();
-        for (size_t t = begin; t < end && ret.size() < limit; t++) {
+        size_t sz = m_view->size();
+        for (size_t t = 0; t < sz && ret.size() < limit; t++) {
             const Obj obj = m_view->try_get_object(t);
             if (eval_object(obj)) {
                 ret.m_key_values.add(obj.get_key());
@@ -1450,31 +1447,20 @@ void Query::find_all(TableView& ret, size_t begin, size_t end, size_t limit) con
         }
     }
     else {
-        if (end == size_t(-1))
-            end = m_table->size();
         if (!has_cond) {
             KeyColumn& refs = ret.m_key_values;
 
-            auto f = [&begin, &end, &limit, &refs](const Cluster* cluster) {
-                size_t e = cluster->node_size();
-                if (begin < e) {
-                    if (e > end) {
-                        e = end;
-                    }
-                    auto offset = cluster->get_offset();
-                    auto key_values = cluster->get_key_array();
-                    for (size_t i = begin; (i < e) && limit; i++) {
-                        refs.add(ObjKey(key_values->get(i) + offset));
-                        --limit;
-                    }
-                    begin = 0;
+            auto f = [&limit, &refs](const Cluster* cluster) {
+                size_t sz = cluster->node_size();
+                auto offset = cluster->get_offset();
+                auto key_values = cluster->get_key_array();
+                for (size_t i = 0; (i < sz); i++) {
+                    refs.add(ObjKey(key_values->get(i) + offset));
+                    // Stop if limit is reached
+                    if (--limit == 0)
+                        return true;
                 }
-                else {
-                    begin -= e;
-                }
-                end -= e;
-                // Stop if end is reached
-                return (end == 0) || (limit == 0);
+                return false;
             };
 
             m_table->traverse_clusters(f);
@@ -1484,9 +1470,6 @@ void Query::find_all(TableView& ret, size_t begin, size_t end, size_t limit) con
             auto best = find_best_node(pn);
             auto node = pn->m_children[best];
             if (node->has_search_index()) {
-                // translate begin/end limiters into corresponding keys
-                auto begin_key = (begin >= m_table->size()) ? ObjKey() : m_table->get_object(begin).get_key();
-                auto end_key = (end >= m_table->size()) ? ObjKey() : m_table->get_object(end).get_key();
                 KeyColumn& refs = ret.m_key_values;
 
                 // The node having the search index can be removed from the query as we know that
@@ -1496,24 +1479,18 @@ void Query::find_all(TableView& ret, size_t begin, size_t end, size_t limit) con
 
                 auto keys = node->index_based_keys();
                 for (auto key : keys) {
-                    if (limit == 0)
-                        break;
-                    if (begin_key && key < begin_key)
-                        continue;
-                    if (end_key && !(key < end_key))
-                        continue;
                     if (pn->m_children.empty()) {
                         // No more conditions - just add key
                         refs.add(key);
-                        limit--;
                     }
                     else {
                         auto obj = m_table->get_object(key);
                         if (eval_object(obj)) {
                             refs.add(key);
-                            limit--;
                         }
                     }
+                    if (--limit == 0)
+                        break;
                 }
                 return;
             }
@@ -1521,24 +1498,14 @@ void Query::find_all(TableView& ret, size_t begin, size_t end, size_t limit) con
             node = pn;
             QueryStateFindAll<KeyColumn> st(ret.m_key_values, limit);
 
-            auto f = [&begin, &end, &node, &st, this](const Cluster* cluster) {
+            auto f = [&node, &st, this](const Cluster* cluster) {
                 size_t e = cluster->node_size();
-                if (begin < e) {
-                    if (e > end) {
-                        e = end;
-                    }
-                    node->set_cluster(cluster);
-                    st.m_key_offset = cluster->get_offset();
-                    st.m_key_values = cluster->get_key_array();
-                    aggregate_internal(node, &st, begin, e, nullptr);
-                    begin = 0;
-                }
-                else {
-                    begin -= e;
-                }
-                end -= e;
+                node->set_cluster(cluster);
+                st.m_key_offset = cluster->get_offset();
+                st.m_key_values = cluster->get_key_array();
+                aggregate_internal(node, &st, 0, e, nullptr);
                 // Stop if limit or end is reached
-                return end == 0 || st.match_count() == st.limit();
+                return st.match_count() == st.limit();
             };
 
             m_table->traverse_clusters(f);
@@ -1546,13 +1513,13 @@ void Query::find_all(TableView& ret, size_t begin, size_t end, size_t limit) con
     }
 }
 
-TableView Query::find_all(size_t start, size_t end, size_t limit)
+TableView Query::find_all(size_t limit)
 {
 #if REALM_METRICS
     std::unique_ptr<MetricTimer> metric_timer = QueryInfo::track(this, QueryInfo::type_FindAll);
 #endif
 
-    TableView ret(m_table, *this, start, end, limit);
+    TableView ret(m_table, *this, limit);
     if (m_ordering) {
         ret.apply_descriptor_ordering(*m_ordering);
     }
@@ -1654,8 +1621,6 @@ TableView Query::find_all(const DescriptorOrdering& descriptor)
         return find_all();
     }
 
-    const size_t default_start = 0;
-    const size_t default_end = size_t(-1);
     const size_t default_limit = size_t(-1);
 
     bool only_limit = true;
@@ -1672,10 +1637,10 @@ TableView Query::find_all(const DescriptorOrdering& descriptor)
         }
     }
     if (only_limit) {
-        return find_all(default_start, default_end, min_limit);
+        return find_all(min_limit);
     }
 
-    TableView ret(m_table, *this, default_start, default_end, default_limit);
+    TableView ret(m_table, *this, default_limit);
     ret.apply_descriptor_ordering(descriptor);
     return ret;
 }
@@ -1690,8 +1655,6 @@ size_t Query::count(const DescriptorOrdering& descriptor)
     if (bool(min_limit) && *min_limit == 0)
         return 0;
 
-    const size_t start = 0;
-    const size_t end = m_table->size();
     size_t limit = size_t(-1);
 
     if (!descriptor.will_apply_distinct()) {
@@ -1701,7 +1664,7 @@ size_t Query::count(const DescriptorOrdering& descriptor)
         return do_count(limit);
     }
 
-    TableView ret(m_table, *this, start, end, limit);
+    TableView ret(m_table, *this, limit);
     ret.apply_descriptor_ordering(descriptor);
     return ret.size();
 }

test/test_query2.cpp
@@ -118,28 +118,14 @@ TEST(Query_Limit)
 
     Query q1 = ttt.where().equal(col_int, 2);
 
-    TableView tv1 = q1.find_all(0, size_t(-1), 2);
+    TableView tv1 = q1.find_all(2);
     CHECK_EQUAL(2, tv1.size());
     CHECK_EQUAL(1, tv1[0].get<Int>(col_id));
     CHECK_EQUAL(4, tv1[1].get<Int>(col_id));
 
-    TableView tv2 = q1.find_all(5, size_t(-1), 2);
-    CHECK_EQUAL(2, tv2.size());
-    CHECK_EQUAL(7, tv2[0].get<Int>(col_id));
-    CHECK_EQUAL(10, tv2[1].get<Int>(col_id));
-
-    TableView tv3 = q1.find_all(11, size_t(-1), 2);
-    CHECK_EQUAL(1, tv3.size());
-    CHECK_EQUAL(13, tv3[0].get<Int>(col_id));
-
-
     Query q2 = ttt.where();
-    TableView tv4 = q2.find_all(0, 5, 3);
+    TableView tv4 = q2.find_all(3);
     CHECK_EQUAL(3, tv4.size());
-
-    Query q3 = ttt.where();
-    TableView tv5 = q3.find_all(0, 3, 5);
-    CHECK_EQUAL(3, tv5.size());
 }
 
 

src/realm/query.hpp
@@ -250,7 +250,7 @@ public:
 
     // Searching
     ObjKey find();
-    TableView find_all(size_t start = 0, size_t end = size_t(-1), size_t limit = size_t(-1));
+    TableView find_all(size_t limit = size_t(-1));
 
     // Aggregates
     size_t count() const;
@@ -370,7 +370,7 @@ private:
     void aggregate_internal(ParentNode* pn, QueryStateBase* st, size_t start, size_t end,
                             ArrayPayload* source_column) const;
 
-    void find_all(TableView& tv, size_t start = 0, size_t end = size_t(-1), size_t limit = size_t(-1)) const;
+    void do_find_all(TableView& tv, size_t limit) const;
     size_t do_count(size_t limit = size_t(-1)) const;
     void delete_nodes() noexcept;
 

src/realm/table_view.cpp
@@ -92,8 +92,6 @@ TableView::TableView(TableView& src, Transaction* tr, PayloadPolicy mode)
         src.m_last_seen_versions.clear();
     }
     m_descriptor_ordering = src.m_descriptor_ordering;
-    m_start = src.m_start;
-    m_end = src.m_end;
     m_limit = src.m_limit;
 }
 
@@ -599,7 +597,7 @@ void TableView::do_sync()
 
         if (m_query.m_view)
             m_query.m_view->sync_if_needed();
-        m_query.find_all(*const_cast<TableView*>(this), m_start, m_end, m_limit);
+        m_query.do_find_all(*const_cast<TableView*>(this), m_limit);
     }
 
     do_sort(m_descriptor_ordering);

src/realm/table_view.hpp
@@ -155,7 +155,7 @@ public:
 
     /// Construct empty view, ready for addition of row indices.
     TableView(ConstTableRef parent);
-    TableView(ConstTableRef parent, Query& query, size_t start, size_t end, size_t limit);
+    TableView(ConstTableRef parent, Query& query, size_t limit);
     TableView(ConstTableRef parent, ColKey column, const Obj& obj);
     TableView(ConstTableRef parent, LinkCollectionPtr collection);
 
@@ -411,8 +411,6 @@ protected:
     // was NOT generated by a query, but follows a table directly.
     Query m_query;
     // parameters for findall, needed to rerun the query
-    size_t m_start = 0;
-    size_t m_end = size_t(-1);
     size_t m_limit = size_t(-1);
 
     // FIXME: This class should eventually be replaced by std::vector<ObjKey>
@@ -463,11 +461,9 @@ inline TableView::TableView(ConstTableRef parent)
     }
 }
 
-inline TableView::TableView(ConstTableRef parent, Query& query, size_t start, size_t end, size_t lim)
+inline TableView::TableView(ConstTableRef parent, Query& query, size_t lim)
     : m_table(parent)
     , m_query(query)
-    , m_start(start)
-    , m_end(end)
     , m_limit(lim)
 {
     m_key_values.create();
@@ -505,8 +501,6 @@ inline TableView::TableView(const TableView& tv)
     , m_collection_source(tv.m_collection_source ? tv.m_collection_source->clone_obj_list() : LinkCollectionPtr{})
     , m_descriptor_ordering(tv.m_descriptor_ordering)
     , m_query(tv.m_query)
-    , m_start(tv.m_start)
-    , m_end(tv.m_end)
     , m_limit(tv.m_limit)
     , m_last_seen_versions(tv.m_last_seen_versions)
 {
@@ -522,8 +516,6 @@ inline TableView::TableView(TableView&& tv) noexcept
     , m_collection_source(std::move(tv.m_collection_source))
     , m_descriptor_ordering(std::move(tv.m_descriptor_ordering))
     , m_query(std::move(tv.m_query))
-    , m_start(tv.m_start)
-    , m_end(tv.m_end)
     , m_limit(tv.m_limit)
     // if we are created from a table view which is outdated, take care to use the outdated
     // version number so that we can later trigger a sync if needed.
@@ -540,8 +532,6 @@ inline TableView& TableView::operator=(TableView&& tv) noexcept
     m_key_values.move_from(tv.m_key_values);
     m_query = std::move(tv.m_query);
     m_last_seen_versions = tv.m_last_seen_versions;
-    m_start = tv.m_start;
-    m_end = tv.m_end;
     m_limit = tv.m_limit;
     m_limit_count = tv.m_limit_count;
     m_source_column_key = tv.m_source_column_key;
@@ -562,8 +552,6 @@ inline TableView& TableView::operator=(const TableView& tv)
 
     m_query = tv.m_query;
     m_last_seen_versions = tv.m_last_seen_versions;
-    m_start = tv.m_start;
-    m_end = tv.m_end;
     m_limit = tv.m_limit;
     m_limit_count = tv.m_limit_count;
     m_source_column_key = tv.m_source_column_key;

test/test_query.cpp
@@ -636,18 +636,14 @@ TEST(Query_NextGenSyntaxMonkey0)
 
         tvpos = 0;
 
-        // with start and limit
-        size_t start = random.draw_int_mod(rows);
+        // with limit
         size_t limit = random.draw_int_mod(rows);
-        tv = q.find_all(start, size_t(-1), limit);
+        tv = q.find_all(limit);
         tvpos = 0;
-        size_t r = 0;
         for (Obj o : table) {
-            if (r >= start && tvpos < limit && o.get<Int>(col_int) > o.get<Float>(col_float) &&
-                o.get<String>(col_str) == "a") {
+            if (tvpos < limit && o.get<Int>(col_int) > o.get<Float>(col_float) && o.get<String>(col_str) == "a") {
                 tvpos++;
             }
-            r++;
         }
         CHECK_EQUAL(tvpos, tv.size());
     }
@@ -2664,8 +2660,6 @@ TEST(Query_Huge)
         size_t res7 = 0;
         size_t res8 = 0;
 
-        size_t start = random.draw_int_mod(3000);
-        size_t end = start + random.draw_int_mod(3000 - start);
         size_t limit;
         if (random.draw_bool())
             limit = random.draw_int_mod(5000);
@@ -2730,30 +2724,28 @@ TEST(Query_Huge)
             obj.set(col_str1, StringData(second));
             obj.set(col_int2, third);
 
-            if ((row >= start && row < end && limit > res1) && (first == "A" && second == "A" && third == 1))
+            if ((limit > res1) && (first == "A" && second == "A" && third == 1))
                 res1++;
 
-            if ((row >= start && row < end && limit > res2) && ((first == "A" || second == "A") && third == 1))
+            if ((limit > res2) && ((first == "A" || second == "A") && third == 1))
                 res2++;
 
-            if ((row >= start && row < end && limit > res3) && (first == "A" && (second == "A" || third == 1)))
+            if ((limit > res3) && (first == "A" && (second == "A" || third == 1)))
                 res3++;
 
-            if ((row >= start && row < end && limit > res4) && (second == "A" && (first == "A" || third == 1)))
+            if ((limit > res4) && (second == "A" && (first == "A" || third == 1)))
                 res4++;
 
-            if ((row >= start && row < end && limit > res5) && (first == "A" || second == "A" || third == 1))
+            if ((limit > res5) && (first == "A" || second == "A" || third == 1))
                 res5++;
 
-            if ((row >= start && row < end && limit > res6) && (first != "A" && second == "A" && third == 1))
+            if ((limit > res6) && (first != "A" && second == "A" && third == 1))
                 res6++;
 
-            if ((row >= start && row < end && limit > res7) &&
-                (first != "longlonglonglonglonglonglong A" && second == "A" && third == 1))
+            if ((limit > res7) && (first != "longlonglonglonglonglonglong A" && second == "A" && third == 1))
                 res7++;
 
-            if ((row >= start && row < end && limit > res8) &&
-                (first != "longlonglonglonglonglonglong A" && second == "A" && third == 2))
+            if ((limit > res8) && (first != "longlonglonglonglonglonglong A" && second == "A" && third == 2))
                 res8++;
         }
 
@@ -2770,13 +2762,13 @@ TEST(Query_Huge)
                 tt.add_search_index(col_str1);
             }
 
-            v = tt.where().equal(col_str0, "A").equal(col_str1, "A").equal(col_int2, 1).find_all(start, end, limit);
+            v = tt.where().equal(col_str0, "A").equal(col_str1, "A").equal(col_int2, 1).find_all(limit);
             CHECK_EQUAL(res1, v.size());
 
-            v = tt.where().equal(col_str1, "A").equal(col_str0, "A").equal(col_int2, 1).find_all(start, end, limit);
+            v = tt.where().equal(col_str1, "A").equal(col_str0, "A").equal(col_int2, 1).find_all(limit);
             CHECK_EQUAL(res1, v.size());
 
-            v = tt.where().equal(col_int2, 1).equal(col_str1, "A").equal(col_str0, "A").find_all(start, end, limit);
+            v = tt.where().equal(col_int2, 1).equal(col_str1, "A").equal(col_str0, "A").find_all(limit);
             CHECK_EQUAL(res1, v.size());
 
             v = tt.where()
@@ -2786,7 +2778,7 @@ TEST(Query_Huge)
                     .equal(col_str1, "A")
                     .end_group()
                     .equal(col_int2, 1)
-                    .find_all(start, end, limit);
+                    .find_all(limit);
             CHECK_EQUAL(res2, v.size());
 
             v = tt.where()
@@ -2796,12 +2788,12 @@ TEST(Query_Huge)
                     .Or()
                     .equal(col_int2, 1)
                     .end_group()
-                    .find_all(start, end, limit);
+                    .find_all(limit);
             CHECK_EQUAL(res3, v.size());
 
             Query q =
                 tt.where().group().equal(col_str0, "A").Or().equal(col_int2, 1).end_group().equal(col_str1, "A");
-            v = q.find_all(start, end, limit);
+            v = q.find_all(limit);
             CHECK_EQUAL(res4, v.size());
 
             v = tt.where()
@@ -2811,37 +2803,27 @@ TEST(Query_Huge)
                     .equal(col_int2, 1)
                     .end_group()
                     .equal(col_str1, "A")
-                    .find_all(start, end, limit);
+                    .find_all(limit);
             CHECK_EQUAL(res4, v.size());
 
-            v = tt.where()
-                    .equal(col_str0, "A")
-                    .Or()
-                    .equal(col_str1, "A")
-                    .Or()
-                    .equal(col_int2, 1)
-                    .find_all(start, end, limit);
+            v = tt.where().equal(col_str0, "A").Or().equal(col_str1, "A").Or().equal(col_int2, 1).find_all(limit);
             CHECK_EQUAL(res5, v.size());
 
-            v = tt.where()
-                    .not_equal(col_str0, "A")
-                    .equal(col_str1, "A")
-                    .equal(col_int2, 1)
-                    .find_all(start, end, limit);
+            v = tt.where().not_equal(col_str0, "A").equal(col_str1, "A").equal(col_int2, 1).find_all(limit);
             CHECK_EQUAL(res6, v.size());
 
             v = tt.where()
                     .not_equal(col_str0, "longlonglonglonglonglonglong A")
                     .equal(col_str1, "A")
                     .equal(col_int2, 1)
-                    .find_all(start, end, limit);
+                    .find_all(limit);
             CHECK_EQUAL(res7, v.size());
 
             v = tt.where()
                     .not_equal(col_str0, "longlonglonglonglonglonglong A")
                     .equal(col_str1, "A")
                     .equal(col_int2, 2)
-                    .find_all(start, end, limit);
+                    .find_all(limit);
             CHECK_EQUAL(res8, v.size());
         }
     }
@@ -2861,22 +2843,19 @@ TEST(Query_OnTableView_where)
         size_t cnt0 = 0;
         size_t limit = random.draw_int_max(REALM_MAX_BPNODE_SIZE * 10);
 
-        size_t lbound = random.draw_int_mod(REALM_MAX_BPNODE_SIZE * 10);
-        size_t ubound = lbound + random.draw_int_mod(REALM_MAX_BPNODE_SIZE * 10 - lbound);
-
         for (size_t i = 0; i < REALM_MAX_BPNODE_SIZE * 10; i++) {
             int v = random.draw_int_mod(3);
 
-            if (v == 1 && i >= lbound && i < ubound && cnt0 < limit)
+            if (v == 1 && cnt0 < limit)
                 cnt1++;
 
-            if (v != 0 && i >= lbound && i < ubound)
+            if (v != 0)
                 cnt0++;
 
             oti.create_object().set(col, v);
         }
 
-        TableView v = oti.where().not_equal(col, 0).find_all(lbound, ubound, limit);
+        TableView v = oti.where().not_equal(col, 0).find_all(limit);
         size_t cnt2 = oti.where(&v).equal(col, 1).count();
 
         CHECK_EQUAL(cnt1, cnt2);
@@ -3644,75 +3623,6 @@ TEST(Query_AggregateSingleCond)
     CHECK_EQUAL(9, s);
 }
 
-TEST(Query_FindAllRange1)
-{
-    Table ttt;
-    ttt.add_column(type_Int, "1");
-    auto col_str = ttt.add_column(type_String, "2");
-
-    ttt.create_object().set_all(1, "a");
-    ttt.create_object().set_all(4, "a");
-    ttt.create_object().set_all(7, "a");
-    ttt.create_object().set_all(10, "a");
-    ttt.create_object().set_all(1, "a");
-    ttt.create_object().set_all(4, "a");
-    ttt.create_object().set_all(7, "a");
-    ttt.create_object().set_all(10, "a");
-    ttt.create_object().set_all(1, "a");
-    ttt.create_object().set_all(4, "a");
-    ttt.create_object().set_all(7, "a");
-    ttt.create_object().set_all(10, "a");
-
-    Query q1 = ttt.where().equal(col_str, "a");
-    TableView tv1 = q1.find_all(4, 10);
-    CHECK_EQUAL(6, tv1.size());
-}
-
-
-TEST(Query_FindAllRangeOrMonkey2)
-{
-    const size_t ROWS = 20;
-    const size_t ITER = 100;
-
-    Random random(random_int<unsigned long>()); // Seed from slow global generator
-
-    for (size_t u = 0; u < ITER; u++) {
-        Table tit;
-        auto col_int0 = tit.add_column(type_Int, "1");
-        auto col_int1 = tit.add_column(type_Int, "2");
-
-        std::vector<ObjKey> a;
-        std::vector<ObjKey> keys;
-        size_t start = random.draw_int_max(ROWS);
-        size_t end = start + random.draw_int_max(ROWS - start);
-
-        if (end > ROWS)
-            end = ROWS;
-
-        for (size_t t = 0; t < ROWS; t++) {
-            int64_t r1 = random.draw_int_mod(10);
-            int64_t r2 = random.draw_int_mod(10);
-            tit.create_object().set_all(r1, r2).get_key();
-        }
-
-        Query q1 = tit.where().group().equal(col_int0, 3).Or().equal(col_int0, 7).end_group().greater(col_int1, 5);
-        TableView tv1 = q1.find_all(start, end);
-
-        auto it = tit.begin() + start;
-        auto e = tit.begin() + end;
-        while (it != e) {
-            if ((it->get<Int>(col_int0) == 3 || it->get<Int>(col_int0) == 7) && it->get<Int>(col_int1) > 5)
-                a.push_back(it->get_key());
-            ++it;
-        }
-
-        CHECK_EQUAL(a.size(), tv1.size());
-        for (size_t t = 0; t < a.size(); t++) {
-            CHECK_EQUAL(tv1.get_key(t), a[t]);
-        }
-    }
-}
-
 TEST(Query_FindAllRangeOr)
 {
     Table ttt;
@@ -3730,14 +3640,8 @@ TEST(Query_FindAllRangeOr)
     ttt.create_object().set_all(3, "b"); //
 
     Query q1 = ttt.where().group().greater(col_int, 1).Or().equal(col_str, "a").end_group().less(col_int, 3);
-    TableView tv1 = q1.find_all(1, 8);
+    TableView tv1 = q1.find_all();
     CHECK_EQUAL(4, tv1.size());
-
-    TableView  tv2 = q1.find_all(2, 8);
-    CHECK_EQUAL(3, tv2.size());
-
-    TableView  tv3 = q1.find_all(1, 7);
-    CHECK_EQUAL(3, tv3.size());
 }
 
 

commit ead184782cbd7c961a46be7c84a68128e15f28f3

Fix error introduced in a5d82daaf2db0
Respecting a limit on find_all would fail in a query with more than one
Equal condition and when there was a search index on one of the properties
involved in the query

src/realm/query.cpp
@@ -1454,13 +1454,11 @@ void Query::do_find_all(TableView& ret, size_t limit) const
                 size_t sz = cluster->node_size();
                 auto offset = cluster->get_offset();
                 auto key_values = cluster->get_key_array();
-                for (size_t i = 0; (i < sz); i++) {
+                for (size_t i = 0; (i < sz) && limit; i++) {
                     refs.add(ObjKey(key_values->get(i) + offset));
-                    // Stop if limit is reached
-                    if (--limit == 0)
-                        return true;
+                    --limit;
                 }
-                return false;
+                return limit == 0;
             };
 
             m_table->traverse_clusters(f);
@@ -1479,18 +1477,20 @@ void Query::do_find_all(TableView& ret, size_t limit) const
 
                 auto keys = node->index_based_keys();
                 for (auto key : keys) {
+                    if (limit == 0)
+                        break;
                     if (pn->m_children.empty()) {
                         // No more conditions - just add key
                         refs.add(key);
+                        limit--;
                     }
                     else {
                         auto obj = m_table->get_object(key);
                         if (eval_object(obj)) {
                             refs.add(key);
+                            limit--;
                         }
                     }
-                    if (--limit == 0)
-                        break;
                 }
                 return;
             }
@@ -1504,7 +1504,7 @@ void Query::do_find_all(TableView& ret, size_t limit) const
                 st.m_key_offset = cluster->get_offset();
                 st.m_key_values = cluster->get_key_array();
                 aggregate_internal(node, &st, 0, e, nullptr);
-                // Stop if limit or end is reached
+                // Stop if limit is reached
                 return st.match_count() == st.limit();
             };
 

test/test_query2.cpp
@@ -96,25 +96,42 @@ TEST(Query_BigString)
 TEST(Query_Limit)
 {
     Table ttt;
-    auto col_id = ttt.add_column(type_Int, "id");
-    auto col_int = ttt.add_column(type_Int, "1");
-    ttt.add_column(type_String, "2");
+    auto col_id = ttt.add_column(type_Int, "id1");
+    auto col_int = ttt.add_column(type_Int, "id2");
+    ttt.add_column(type_String, "str");
 
     ttt.create_object().set_all(0, 1, "a");
-    ttt.create_object().set_all(1, 2, "a"); //
-    ttt.create_object().set_all(2, 3, "X");
-    ttt.create_object().set_all(3, 1, "a");
+    ttt.create_object().set_all(1, 2, "x"); //
+    ttt.create_object().set_all(2, 3, "a");
+    ttt.create_object().set_all(3, 1, "x");
     ttt.create_object().set_all(4, 2, "a"); //
-    ttt.create_object().set_all(5, 3, "X");
+    ttt.create_object().set_all(5, 3, "x");
     ttt.create_object().set_all(6, 1, "a");
-    ttt.create_object().set_all(7, 2, "a"); //
-    ttt.create_object().set_all(8, 3, "X");
-    ttt.create_object().set_all(9, 1, "a");
+    ttt.create_object().set_all(7, 2, "x"); //
+    ttt.create_object().set_all(8, 3, "a");
+    ttt.create_object().set_all(9, 1, "x");
     ttt.create_object().set_all(10, 2, "a"); //
-    ttt.create_object().set_all(11, 3, "X");
+    ttt.create_object().set_all(11, 3, "x");
     ttt.create_object().set_all(12, 1, "a");
-    ttt.create_object().set_all(13, 2, "a"); //
-    ttt.create_object().set_all(14, 3, "X");
+    ttt.create_object().set_all(13, 2, "x"); //
+    ttt.create_object().set_all(14, 3, "a");
+
+    auto q = ttt.where();
+    auto tv = q.find_all();
+    CHECK_EQUAL(tv.size(), 15);
+    tv = q.find_all(0);
+    CHECK_EQUAL(tv.size(), 0);
+    tv = q.find_all(1);
+    CHECK_EQUAL(tv.size(), 1);
+
+    q = ttt.query("id2 == 3 && str == 'a'");
+    tv = q.find_all();
+    CHECK_EQUAL(tv.size(), 3);
+    tv = q.find_all(2);
+    CHECK_EQUAL(tv.size(), 2);
+    ttt.add_search_index(col_int);
+    tv = q.find_all(2);
+    CHECK_EQUAL(tv.size(), 2);
 
     Query q1 = ttt.where().equal(col_int, 2);
 