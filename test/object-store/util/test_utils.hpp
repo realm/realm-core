bcaca0a624192811c967487d4a3f196a3a4915d3
Nicola Cabiddu <nicola.cabiddu@mongodb.com>
2022-02-17 10:01:03 +0000

    fix swift and mac os builds for using std::fs

--- test/object-store/util/test_utils.hpp
+++ test/object-store/util/test_utils.hpp
@@ -24,17 +24,8 @@
 #include <realm/util/optional.hpp>
 
 #include <functional>
-
-#if defined __has_include
-#if __has_include(<filesystem>)
 #include <filesystem>
 namespace fs = std::filesystem;
-#elif __has_include(<experimental/filesystem>)
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
-#endif
-#endif
-
 
 namespace realm {
 
