realm/realm-core

commit 14210d1dbd235a11c5bef862f7da3d674a61e415

Support async writes with all schedulers and fix a use-after-free
Scheduler is not intended to be tightly coupled to Realm, and so should not
have several different notify functions for Realm-specific things. Instead,
redesign it to have a generic "invoke this function instead the scheduler"
function, and actually implement that in all of the Schedulers.

Some of the Scheduler callbacks captured a pointer to a Realm without any
guarantee that the Realm would still be alive when the callback was invoked.

The Android, dispatch queue, and C API schedulers did not support async writes.

src/realm/object-store/util/event_loop_dispatcher.hpp
@@ -21,10 +21,7 @@
 
 #include <realm/object-store/util/scheduler.hpp>
 
-#include <mutex>
-#include <queue>
 #include <tuple>
-#include <thread>
 
 namespace realm {
 
@@ -37,56 +34,29 @@ class EventLoopDispatcher<void(Args...)> {
     using Tuple = std::tuple<typename std::remove_reference<Args>::type...>;
 
 private:
-    struct State {
-        State(util::UniqueFunction<void(Args...)> func)
-            : func(std::move(func))
-        {
-        }
-
-        const util::UniqueFunction<void(Args...)> func;
-        std::queue<Tuple> invocations;
-        std::mutex mutex;
-        std::shared_ptr<util::Scheduler> scheduler;
-    };
-    const std::shared_ptr<State> m_state;
+    const std::shared_ptr<util::UniqueFunction<void(Args...)>> m_func;
     const std::shared_ptr<util::Scheduler> m_scheduler = util::Scheduler::make_default();
 
 public:
     EventLoopDispatcher(util::UniqueFunction<void(Args...)> func)
-        : m_state(std::make_shared<State>(std::move(func)))
+        : m_func(std::make_shared<util::UniqueFunction<void(Args...)>>(std::move(func)))
     {
-        m_scheduler->set_notify_callback([state = m_state] {
-            std::unique_lock<std::mutex> lock(state->mutex);
-            while (!state->invocations.empty()) {
-                auto& tuple = state->invocations.front();
-                std::apply(state->func, std::move(tuple));
-                state->invocations.pop();
-            }
-
-            // scheduler retains state, so state needs to only retain scheduler
-            // while it has pending work or neither will ever be destroyed
-            state->scheduler.reset();
-        });
     }
 
     const util::UniqueFunction<void(Args...)>& func() const
     {
-        return m_state->func;
+        return *m_func;
     }
 
     void operator()(Args... args)
     {
         if (m_scheduler->is_on_thread()) {
-            m_state->func(std::forward<Args>(args)...);
+            (*m_func)(std::forward<Args>(args)...);
             return;
         }
-
-        {
-            std::unique_lock<std::mutex> lock(m_state->mutex);
-            m_state->scheduler = m_scheduler;
-            m_state->invocations.push(std::make_tuple(std::forward<Args>(args)...));
-        }
-        m_scheduler->notify();
+        m_scheduler->invoke([func = m_func, args = std::make_tuple(std::forward<Args>(args)...)]() mutable {
+            std::apply(*func, std::move(args));
+        });
     }
 };
 

test/object-store/realm.cpp
@@ -477,12 +477,11 @@ TEST_CASE("SharedRealm: get_shared_realm()") {
             const SimpleScheduler* o = dynamic_cast<const SimpleScheduler*>(other);
             return (o && (o->m_id == m_id));
         }
-        bool can_deliver_notifications() const noexcept override
+        bool can_invoke() const noexcept override
         {
             return false;
         }
-        void set_notify_callback(std::function<void()>) override {}
-        void notify() override {}
+        void invoke(util::UniqueFunction<void()>&&) override {}
 
     protected:
         size_t m_id;
@@ -929,7 +928,8 @@ TEST_CASE("Get Realm using Async Open", "[asyncOpen]") {
 }
 #endif
 
-TEST_CASE("SharedRealm: async_writes") {
+TEST_CASE("SharedRealm: async writes") {
+    _impl::RealmCoordinator::assert_no_open_realms();
     if (!util::EventLoop::has_implementation())
         return;
 
@@ -978,21 +978,31 @@ TEST_CASE("SharedRealm: async_writes") {
         REQUIRE(done);
     }
     SECTION("realm closed") {
-        bool timeout = false;
-        auto has_timer = realm->scheduler()->set_timeout_callback(100, [&timeout]() {
-            timeout = true;
+        auto scheduler = realm->scheduler();
+        realm->async_begin_transaction([&] {
+            // We should never get here as the realm is closed
+            FAIL();
         });
-        if (has_timer) {
-            realm->async_begin_transaction([&] {
-                // We should never get here as the realm is closed
-                done = true;
-            });
-            realm->close();
-            util::EventLoop::main().run_until([&] {
-                return done || timeout;
-            });
-            REQUIRE(!done);
-        }
+        realm->close();
+        scheduler->invoke([&] {
+            done = true;
+        });
+        util::EventLoop::main().run_until([&] {
+            return done;
+        });
+    }
+    SECTION("realm invalidated") {
+        realm->async_begin_transaction([&] {
+            // We should never get here as the realm is closed
+            FAIL();
+        });
+        realm->invalidate();
+        realm->scheduler()->invoke([&] {
+            done = true;
+        });
+        util::EventLoop::main().run_until([&] {
+            return done;
+        });
     }
     SECTION("realm closed when sync in progress") {
         bool persisted = false;

src/realm.h
@@ -338,8 +338,6 @@ typedef void (*realm_scheduler_notify_func_t)(void* userdata);
 typedef bool (*realm_scheduler_is_on_thread_func_t)(void* userdata);
 typedef bool (*realm_scheduler_is_same_as_func_t)(const void* userdata1, const void* userdata2);
 typedef bool (*realm_scheduler_can_deliver_notifications_func_t)(void* userdata);
-typedef void (*realm_scheduler_set_notify_callback_func_t)(void* userdata, void* callback_userdata,
-                                                           realm_free_userdata_func_t, realm_scheduler_notify_func_t);
 typedef realm_scheduler_t* (*realm_scheduler_default_factory_func_t)(void* userdata);
 
 /**
@@ -746,25 +744,31 @@ RLM_API void realm_config_set_max_number_of_active_versions(realm_config_t*, uin
  * Create a custom scheduler object from callback functions.
  *
  * @param userdata Pointer passed to all callbacks.
- * @param notify Function to trigger a call to the registered callback on the
- *               scheduler's event loop. This function must be thread-safe, or
- *               NULL, in which case the scheduler is considered unable to
- *               deliver notifications.
+ * @param notify Function which will be called whenever the scheduler has work
+ *               to do. Each call to this should trigger a call to
+ *               `realm_scheduler_perform_work()` from within the scheduler's
+ *               event loop. This function must be thread-safe, or NULL, in
+ *               which case the scheduler is considered unable to deliver
+ *               notifications.
  * @param is_on_thread Function to return true if called from the same thread as
  *                     the scheduler. This function must be thread-safe.
  * @param can_deliver_notifications Function to return true if the scheduler can
  *                                  support `notify()`. This function does not
  *                                  need to be thread-safe.
- * @param set_notify_callback Function to accept a callback that will be invoked
- *                            by `notify()` on the scheduler's event loop. This
- *                            function does not need to be thread-safe.
  */
 RLM_API realm_scheduler_t*
 realm_scheduler_new(void* userdata, realm_free_userdata_func_t, realm_scheduler_notify_func_t notify,
                     realm_scheduler_is_on_thread_func_t is_on_thread, realm_scheduler_is_same_as_func_t is_same_as,
-                    realm_scheduler_can_deliver_notifications_func_t can_deliver_notifications,
-                    realm_scheduler_set_notify_callback_func_t set_notify_callback);
+                    realm_scheduler_can_deliver_notifications_func_t can_deliver_notifications);
 
+/**
+ * Performs all of the pending work for the given scheduler.
+ *
+ * This function must be called from within the scheduler's event loop. It must
+ * be called after each time that the notify function passed to the scheduler
+ * is involved.
+ */
+RLM_API void realm_scheduler_perform_work(realm_scheduler_t*);
 /**
  * Create an instance of the default scheduler for the current platform,
  * normally confined to the calling thread.
@@ -805,40 +809,6 @@ RLM_API bool realm_scheduler_has_default_factory(void);
  */
 RLM_API bool realm_scheduler_set_default_factory(void* userdata, realm_free_userdata_func_t,
                                                  realm_scheduler_default_factory_func_t);
-
-/**
- * Trigger a call to the registered notifier callback on the scheduler's event loop.
- *
- * This function is thread-safe.
- */
-RLM_API void realm_scheduler_notify(realm_scheduler_t*);
-
-/**
- * Returns true if the caller is currently running on the scheduler's thread.
- *
- * This function is thread-safe.
- */
-RLM_API bool realm_scheduler_is_on_thread(const realm_scheduler_t*);
-
-/**
- * Returns true if the scheduler is able to deliver notifications.
- *
- * A false return value may indicate that notifications are not applicable for
- * the scheduler, not implementable, or a temporary inability to deliver
- * notifications.
- *
- * This function is not thread-safe.
- */
-RLM_API bool realm_scheduler_can_deliver_notifications(const realm_scheduler_t*);
-
-/**
- * Set the callback that will be invoked by `realm_scheduler_notify()`.
- *
- * This function is not thread-safe.
- */
-RLM_API bool realm_scheduler_set_notify_callback(realm_scheduler_t*, void* userdata, realm_free_userdata_func_t,
-                                                 realm_scheduler_notify_func_t);
-
 
 /**
  * Open a Realm file.

src/realm/db.cpp
@@ -1713,6 +1713,7 @@ public:
     void sync_to_disk(util::UniqueFunction<void()> fn)
     {
         std::unique_lock lg(m_mutex);
+        REALM_ASSERT(!m_pending_sync);
         m_pending_sync.emplace(std::move(fn));
         m_changed.notify_one();
     }
@@ -2760,7 +2761,7 @@ TransactionRef DB::start_write(bool nonblocking)
     return tr;
 }
 
-void DB::async_request_write_mutex(TransactionRef& tr, util::UniqueFunction<void()>& when_acquired)
+void DB::async_request_write_mutex(TransactionRef& tr, util::UniqueFunction<void()>&& when_acquired)
 {
     std::unique_lock<std::mutex> lck(tr->m_async_mutex);
     REALM_ASSERT(tr->m_async_stage == Transaction::AsyncState::Idle);

src/realm/db.hpp
@@ -243,7 +243,7 @@ public:
     // ask for write mutex. Callback takes place when mutex has been acquired.
     // callback may occur on ANOTHER THREAD. Must not be called if write mutex
     // has already been acquired.
-    void async_request_write_mutex(TransactionRef& tr, util::UniqueFunction<void()>& when_acquired);
+    void async_request_write_mutex(TransactionRef& tr, util::UniqueFunction<void()>&& when_acquired);
 
     // report statistics of last commit done on THIS DB.
     // The free space reported is what can be expected to be freed

src/realm/object-store/c_api/scheduler.cpp
@@ -18,64 +18,15 @@ using namespace realm::util;
 namespace realm::c_api {
 namespace {
 
-// A callback coming from C++ code registered in a scheduler created by
-// `realm_scheduler_new()`.
-struct NotifyCppCallback {
-    std::function<void()> m_callback;
-};
-
-void free_notify_cpp_callback(void* ptr)
-{
-    delete static_cast<NotifyCppCallback*>(ptr);
-}
-
-void invoke_notify_cpp_callback(void* ptr)
-{
-    static_cast<NotifyCppCallback*>(ptr)->m_callback();
-}
-
-// A callback coming from C code registered in a scheduler defined in C++ code.
-struct NotifyCAPICallback {
-    struct Inner {
-        void* m_userdata = nullptr;
-        realm_free_userdata_func_t m_free = nullptr;
-        realm_scheduler_notify_func_t m_notify = nullptr;
-
-        ~Inner()
-        {
-            if (m_free)
-                m_free(m_userdata);
-        }
-    };
-
-    // Indirection because we are wrapping ourselves in an `std::function`,
-    // which must be copyable.
-    std::shared_ptr<Inner> m_inner;
-
-    NotifyCAPICallback(void* userdata, realm_free_userdata_func_t free_func, realm_scheduler_notify_func_t notify)
-        : m_inner(std::make_shared<Inner>())
-    {
-        m_inner->m_userdata = userdata;
-        m_inner->m_free = free_func;
-        m_inner->m_notify = notify;
-    }
-
-    void operator()()
-    {
-        if (m_inner->m_notify)
-            m_inner->m_notify(m_inner->m_userdata);
-    }
-};
-
 struct CAPIScheduler : Scheduler {
     void* m_userdata = nullptr;
     realm_free_userdata_func_t m_free = nullptr;
     realm_scheduler_notify_func_t m_notify = nullptr;
     realm_scheduler_is_on_thread_func_t m_is_on_thread = nullptr;
     realm_scheduler_is_same_as_func_t m_is_same_as = nullptr;
-    realm_scheduler_can_deliver_notifications_func_t m_can_deliver_notifications = nullptr;
-    realm_scheduler_set_notify_callback_func_t m_set_notify_callback = nullptr;
+    realm_scheduler_can_deliver_notifications_func_t m_can_invoke = nullptr;
 
+    InvocationQueue m_queue;
 
     CAPIScheduler() = default;
     CAPIScheduler(CAPIScheduler&& other)
@@ -83,8 +34,7 @@ struct CAPIScheduler : Scheduler {
         , m_free(std::exchange(other.m_free, nullptr))
         , m_notify(std::exchange(other.m_notify, nullptr))
         , m_is_on_thread(std::exchange(other.m_is_on_thread, nullptr))
-        , m_can_deliver_notifications(std::exchange(other.m_can_deliver_notifications, nullptr))
-        , m_set_notify_callback(std::exchange(other.m_set_notify_callback, nullptr))
+        , m_can_invoke(std::exchange(other.m_can_invoke, nullptr))
     {
     }
 
@@ -94,10 +44,12 @@ struct CAPIScheduler : Scheduler {
             m_free(m_userdata);
     }
 
-    void notify() final
+    void invoke(util::UniqueFunction<void()>&& fn) final
     {
-        if (m_notify)
+        if (m_notify) {
+            m_queue.push(std::move(fn));
             m_notify(m_userdata);
+        }
     }
 
     bool is_on_thread() const noexcept final
@@ -112,8 +64,7 @@ struct CAPIScheduler : Scheduler {
         if (auto rhs = dynamic_cast<const CAPIScheduler*>(other)) {
             bool same_callbacks = m_free == rhs->m_free && m_notify == rhs->m_notify &&
                                   m_is_same_as == rhs->m_is_same_as && m_is_on_thread == rhs->m_is_on_thread &&
-                                  m_can_deliver_notifications == rhs->m_can_deliver_notifications &&
-                                  m_set_notify_callback == rhs->m_set_notify_callback;
+                                  m_can_invoke == rhs->m_can_invoke;
             if (same_callbacks && m_userdata == rhs->m_userdata) {
                 return true;
             }
@@ -124,19 +75,11 @@ struct CAPIScheduler : Scheduler {
         return false;
     }
 
-    bool can_deliver_notifications() const noexcept final
+    bool can_invoke() const noexcept final
     {
-        if (m_can_deliver_notifications)
-            return m_can_deliver_notifications(m_userdata);
+        if (m_can_invoke)
+            return m_can_invoke(m_userdata);
         return false;
-    }
-
-    void set_notify_callback(std::function<void()> callback) final
-    {
-        if (m_set_notify_callback) {
-            auto ptr = new NotifyCppCallback{std::move(callback)};
-            m_set_notify_callback(m_userdata, ptr, free_notify_cpp_callback, invoke_notify_cpp_callback);
-        }
     }
 };
 
@@ -184,8 +127,7 @@ RLM_API realm_scheduler_t*
 realm_scheduler_new(void* userdata, realm_free_userdata_func_t free_func, realm_scheduler_notify_func_t notify_func,
                     realm_scheduler_is_on_thread_func_t is_on_thread_func,
                     realm_scheduler_is_same_as_func_t is_same_as,
-                    realm_scheduler_can_deliver_notifications_func_t can_deliver_notifications_func,
-                    realm_scheduler_set_notify_callback_func_t set_notify_callback_func)
+                    realm_scheduler_can_deliver_notifications_func_t can_deliver_notifications_func)
 {
     return wrap_err([&]() {
         auto capi_scheduler = std::make_shared<CAPIScheduler>();
@@ -194,10 +136,16 @@ realm_scheduler_new(void* userdata, realm_free_userdata_func_t free_func, realm_
         capi_scheduler->m_notify = notify_func;
         capi_scheduler->m_is_on_thread = is_on_thread_func;
         capi_scheduler->m_is_same_as = is_same_as;
-        capi_scheduler->m_can_deliver_notifications = can_deliver_notifications_func;
-        capi_scheduler->m_set_notify_callback = set_notify_callback_func;
+        capi_scheduler->m_can_invoke = can_deliver_notifications_func;
         return new realm_scheduler_t(std::move(capi_scheduler));
     });
+}
+
+RLM_API void realm_scheduler_perform_work(realm_scheduler_t* scheduler)
+{
+    if (auto capi_scheduler = dynamic_cast<CAPIScheduler*>(scheduler->get())) {
+        capi_scheduler->m_queue.invoke_all();
+    }
 }
 
 RLM_API realm_scheduler_t* realm_scheduler_make_default()
@@ -252,41 +200,6 @@ RLM_API bool realm_scheduler_set_default_factory(void* userdata, realm_free_user
         s_default_factory_set = true;
         return true;
 #endif
-    });
-}
-
-RLM_API void realm_scheduler_notify(realm_scheduler_t* scheduler)
-{
-    (*scheduler)->notify();
-}
-
-RLM_API bool realm_scheduler_is_on_thread(const realm_scheduler_t* scheduler)
-{
-    return (*scheduler)->is_on_thread();
-}
-
-RLM_API bool realm_scheduler_can_deliver_notifications(const realm_scheduler_t* scheduler)
-{
-    return (*scheduler)->can_deliver_notifications();
-}
-
-RLM_API bool realm_scheduler_set_notify_callback(realm_scheduler_t* scheduler, void* userdata,
-                                                 realm_free_userdata_func_t free_func,
-                                                 realm_scheduler_notify_func_t notify_func)
-{
-    return wrap_err([&]() {
-        auto capi_scheduler = dynamic_cast<CAPIScheduler*>(scheduler->get());
-        if (capi_scheduler) {
-            // Avoid needless roundtrips through the std::function wrappers.
-            if (capi_scheduler->m_set_notify_callback) {
-                capi_scheduler->m_set_notify_callback(capi_scheduler->m_userdata, userdata, free_func, notify_func);
-            }
-        }
-        else {
-            NotifyCAPICallback callback{userdata, free_func, notify_func};
-            (*scheduler)->set_notify_callback(std::move(callback));
-        }
-        return true;
     });
 }
 

src/realm/object-store/impl/realm_coordinator.cpp
@@ -1299,3 +1299,14 @@ void RealmCoordinator::write_copy(StringData path, BinaryData key, bool allow_ov
 {
     m_db->write_copy(path, key.data(), allow_overwrite);
 }
+
+void RealmCoordinator::async_request_write_mutex(Realm& realm)
+{
+    auto tr = Realm::Internal::get_transaction_ref(realm);
+    m_db->async_request_write_mutex(tr, [realm = realm.shared_from_this()]() mutable {
+        auto& scheduler = *realm->scheduler();
+        scheduler.invoke([realm = std::move(realm)] {
+            realm->run_writes();
+        });
+    });
+}

src/realm/object-store/impl/realm_coordinator.hpp
@@ -201,10 +201,7 @@ public:
     template <typename Pred>
     util::CheckedUniqueLock wait_for_notifiers(Pred&& wait_predicate) REQUIRES(!m_notifier_mutex);
 
-    void async_request_write_mutex(TransactionRef& tr, util::UniqueFunction<void()> when_acquired = nullptr)
-    {
-        m_db->async_request_write_mutex(tr, when_acquired);
-    }
+    void async_request_write_mutex(Realm& realm);
 
     AuditInterface* audit_context() const noexcept
     {

commit e96c4512e363e9319a3228fc52c0bd70bdc6cb7e

Ensure scheduler is not deleted until callback has been done
This is fixing a problem introduced in 14210d1dbd235

The issue was discovered in realm-js, where async open would
hang because the scheduler was deleted before the callback was done on the
main thread. Beforehand the EventLoopDispatcher has a State member that
would retain the scheduler until the invocations were done.

src/realm/object-store/util/event_loop_dispatcher.hpp
@@ -54,9 +54,12 @@ public:
             (*m_func)(std::forward<Args>(args)...);
             return;
         }
-        m_scheduler->invoke([func = m_func, args = std::make_tuple(std::forward<Args>(args)...)]() mutable {
-            std::apply(*func, std::move(args));
-        });
+        m_scheduler->invoke(
+            [scheduler = m_scheduler, func = m_func, args = std::make_tuple(std::forward<Args>(args)...)]() mutable {
+                std::apply(*func, std::move(args));
+                // Each invocation block will retain the scheduler, so the scheduler
+                // will not be released until all blocks are called
+            });
     }
 };
 

test/object-store/realm.cpp
@@ -33,6 +33,7 @@
 #include <realm/object-store/schema.hpp>
 #include <realm/object-store/thread_safe_reference.hpp>
 #include <realm/object-store/util/scheduler.hpp>
+#include <realm/object-store/util/event_loop_dispatcher.hpp>
 
 #if REALM_ENABLE_SYNC
 #include <realm/object-store/sync/async_open_task.hpp>
@@ -616,16 +617,15 @@ TEST_CASE("Get Realm using Async Open", "[asyncOpen]") {
     std::mutex mutex;
     SECTION("can open synced Realms that don't already exist") {
         ThreadSafeReference realm_ref;
-        std::atomic<bool> called{false};
+        bool called = false;
         auto task = Realm::get_synchronized_realm(config);
-        task->start([&](auto ref, auto error) {
-            std::lock_guard<std::mutex> lock(mutex);
+        task->start(realm::util::EventLoopDispatcher([&](ThreadSafeReference&& ref, std::exception_ptr error) {
             REQUIRE(!error);
             called = true;
             realm_ref = std::move(ref);
-        });
+        }));
         util::EventLoop::main().run_until([&] {
-            return called.load();
+            return called;
         });
         std::lock_guard<std::mutex> lock(mutex);
         REQUIRE(called);