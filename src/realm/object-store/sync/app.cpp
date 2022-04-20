a36e6fd3a31ccaa3bfc7831d040ed2e4cc0e90f1
Christian Melchior <christian@ilios.dk>
2022-04-20 17:43:33 +0200

    Formatting

--- src/realm/object-store/sync/app.cpp
+++ src/realm/object-store/sync/app.cpp
@@ -649,11 +649,11 @@ void App::delete_user(const std::shared_ptr<SyncUser>& user, UniqueFunction<void
 {
     if (!user) {
         return completion(AppError(make_client_error_code(ClientErrorCode::user_not_found),
-                                                  "The specified user could not be found. Was it already deleted?"));
+                                   "The specified user could not be found. Was it already deleted?"));
     }
     if (user->state() != SyncUser::State::LoggedIn) {
         return completion(AppError(make_client_error_code(ClientErrorCode::user_not_logged_in),
-                                                  "User must be logged in to be deleted."));
+                                   "User must be logged in to be deleted."));
     }
 
     if (!verify_user_present(user)) {
@@ -682,15 +682,15 @@ void App::link_user(const std::shared_ptr<SyncUser>& user, const AppCredentials&
 {
     if (!user) {
         return completion(AppError(make_client_error_code(ClientErrorCode::user_not_found),
-                                                  "The specified user could not be found. Was it deleted?"));
+                                   "The specified user could not be found. Was it deleted?"));
     }
     if (user->state() != SyncUser::State::LoggedIn) {
         return completion(AppError(make_client_error_code(ClientErrorCode::user_not_logged_in),
-                                                  "The specified user is not logged in."));
+                                   "The specified user is not logged in."));
     }
     if (!verify_user_present(user)) {
         return completion(AppError(make_client_error_code(ClientErrorCode::user_not_found),
-                                            "The specified user was not found. Was it deleted?"));
+                                   "The specified user was not found. Was it deleted?"));
     }
 
     App::log_in_with_credentials(credentials, user, std::move(completion));
