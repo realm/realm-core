e504abe833b7d30f5892ac4cae20bdc6482b5599
Dominic Frei <business.dominic.frei@gmail.com>
2022-04-29 11:04:20 +0100

    Update src/realm/object-store/shared_realm.hpp
    Co-authored-by: FFranck <franck.franck@mongodb.com>

--- src/realm/object-store/shared_realm.hpp
+++ src/realm/object-store/shared_realm.hpp
@@ -369,7 +369,7 @@ public:
      */
     void convert(const Config& config);
     /**
-     * Copy a Realm using a config.
+     * Copy a Realm using a path.
      *
      * @param path The path the realm should be copied to. Local realms will remain local, synced
      *             realms will remain synced realms.
