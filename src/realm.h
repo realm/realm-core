7f98429af79fb3899a1220826a0866c748411670
Yavor Georgiev <fealebenpae@gmail.com>
2022-05-04 20:02:23 +0200

    fix code formatting

--- src/realm.h
+++ src/realm.h
@@ -3219,14 +3219,14 @@ RLM_API void realm_sync_config_set_resync_mode(realm_sync_config_t*,
 RLM_API realm_object_id_t realm_flx_sync_subscription_id(const realm_flx_sync_subscription_t* subscription)
     RLM_API_NOEXCEPT;
 
-RLM_API realm_string_t
-realm_flx_sync_subscription_name(const realm_flx_sync_subscription_t* subscription) RLM_API_NOEXCEPT;
+RLM_API realm_string_t realm_flx_sync_subscription_name(const realm_flx_sync_subscription_t* subscription)
+    RLM_API_NOEXCEPT;
 
 RLM_API realm_string_t
 realm_flx_sync_subscription_object_class_name(const realm_flx_sync_subscription_t* subscription) RLM_API_NOEXCEPT;
 
-RLM_API realm_string_t
-realm_flx_sync_subscription_query_string(const realm_flx_sync_subscription_t* subscription) RLM_API_NOEXCEPT;
+RLM_API realm_string_t realm_flx_sync_subscription_query_string(const realm_flx_sync_subscription_t* subscription)
+    RLM_API_NOEXCEPT;
 
 RLM_API realm_timestamp_t realm_flx_sync_subscription_created_at(const realm_flx_sync_subscription_t* subscription)
     RLM_API_NOEXCEPT;
