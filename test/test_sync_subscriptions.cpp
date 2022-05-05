ab3bad0ddbbae13503852e38455737f6ce2c1d2b
Jonathan Reams <jonathan@reamstronics.net>
2022-05-05 16:59:12 -0400

    fix windows build

--- test/test_sync_subscriptions.cpp
+++ test/test_sync_subscriptions.cpp
@@ -429,7 +429,7 @@ TEST(Sync_SubscriptionStoreInternalSchemaMigration)
     //   });
     auto path = std::filesystem::path(test_util::get_test_resource_path()) / "test_flx_metadata_tables_v1.realm";
     CHECK(util::File::exists(path.string()));
-    util::File::copy(path.native(), sub_store_path);
+    util::File::copy(path.string(), sub_store_path);
     SubscriptionStoreFixture fixture(sub_store_path);
     auto store = SubscriptionStore::create(fixture.db, [](int64_t) {});
     auto [active_version, latest_version] = store->get_active_and_latest_versions();
