6dc330f3b022977be6ab6454da731034f7273dc9
Kenneth Geisshirt <geisshirt@gmail.com>
2022-05-12 16:45:53 +0200

    More constructors

--- src/realm/object-store/object_schema.hpp
+++ src/realm/object-store/object_schema.hpp
@@ -49,7 +49,7 @@ public:
                  std::initializer_list<Property> computed_properties, std::string name_alias = "");
     ObjectSchema(std::string name, IsEmbedded is_embedded, std::initializer_list<Property> persisted_properties,
                  std::initializer_list<Property> computed_properties, std::string name_alias = "");
-    ObjectSchema(std::string name, IsEmbedded is_embedded, IsAsymmetric is_asymmetric, std::initializer_list<Property> persisted_properties,
+    ObjectSchema(std::string name, IsAsymmetric is_asymmetric, std::initializer_list<Property> persisted_properties,
                  std::initializer_list<Property> computed_properties, std::string name_alias = "");
     ~ObjectSchema();
 
