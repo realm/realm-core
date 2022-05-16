c3610613f1187b3ec2a8c89a146d41a118ecacc4
Nicola Cabiddu <nicola.cabiddu@mongodb.com>
2022-05-16 15:39:01 +0100

    removed reduntant if

--- src/realm/object-store/c_api/app.cpp
+++ src/realm/object-store/c_api/app.cpp
@@ -819,10 +819,8 @@ RLM_API realm_app_t* realm_user_get_app(const realm_user_t* user)
 {
     REALM_ASSERT(user);
     return wrap_err([&]() -> realm_app_t* {
-        if ((*user)->state() != SyncUser::State::Removed) {
-            if (auto shared_app = (*user)->sync_manager()->app().lock()) {
-                return new realm_app_t(shared_app);
-            }
+        if (auto shared_app = (*user)->sync_manager()->app().lock()) {
+            return new realm_app_t(shared_app);
         }
         return nullptr;
     });
