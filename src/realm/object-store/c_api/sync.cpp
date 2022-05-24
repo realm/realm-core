b06671e40fa1d0d657ad866e94876e770a6062be
nicola cabiddu <nicola.cabiddu@mongodb.com>
2022-05-24 10:27:42 +0100

    added flx sync errors to C API (#5523)
    * added flx sync errors to C API
    
    * changelog entry
    
    * Add one more error code in C API
    
    Co-authored-by: Jørgen Edelbo <jorgen.edelbo@mongodb.com>

--- src/realm/object-store/c_api/sync.cpp
+++ src/realm/object-store/c_api/sync.cpp
@@ -110,6 +110,8 @@ static_assert(realm_sync_errno_client_e(Client::Error::bad_state_message) == RLM
 static_assert(realm_sync_errno_client_e(Client::Error::missing_protocol_feature) ==
               RLM_SYNC_ERR_CLIENT_MISSING_PROTOCOL_FEATURE);
 static_assert(realm_sync_errno_client_e(Client::Error::http_tunnel_failed) == RLM_SYNC_ERR_CLIENT_HTTP_TUNNEL_FAILED);
+static_assert(realm_sync_errno_client_e(Client::Error::auto_client_reset_failure) ==
+              RLM_SYNC_ERR_CLIENT_AUTO_CLIENT_RESET_FAILURE);
 } // namespace
 
 namespace {
@@ -183,6 +185,14 @@ static_assert(realm_sync_errno_session_e(ProtocolError::user_mismatch) == RLM_SY
 static_assert(realm_sync_errno_session_e(ProtocolError::too_many_sessions) == RLM_SYNC_ERR_SESSION_TOO_MANY_SESSIONS);
 static_assert(realm_sync_errno_session_e(ProtocolError::invalid_schema_change) ==
               RLM_SYNC_ERR_SESSION_INVALID_SCHEMA_CHANGE);
+static_assert(realm_sync_errno_session_e(ProtocolError::bad_query) == RLM_SYNC_ERR_SESSION_BAD_QUERY);
+static_assert(realm_sync_errno_session_e(ProtocolError::object_already_exists) ==
+              RLM_SYNC_ERR_SESSION_OBJECT_ALREADY_EXISTS);
+static_assert(realm_sync_errno_session_e(ProtocolError::server_permissions_changed) ==
+              RLM_SYNC_ERR_SESSION_SERVER_PERMISSIONS_CHANGED);
+static_assert(realm_sync_errno_session_e(ProtocolError::initial_sync_not_completed) ==
+              RLM_SYNC_ERR_SESSION_INITIAL_SYNC_NOT_COMPLETED);
+static_assert(realm_sync_errno_session_e(ProtocolError::write_not_allowed) == RLM_SYNC_ERR_SESSION_WRITE_NOT_ALLOWED);
 } // namespace
 
 static realm_sync_error_code_t to_capi(const std::error_code& error_code, std::string& message)
