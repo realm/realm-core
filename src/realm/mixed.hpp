6b280bf93801bf74c39ba898c294a71f931e7551
Jonathan Reams <jonathan@reamstronics.net>
2022-05-25 00:19:47 -0400

    formatting

--- src/realm/mixed.hpp
+++ src/realm/mixed.hpp
@@ -158,9 +158,7 @@ public:
     {
     }
 
-    ~Mixed() noexcept
-    {
-    }
+    ~Mixed() noexcept {}
 
     DataType get_type() const noexcept
     {
@@ -298,6 +296,7 @@ public:
     }
 
     using Mixed::Mixed;
+
 private:
     std::string m_owned_string;
 };
