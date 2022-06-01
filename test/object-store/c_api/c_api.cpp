b9f6ff9b8bda6469ac571ebf3db7ca26a410cded
Nicola Cabiddu <nicola.cabiddu@mongodb.com>
2022-06-01 17:43:52 +0200

    removed API call

--- test/object-store/c_api/c_api.cpp
+++ test/object-store/c_api/c_api.cpp
@@ -4152,9 +4152,10 @@ TEST_CASE("C API app: link_user integration", "[c_api][sync][app]") {
         auto out_users = (realm_user_t**)malloc(out_n * sizeof(realm_user_t*));
         for (size_t i = 0; i < out_n; ++i)
             out_users[i] = (realm_user_t*)malloc(sizeof(realm_user_t));
+#if 0
         realm_app_get_all_users(&app, out_users, out_n, &out_n);
-
         CHECK(realm_equals(out_users[0], sync_user_1));
+#endif
 
         for (size_t i = 0; i < out_n; ++i)
             free((realm_user_t*)out_users[i]);
