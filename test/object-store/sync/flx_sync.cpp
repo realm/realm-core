68062b69dedda89b50d351afc11624f28234dbe8
Jonathan Reams <jonathan@reamstronics.net>
2022-06-01 12:21:16 -0400

    make sure to call sync transact notifier

--- test/object-store/sync/flx_sync.cpp
+++ test/object-store/sync/flx_sync.cpp
@@ -130,6 +130,7 @@ TEST_CASE("flx: connect to FLX-enabled app", "[sync][flx][app]") {
             empty_subs.get_state_change_notification(sync::SubscriptionSet::State::Complete).get();
         }
 
+        realm->set_auto_refresh(true);
         auto table = realm->read_group().get_table("class_TopLevel");
         auto col_key = table->get_column_key("queryable_str_field");
         Query query_foo(table);
@@ -143,7 +144,6 @@ TEST_CASE("flx: connect to FLX-enabled app", "[sync][flx][app]") {
 
         wait_for_download(*realm);
         {
-            realm->refresh();
             Results results(realm, table);
             CHECK(results.size() == 1);
             auto obj = results.get<Obj>(0);
@@ -161,7 +161,6 @@ TEST_CASE("flx: connect to FLX-enabled app", "[sync][flx][app]") {
         }
 
         {
-            realm->refresh();
             Results results(realm, Query(table));
             CHECK(results.size() == 2);
         }
@@ -179,7 +178,6 @@ TEST_CASE("flx: connect to FLX-enabled app", "[sync][flx][app]") {
         }
 
         {
-            realm->refresh();
             Results results(realm, Query(table));
             CHECK(results.size() == 1);
             auto obj = results.get<Obj>(0);
@@ -195,7 +193,6 @@ TEST_CASE("flx: connect to FLX-enabled app", "[sync][flx][app]") {
         }
 
         {
-            realm->refresh();
             Results results(realm, table);
             CHECK(results.size() == 0);
         }
