040339494aa381a0bee3534cf76cbf923062c139
Daniel Tabacaru <daniel.tabacaru9@gmail.com>
2022-06-17 14:11:22 +0200

    Allow lists and dictionaries of embedded objects in asymmetric objects

--- src/realm/table.cpp
+++ src/realm/table.cpp
@@ -359,7 +359,7 @@ ColKey Table::add_column(Table& target, StringData name)
         throw LogicError(LogicError::wrong_kind_of_table);
     if (origin_group != target_group)
         throw LogicError(LogicError::group_mismatch);
-    // Outgoing links from an asymmetric table are not allowed.
+    // Only links to embedded objects are allowed.
     if (is_asymmetric() && !target.is_embedded()) {
         throw LogicError(LogicError::wrong_kind_of_table);
     }
@@ -409,8 +409,8 @@ ColKey Table::add_column_list(Table& target, StringData name)
         throw LogicError(LogicError::wrong_kind_of_table);
     if (origin_group != target_group)
         throw LogicError(LogicError::group_mismatch);
-    // Outgoing links from an asymmetric table are not allowed.
-    if (is_asymmetric()) {
+    // Only links to embedded objects are allowed.
+    if (is_asymmetric() && !target.is_embedded()) {
         throw LogicError(LogicError::wrong_kind_of_table);
     }
     // Incoming links from an asymmetric table are not allowed.
@@ -487,8 +487,8 @@ ColKey Table::add_column_dictionary(Table& target, StringData name, DataType key
         throw LogicError(LogicError::wrong_kind_of_table);
     if (origin_group != target_group)
         throw LogicError(LogicError::group_mismatch);
-    // Outgoing links from an asymmetric table are not allowed.
-    if (is_asymmetric()) {
+    // Only links to embedded objects are allowed.
+    if (is_asymmetric() && !target.is_embedded()) {
         throw LogicError(LogicError::wrong_kind_of_table);
     }
     // Incoming links from an asymmetric table are not allowed.
