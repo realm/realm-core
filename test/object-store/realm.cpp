c6db4f39deb72613fa512ff62d8ca15f2cbe982c
Nicola Cabiddu <nicola.cabiddu@mongodb.com>
2022-07-07 12:34:19 +0200

    indentation fix

--- test/object-store/realm.cpp
+++ test/object-store/realm.cpp
@@ -1043,11 +1043,11 @@ TEST_CASE("Get Realm using Async Open", "[asyncOpen]") {
 
 //#ifndef _WIN32
 TEST_CASE("SharedRealm: async writes") {
-    
+
 #ifdef _WIN32
     _impl::RealmCoordinator::clear_all_caches();
 #endif
-    
+
     _impl::RealmCoordinator::assert_no_open_realms();
     if (!util::EventLoop::has_implementation())
         return;
