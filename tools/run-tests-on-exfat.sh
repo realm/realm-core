4eddeb7bed7071a7d64aebe3fcd7ab43634d6078
James Stone <james.stone@mongodb.com>
2022-08-31 15:10:32 -0700

    Add a fallback to full sync if fcntl(F_BARRIERFSYNC) fails
    F_BARRIERFSYNC is available on HFS and APFS, but is not supported
    on filesystems such as exFAT.

--- tools/run-tests-on-exfat.sh
+++ tools/run-tests-on-exfat.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+set -e -x
 
 readonly build_dir="$PWD"
 readonly dmg_file="$build_dir/exfat.dmg"
 
-if ! [ -f "$build_dir/test/realm-tests" ]; then
+if ! [ -f "$build_dir/test/realm-tests.app/Contents/MacOS/realm-tests" ]; then
     echo 'Run this script from the build directory after building tests'
     exit 1
 fi
@@ -19,10 +19,11 @@ function cleanup() {
 trap cleanup EXIT
 
 rm -f "$dmg_file"
-hdiutil create -fs exFAT -size 100MB "$dmg_file"
+hdiutil create -fs exFAT -size 400MB "$dmg_file"
 hdiutil_out=$(hdiutil attach exfat.dmg)
 device=$(echo "$hdiutil_out" | head -n1 | cut -f1 | awk '{$1=$1};1')
 path=$(echo "$hdiutil_out" | tail -n1 | cut -f3)
 
-./test/realm-tests "$path/"
+UNITTEST_ENABLE_SYNC_TO_DISK=1 ./test/realm-tests.app/Contents/MacOS/realm-tests "$path/"
+UNITTEST_ENABLE_SYNC_TO_DISK=1 ./test/realm-sync-tests.app/Contents/MacOS/realm-sync-tests "$path/"
 
