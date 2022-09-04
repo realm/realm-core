3863088563c4cfb6fa68e1f6cfd5c6c00fb7dbcc
Christian Melchior <christian.melchior@mongodb.com>
2022-09-04 19:23:48 +0200

    Add support for using Realm Core as a sub project.

--- tools/cmake/XCodeGeneratorCCacheSupport.cmake
+++ tools/cmake/XCodeGeneratorCCacheSupport.cmake
@@ -1,31 +1,36 @@
 # Work-around for the XCode Generator not working correctly with CCache.
-# While not needed for Ninja and Makefiles, they use the same helper scripts 
+# While not needed for Ninja and Makefiles, use the same helper scripts 
 # to ensure that all build types are routed through the same code path.
 #
 # See # See https://crascit.com/2016/04/09/using-ccache-with-cmake/#h-improved-functionality-from-cmake-3-4
-
+#
+# The work-around mentioned in the above link modified slightly to also work when Realm Core is used through
+# `add_subdirectory()`.
+#
 find_program(CCACHE_PROGRAM ccache)
 if(CCACHE_PROGRAM)
+    message(STATUS "Found ccache. Copying launcher scripts from ${PROJECT_SOURCE_DIR} to ${PROJECT_BINARY_DIR}")
+
     # Set up wrapper scripts
     set(C_LAUNCHER   "${CCACHE_PROGRAM}")
     set(CXX_LAUNCHER "${CCACHE_PROGRAM}")
-    configure_file(./tools/cmake/launch-c.in launch-c)
-    configure_file(./tools/cmake/launch-cxx.in launch-cxx)
+    configure_file("${PROJECT_SOURCE_DIR}/tools/cmake/launch-c.in" "${PROJECT_BINARY_DIR}/launch-c")
+    configure_file("${PROJECT_SOURCE_DIR}/tools/cmake/launch-cxx.in" "${PROJECT_BINARY_DIR}/launch-cxx")
     execute_process(COMMAND chmod a+rx
-                     "${CMAKE_BINARY_DIR}/launch-c"
-                     "${CMAKE_BINARY_DIR}/launch-cxx"
+                     "${PROJECT_BINARY_DIR}/launch-c"
+                     "${PROJECT_BINARY_DIR}/launch-cxx"
     )
 
     if(CMAKE_GENERATOR STREQUAL "Xcode")
         # Set Xcode project attributes to route compilation and linking
         # through our scripts
-        set(CMAKE_XCODE_ATTRIBUTE_CC         "${CMAKE_BINARY_DIR}/launch-c")
-        set(CMAKE_XCODE_ATTRIBUTE_CXX        "${CMAKE_BINARY_DIR}/launch-cxx")
-        set(CMAKE_XCODE_ATTRIBUTE_LD         "${CMAKE_BINARY_DIR}/launch-c")
-        set(CMAKE_XCODE_ATTRIBUTE_LDPLUSPLUS "${CMAKE_BINARY_DIR}/launch-cxx")
+        set(CMAKE_XCODE_ATTRIBUTE_CC         "${PROJECT_BINARY_DIR}/launch-c")
+        set(CMAKE_XCODE_ATTRIBUTE_CXX        "${PROJECT_BINARY_DIR}/launch-cxx")
+        set(CMAKE_XCODE_ATTRIBUTE_LD         "${PROJECT_BINARY_DIR}/launch-c")
+        set(CMAKE_XCODE_ATTRIBUTE_LDPLUSPLUS "${PROJECT_BINARY_DIR}/launch-cxx")
     else()
         # Support Unix Makefiles and Ninja
-        set(CMAKE_C_COMPILER_LAUNCHER   "${CMAKE_BINARY_DIR}/launch-c")
-        set(CMAKE_CXX_COMPILER_LAUNCHER "${CMAKE_BINARY_DIR}/launch-cxx")
+        set(CMAKE_C_COMPILER_LAUNCHER   "${PROJECT_BINARY_DIR}/launch-c")
+        set(CMAKE_CXX_COMPILER_LAUNCHER "${PROJECT_BINARY_DIR}/launch-cxx")
     endif()
  endif()
