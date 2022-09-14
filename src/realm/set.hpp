183259569a8507244615c0a5e356e60157cb1d35
Nicola Cabiddu <nicola.cabiddu@mongodb.com>
2022-09-14 11:45:14 +0100

    allow type_TypedLink for sets

--- src/realm/set.hpp
+++ src/realm/set.hpp
@@ -1240,13 +1240,25 @@ inline size_t LnkSet::find_any(Mixed value) const
 {
     if (value.is_null())
         return not_found;
-    if (value.get_type() != type_Link)
-        return not_found;
-    size_t found = find(value.get<ObjKey>());
-    if (found != not_found) {
-        found = real2virtual(found);
+
+    auto find_obj_key = [this](ObjKey objkey) {
+        size_t found = find(objkey);
+        if (found != not_found) {
+            found = real2virtual(found);
+        }
+        return found;
+    };
+    const auto type = value.get_type();
+    if (type == type_Link) {
+        return find_obj_key(value.get<ObjKey>());
     }
-    return found;
+    if (type == type_TypedLink) {
+        auto link = value.get_link();
+        if (link.get_table_key() == get_target_table()->get_key()) {
+            return find_obj_key(link.get_obj_key());
+        }
+    }
+    return not_found;
 }
 
 inline bool LnkSet::is_obj_valid(size_t) const noexcept
