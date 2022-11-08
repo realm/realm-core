be206de9796924d361e30a44c9437ae62050f524
Mathias Stearn <redbeard0531@gmail.com>
2022-11-08 14:05:11 +0100

    use a null pointer rather than empty string for error case in App::call_function callback

--- src/realm/object-store/sync/app.cpp
+++ src/realm/object-store/sync/app.cpp
@@ -1052,7 +1052,7 @@ std::string App::function_call_url_path() const
 
 void App::call_function(const std::shared_ptr<SyncUser>& user, const std::string& name, std::string_view args_ejson,
                         const Optional<std::string>& service_name_opt,
-                        UniqueFunction<void(const std::string&, Optional<AppError>)>&& completion)
+                        UniqueFunction<void(const std::string*, Optional<AppError>)>&& completion)
 {
     auto service_name = service_name_opt ? *service_name_opt : "<none>";
     if (would_log()) {
@@ -1069,9 +1069,9 @@ void App::call_function(const std::shared_ptr<SyncUser>& user, const std::string
             if (auto error = AppUtils::check_for_errors(response)) {
                 self->log("App: call_function: %1 service_name: %2 -> %3 ERROR: %4", name, service_name,
                           response.http_status_code, error->message);
-                return completion({}, error);
+                return completion(nullptr, error);
             }
-            completion(response.body, util::none);
+            completion(&response.body, util::none);
         });
 }
 
@@ -1092,13 +1092,13 @@ void App::call_function(const std::shared_ptr<SyncUser>& user, const std::string
 
     call_function(user, name, std::move(args_ejson).str(), service_name,
                   [self = shared_from_this(), name, service_name = std::move(service_name2),
-                   completion = std::move(completion)](const std::string& response, util::Optional<AppError>&& err) {
+                   completion = std::move(completion)](const std::string* response, util::Optional<AppError>&& err) {
                       if (err) {
                           return completion({}, err);
                       }
                       util::Optional<Bson> body_as_bson;
                       try {
-                          body_as_bson = bson::parse(response);
+                          body_as_bson = bson::parse(*response);
                           if (self->would_log()) {
                               self->log("App: call_function: %1 service_name: %2 - results: %3", name, service_name,
                                         body_as_bson ? body_as_bson->to_string() : "<none>");
