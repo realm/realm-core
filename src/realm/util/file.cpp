d718be8afcc491318172737e1322303f12d1d16c
Nicola Cabiddu <nicola.cabiddu@mongodb.com>
2022-11-10 15:21:02 +0000

    code cleanup

--- src/realm/util/file.cpp
+++ src/realm/util/file.cpp
@@ -1042,11 +1042,10 @@ void File::sync()
 void File::barrier()
 {
 #if REALM_PLATFORM_APPLE
-    if (::fcntl(m_fd, F_BARRIERFSYNC) == 0)
-        return;
-        // If F_BARRIERSYNC is not suppported (this is known on exFAT) we don't fallback to sync().
-        // This is not longer needed becasue msync already guarantees that the pages are going to be
-        // flushed on disk.
+    // If F_BARRIERSYNC is not suppported (this is known on exFAT) we don't fallback to sync().
+    // This is not longer needed becasue msync already guarantees that the pages are going to be
+    // flushed on disk.
+    ::fcntl(m_fd, F_BARRIERFSYNC);
 #else
     sync();
 #endif
