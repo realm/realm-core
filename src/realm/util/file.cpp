e4d8531376777be51e1c65bb9f5f8843ec9ed1b4
Nicola Cabiddu <nicola.cabiddu@mongodb.com>
2022-11-10 13:35:41 +0000

    cleanup

--- src/realm/util/file.cpp
+++ src/realm/util/file.cpp
@@ -1047,7 +1047,6 @@ void File::barrier()
         // If fcntl fails, we fallback to full sync.
         // This is known to occur on exFAT which does not support F_BARRIERSYNC.
 #endif
-    // sync();
 }
 
 #ifndef _WIN32
