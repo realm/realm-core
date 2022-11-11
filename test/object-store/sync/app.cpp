16b7d23971fa158cb8ec4a824b3d1294a1c6ce1b
Michael Wilkerson-Barker <michael.wilkersonbarker@mongodb.com>
2022-11-10 21:32:37 -0500

    Updates from review

--- test/object-store/sync/app.cpp
+++ test/object-store/sync/app.cpp
@@ -4431,13 +4431,16 @@ TEST_CASE("app: app destroyed during token refresh", "[sync][app]") {
         // Ignore websocket errors, since sometimes a websocket connection gets started during the test
         config.sync_config->error_handler = [](std::shared_ptr<SyncSession> session, SyncError error) mutable {
             // Ignore websocket errors, since there's not really an app out there...
-            if (error.message.find("Bad WebSocket")) {
-                util::format(std::cerr, "An expected possible WebSocket error was caught: '%1' for '%2'",
+            if (error.message.find("Bad WebSocket") != std::string::npos) {
+                util::format(std::cerr,
+                             "An expected possible WebSocket error was caught during test: 'app destroyed during "
+                             "token refresh': '%1' for '%2'",
                              error.message, session->path());
             }
             else {
                 util::format(std::cerr,
-                             "An unexpected sync error was caught by the default SyncTestFile handler: '%1' for '%2'",
+                             "An unexpected sync error was caught during test: 'app destroyed during token refresh': "
+                             "'%1' for '%2'",
                              error.message, session->path());
                 abort();
             }
