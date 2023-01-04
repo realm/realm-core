55beae063a657c51757b441cd6b8b33cea39c88e
Michael Wilkerson-Barker <michael.wilkersonbarker@mongodb.com>
2023-01-04 16:00:08 -0500

    Integrate default socket provider as SyncSocketProvider in sync client

--- src/realm/sync/client.cpp
+++ src/realm/sync/client.cpp
@@ -492,14 +492,14 @@ void ClientImpl::stop() noexcept
         return;
     m_stopped = true;
     m_wait_or_client_stopped_cond.notify_all();
-    m_service.stop();
+    m_socket_provider->stop();
 }
 
 
 void ClientImpl::run()
 {
     auto ta = util::make_temp_assign(m_running, true);
-    m_service.run(); // Throws
+    m_socket_provider->run();
 }
 
 
@@ -1660,16 +1660,16 @@ ClientImpl::Connection::Connection(ClientImpl& client, connection_ident_type ide
     , m_protocol_envelope{std::get<0>(endpoint)}
     , m_address{std::get<1>(endpoint)}
     , m_port{std::get<2>(endpoint)}
-    , m_verify_servers_ssl_certificate{verify_servers_ssl_certificate}
-    , m_ssl_trust_certificate_path{std::move(ssl_trust_certificate_path)}
-    , m_ssl_verify_callback{std::move(ssl_verify_callback)}
-    , m_proxy_config{std::move(proxy_config)}
+    , m_verify_servers_ssl_certificate{verify_servers_ssl_certificate}    // DEPRECATED
+    , m_ssl_trust_certificate_path{std::move(ssl_trust_certificate_path)} // DEPRECATED
+    , m_ssl_verify_callback{std::move(ssl_verify_callback)}               // DEPRECATED
+    , m_proxy_config{std::move(proxy_config)}                             // DEPRECATED
     , m_reconnect_info{reconnect_info}
     , m_sync_mode(sync_mode)
     , m_ident{ident}
     , m_server_endpoint{std::move(endpoint)}
-    , m_authorization_header_name{authorization_header_name}
-    , m_custom_http_headers{custom_http_headers}
+    , m_authorization_header_name{authorization_header_name} // DEPRECATED
+    , m_custom_http_headers{custom_http_headers}             // DEPRECATED
 {
     m_on_idle = m_client.create_trigger([this](Status status) {
         if (status == ErrorCodes::OperationAborted)
