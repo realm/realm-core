d721dd42cda08a5474f01ed130a4499b98c31f32
Jonathan Reams <jbreams@mongodb.com>
2023-01-19 15:44:38 -0500

    clang format js file

--- evergreen/add_admin_roles.js
+++ evergreen/add_admin_roles.js
@@ -1,14 +1,12 @@
-let user_doc = db.users.findOne({"data.email": "unique_user@domain.com"});
+let user_doc = db.users.findOne({"data.email" : "unique_user@domain.com"});
 if (!user_doc) {
     throw "could not find admin user!";
 }
 
-let update_res = db.users.updateOne({"_id": user_doc._id}, {
-    "$addToSet": {"roles": {"$each": [
-        { "roleName": "GLOBAL_STITCH_ADMIN" },
-        { "roleName": "GLOBAL_BAAS_FEATURE_ADMIN" }
-    ]
-}}});
+let update_res = db.users.updateOne({"_id" : user_doc._id}, {
+    "$addToSet" :
+        {"roles" : {"$each" : [ {"roleName" : "GLOBAL_STITCH_ADMIN"}, {"roleName" : "GLOBAL_BAAS_FEATURE_ADMIN"} ]}}
+});
 
 if (update_res.modifiedCount != 1) {
     throw "could not update admin user!";
