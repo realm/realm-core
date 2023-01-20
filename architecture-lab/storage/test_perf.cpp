c47f88fd060218a8b7cf6481a9af9d0e0beaa6f8
Finn Schiermer Andersen <finn.schiermer.andersen@gmail.com>
2023-01-20 08:28:45 +0100

    wip

--- architecture-lab/storage/test_perf.cpp
+++ architecture-lab/storage/test_perf.cpp
@@ -28,14 +28,22 @@
 #include <vector>
 #include <unordered_map>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
 #include "db.hpp"
 
+#define CHUNK_SIZE 20
+
 struct chunk {
-    char chars[12];
+    char chars[CHUNK_SIZE];
     int prefix_index = -1;
     bool operator==(const chunk& a2) const
     {
-        return memcmp(chars, a2.chars, 12) == 0 && prefix_index == a2.prefix_index;
+        return memcmp(chars, a2.chars, CHUNK_SIZE) == 0 && prefix_index == a2.prefix_index;
     };
 };
 
@@ -44,17 +52,29 @@ struct std::hash<chunk> {
     std::size_t operator()(chunk const& c) const noexcept
     {
         std::size_t ret = c.chars[0] << 56;
-        ret |= (unsigned long)c.chars[1] << 48;
-        ret |= (unsigned long)c.chars[2] << 40;
-        ret |= (unsigned long)c.chars[3] << 32;
-        ret |= (unsigned long)c.chars[4] << 24;
-        ret |= (unsigned long)c.chars[5] << 16;
-        ret |= (unsigned long)c.chars[6] << 8;
-        ret |= (unsigned long)c.chars[7];
+        ret ^= (unsigned long)c.chars[1] << 48;
+        ret ^= (unsigned long)c.chars[2] << 40;
+        ret ^= (unsigned long)c.chars[3] << 32;
+#if (CHUNK_SIZE >= 12)
+        ret ^= (unsigned long)c.chars[4] << 24;
+        ret ^= (unsigned long)c.chars[5] << 16;
+        ret ^= (unsigned long)c.chars[6] << 8;
+        ret ^= (unsigned long)c.chars[7];
         ret ^= (unsigned long)c.chars[8] << 56;
         ret ^= (unsigned long)c.chars[9] << 48;
         ret ^= (unsigned long)c.chars[10] << 40;
         ret ^= (unsigned long)c.chars[11] << 32;
+#endif
+#if (CHUNK_SIZE >= 20)
+        ret ^= (unsigned long)c.chars[12] << 24;
+        ret ^= (unsigned long)c.chars[13] << 16;
+        ret ^= (unsigned long)c.chars[14] << 8;
+        ret ^= (unsigned long)c.chars[15];
+        ret ^= (unsigned long)c.chars[16] << 56;
+        ret ^= (unsigned long)c.chars[17] << 48;
+        ret ^= (unsigned long)c.chars[18] << 40;
+        ret ^= (unsigned long)c.chars[19] << 32;
+#endif
         ret ^= (unsigned long)c.prefix_index;
         // std::cout << " *" << ret << "* " << std::flush;
         return ret;
@@ -64,19 +84,19 @@ struct std::hash<chunk> {
 struct string_compressor {
     std::vector<chunk> chunks;
     std::unordered_map<chunk, int> map;
-    int handle(std::string tmp)
+    int handle(const char* first, const char* past)
     {
-        total_chars += tmp.size();
-        int first = 0;
-        int last = 12;
+        int size = past - first;
+        total_chars += size;
+        const char* last = first + CHUNK_SIZE;
         int prefix = -1;
         chunk c;
-        while (first < tmp.size()) {
-            if (last >= tmp.size()) {
-                last = tmp.size();
-                memset(c.chars, 0, 12);
+        while (first < past) {
+            if (last >= past) {
+                last = past;
+                memset(c.chars, 0, CHUNK_SIZE);
             }
-            memcpy(c.chars, tmp.data() + first, last - first);
+            memcpy(c.chars, first, last - first);
             c.prefix_index = prefix;
             auto it = map.find(c);
             if (it == map.end()) {
@@ -87,8 +107,8 @@ struct string_compressor {
             else {
                 prefix = it->second;
             }
-            first += 12;
-            last += 12;
+            first += CHUNK_SIZE;
+            last += CHUNK_SIZE;
         }
         // std::cout << "   " << tmp << " -> " << prefix << std::endl;
         return prefix;
@@ -96,6 +116,24 @@ struct string_compressor {
     int next_id = 0;
     int next_prefix_id = -1;
     int64_t total_chars = 0;
+};
+
+struct results {
+    long* values;
+    long first_line;
+    long num_lines;
+    long num_fields;
+    results(long first_line, long num_lines, long num_fields)
+        : first_line(first_line)
+        , num_lines(num_lines)
+        , num_fields(num_fields)
+    {
+        values = new long[num_lines * num_fields];
+    }
+    ~results()
+    {
+        delete values;
+    }
 };
 
 int main(int argc, char* argv[])
@@ -150,37 +188,43 @@ int main(int argc, char* argv[])
         else
             f_s[j] = ss.get_field<String>(t, j);
     }
-    long total_lines;
-    {
-        std::cout << "Creating default objects....\n" << std::flush;
-        std::ifstream infile(argv[1]);
-        std::string line;
-        long num_line = 0;
+    int fd = open(argv[1], O_RDONLY);
+    assert(fd >= 0);
+    off_t size = lseek(fd, 0, SEEK_END);
+    void* file_start = mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    assert(file_start != (void*)-1);
+
+    long total_lines = 0;
+    std::thread writer([&]() {
+        std::cout << "Initial scan / object creation" << std::endl;
         start = std::chrono::high_resolution_clock::now();
-        while (std::getline(infile, line)) {
-            if ((num_line % 100000) == 0)
+        const char* line_start = (const char*)file_start;
+        while (line_start < file_start + size) {
+            while (*line_start++ != '\n')
+                ;
+            if ((total_lines % 100000) == 0)
                 std::cout << "." << std::flush;
-            if ((num_line % 1000000) == 0)
-                std::cout << num_line << std::endl;
-            ss.insert(t, {num_line << 1});
-            if (!ss.exists(t, {num_line << 1})) {
-                std::cout << "Weird - " << num_line << " is missing" << std::endl;
-            }
-            auto o = ss.get(t, {num_line << 1});
-            ++num_line;
+            if ((total_lines % 1000000) == 0)
+                std::cout << total_lines << std::endl;
+            ss.insert(t, {total_lines << 1});
+            // if (!ss.exists(t, {total_lines << 1})) {
+            //     std::cout << "Weird - " << total_lines << " is missing" << std::endl;
+            // }
+            // auto o = ss.get(t, {total_lines << 1});
+            total_lines++;
         }
-        total_lines = num_line;
         end = std::chrono::high_resolution_clock::now();
-        std::chrono::nanoseconds ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start) / limit;
-        std::cout << "   ...done in " << ns.count() << " nsecs/key" << std::endl;
+        std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        std::cout << "   ...done in " << ms.count() << " millisecs" << std::endl;
         ss.print_stat(std::cout);
         std::cout << "Committing to stable storage" << std::flush;
         start = std::chrono::high_resolution_clock::now();
         db.commit(std::move(ss));
         end = std::chrono::high_resolution_clock::now();
-        std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
         std::cout << "   ...done in " << ms.count() << " msecs" << std::endl << std::endl;
-    }
+    });
+    writer.join();
     // now populate dataset
     {
         std::cout << "Optimizing access order..." << std::endl;
@@ -194,69 +238,92 @@ int main(int argc, char* argv[])
             db.release(std::move(s3));
         }
         std::cout << std::endl << "Ingesting data (" << total_lines << " objects).... " << std::endl;
-        std::ifstream infile(argv[1]);
-        std::string line;
         long num_line = 0;
         std::vector<std::unique_ptr<string_compressor>> compressors;
         compressors.resize(105);
-        while (num_line < total_lines) {
-            long limit = std::min(total_lines, num_line + 5000000);
+        const char* read_ptr = (const char*)file_start;
+        while (read_ptr < file_start + size) {
+            long limit = std::min(total_lines, num_line + 1000000);
             start = std::chrono::high_resolution_clock::now();
-            const Snapshot& s3 = db.open_snapshot();
-            db.release(std::move(s3));
-            Snapshot& s2 = db.create_changes(); // create_changes();
+            results* res = new results(num_line, limit - num_line, max_fields);
             while (num_line < limit) {
-                if (!std::getline(infile, line)) {
-                    std::cout << "Kludder i den!" << std::endl;
-                    exit(-1);
-                }
                 long num_value = 0;
                 if ((num_line % 100000) == 0)
                     std::cout << num_line << " " << std::flush;
-                auto row = row_order[num_line];
                 // std::cout << "Index " << num_line << " - key " << row.key << std::endl;
-                if (!s2.exists(t, row)) {
-                    std::cout << "Weird - " << num_line << " with key " << row.key << " is missing" << std::endl;
-                }
-                auto o = s2.get(t, row);
-                std::istringstream iss(line);
+                long* line_results = res->values + (num_line - res->first_line) * max_fields;
                 ++num_line;
-                std::string tmp;
-                while (std::getline(iss, tmp, '\t')) {
-                    // std::cout << num_line << " : " << num_value << " : " << tmp << std::endl;
+                while (num_value < max_fields) {
+                    const char* read_ptr2 = read_ptr;
+                    while (*read_ptr2 != '\t' && *read_ptr2 != 0 && *read_ptr2 != '\n')
+                        ++read_ptr2;
                     if (compressible[num_value] == 's') {
                         if (!compressors[num_value]) {
                             compressors[num_value] = std::make_unique<string_compressor>();
                         }
                         auto& compressor = *compressors[num_value];
-                        long compressed = compressor.handle(tmp);
-                        o.set(f_i[num_value], compressed);
+                        long compressed = compressor.handle(read_ptr, read_ptr2);
+                        line_results[num_value] = compressed;
                     }
-                    else if (tmp.empty())
-                        o.set(f_i[num_value], 0L);
+                    else if (read_ptr == read_ptr2)
+                        line_results[num_value] = 0;
                     else
-                        o.set(f_i[num_value], stol(tmp));
+                        line_results[num_value] = atol(read_ptr);
+
                     ++num_value;
+                    read_ptr = read_ptr2 + 1;
                 }
             }
-            // db.release(std::move(s3));
-            end = std::chrono::high_resolution_clock::now();
-            std::chrono::nanoseconds ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start) / limit;
-            std::cout << "   ...done in " << ns.count() << " nsecs/key" << std::endl;
-            s2.print_stat(std::cout);
-            std::cout << "Committing to stable storage" << std::flush;
-            start = std::chrono::high_resolution_clock::now();
-            db.commit(std::move(s2));
-            end = std::chrono::high_resolution_clock::now();
-            std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-            std::cout << "   ...done in " << ms.count() << " msecs" << std::endl << std::endl;
+            {
+                const Snapshot& s3 = db.open_snapshot();
+                db.release(std::move(s3));
+                Snapshot& s2 = db.create_changes(); // create_changes();
+                auto num_line = res->first_line;
+                auto num_value = 0;
+                auto val_ptr = res->values;
+                while (num_line < res->num_lines) {
+                    Object o;
+                    if (num_value == 0) {
+                        auto row = row_order[num_line];
+                        if (!s2.exists(t, row)) {
+                            std::cout << "Weird - " << num_line << " with key " << row.key << " is missing"
+                                      << std::endl;
+                        }
+                        o = s2.get(t, row);
+                    }
+                    auto val = *val_ptr++;
+                    o.set(f_i[num_value++], val);
+                    if (num_value == res->num_fields) {
+                        num_value = 0;
+                        num_line++;
+                    }
+                }
+                delete res;
+                end = std::chrono::high_resolution_clock::now();
+                std::chrono::nanoseconds ns =
+                    std::chrono::duration_cast<std::chrono::nanoseconds>(end - start) / limit;
+                std::cout << "   ...done in " << ns.count() << " nsecs/key" << std::endl;
+                s2.print_stat(std::cout);
+                std::cout << "Committing to stable storage" << std::flush;
+                start = std::chrono::high_resolution_clock::now();
+                db.commit(std::move(s2));
+                end = std::chrono::high_resolution_clock::now();
+                std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+                std::cout << "   ...done in " << ms.count() << " msecs" << std::endl << std::endl;
+            }
         }
+        uint64_t from_size = 0;
+        uint64_t to_size = 0;
         for (int i = 0; i < max_fields; ++i) {
             if (compressors[i]) {
+                from_size += compressors[i]->total_chars;
+                to_size += compressors[i]->map.size() * sizeof(chunk);
                 std::cout << "Field " << i << " with " << compressors[i]->map.size() << " chunks ("
                           << compressors[i]->map.size() * sizeof(chunk) << " bytes) from total "
                           << compressors[i]->total_chars << " chars" << std::endl;
             }
         }
+        std::cout << "Total effect: from " << from_size << " to " << to_size << " bytes ("
+                  << 100 - (to_size * 100) / from_size << " pct reduction)" << std::endl;
     }
 }
