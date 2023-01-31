714677b2b908bbe39ed9fecfe9896a91a5542b56
Jørgen Edelbo <jorgen.edelbo@mongodb.com>
2023-01-31 12:54:01 +0100

    Fix compile warnings

--- test/fuzzy/libfuzzer_entry.cpp
+++ test/fuzzy/libfuzzer_entry.cpp
@@ -6,8 +6,10 @@
 using namespace realm;
 using namespace realm::util;
 
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size);
+
 // This function is the entry point for libfuzzer, main is auto-generated
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
+int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
 {
     if (Size == 0) {
         return 0;
@@ -15,8 +17,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
     realm::test_util::RealmPathInfo test_context{"libfuzzer_test"};
     SHARED_GROUP_TEST_PATH(path);
     disable_sync_to_disk();
-    util::Optional<std::ostream&> log; // logging off
     std::string contents(reinterpret_cast<const char*>(Data), Size);
-    parse_and_apply_instructions(contents, path, log);
+    parse_and_apply_instructions(contents, path, nullptr);
     return 0; // Non-zero return values are reserved for future use.
 }
--- test/realm-fuzzer/libfuzzer_runner.cpp
+++ test/realm-fuzzer/libfuzzer_runner.cpp
@@ -19,7 +19,9 @@
 #include <exception>
 
 // This function is the entry point for libfuzzer, main is auto-generated
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size);
+
+int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
 {
     if (Size == 0)
         return 0;
