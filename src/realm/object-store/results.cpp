7f261bcac056beb5257cb5c0d64fb663d262a467
Nicola Cabiddu <nicola.cabiddu@mongodb.com>
2023-03-28 19:02:52 +0100

    git format

--- src/realm/object-store/results.cpp
+++ src/realm/object-store/results.cpp
@@ -1080,9 +1080,9 @@ Results Results::import_copy_into_realm(std::shared_ptr<Realm> const& realm)
     util::CheckedUniqueLock lock(m_mutex);
     if (m_mode == Mode::Empty)
         return *this;
-    if (!is_valid()) 
+    if (!is_valid())
         throw StaleAccessor("Attempt to construct a Results for a deleted object");
-    
+
     switch (m_mode) {
         case Mode::Table:
             return Results(realm, realm->import_copy_of(m_table));
