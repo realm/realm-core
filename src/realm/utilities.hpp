bd5267ac28f37efe787b108bba031f1d5484476b
James Stone <james.stone@mongodb.com>
2023-03-29 11:03:11 -0700

    fix clang-format complaint

--- src/realm/utilities.hpp
+++ src/realm/utilities.hpp
@@ -193,7 +193,7 @@ inline int ctz(size_t x)
 #ifdef REALM_PTR_64
     return __builtin_ctzll(x); // returns int
 #else
-    return __builtin_ctz(x); // returns int
+    return __builtin_ctz(x);      // returns int
 #endif
 #elif defined(_WIN32)
     unsigned long index = 0;
