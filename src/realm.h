33f017a1eb7ecc1dd3ebd54c8f1a381ea4f3eeda
Desislava Stefanova <dst.stefanova@gmail.com>
2023-05-03 17:09:51 +0300

    Fixed comment

--- src/realm.h
+++ src/realm.h
@@ -3180,9 +3180,9 @@ RLM_API bool realm_app_push_notification_client_deregister_device(const realm_ap
  * Run a named MongoDB Realm function.
  *
  * @param serialized_ejson_args The arguments array to invoke the function with,
- *                              serialized as an Extended JSON string.
- * @param service_name The name of the remote service whose system function to call. 
- *                              The service_name should be null when calling a user function.
+ *                        serialized as an Extended JSON string.
+ * @param service_name The name of the remote service whose system function to call. Can be null,
+ *                        in which case the called function is expected to be a user function.
  * @return true, if no error occurred.
  */
 RLM_API bool realm_app_call_function(const realm_app_t*, const realm_user_t*, const char* function_name,
