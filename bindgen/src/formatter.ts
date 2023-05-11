e9ffb678f0844c58fbdaffacde6800538935750b
Kræn Hansen <kraen.hansen@mongodb.com>
2023-05-11 10:59:42 +0200

    Adding ability to provide a callback as formatter

--- bindgen/src/formatter.ts
+++ bindgen/src/formatter.ts
@@ -22,17 +22,42 @@ import cp from "child_process";
 import { extend } from "./debug";
 const debug = extend("format");
 
-const FORMATTERS: Record<string, string[]> = {
-  eslint: ["npx", "eslint", "--fix", "--format=stylish"],
-  "clang-format": ["npx", "clang-format", "-i"],
+type Formatter = (cwd: string, filePaths: string[]) => void;
+
+export function executeCommand(cwd: string, command: string, ...args: string[]) {
+  console.log(chalk.dim(command, ...args));
+  const result = cp.spawnSync(command, args, {
+    cwd,
+    encoding: "utf8",
+    stdio: "inherit",
+    shell: true,
+  });
+  if (result.error) throw result.error;
+  if (result.status) {
+    throw new Error(`Exited with status ${result.status}`)
+  }
+}
+
+function createCommandFormatter(commandAndArgs: string[]): Formatter {
+  return (cwd, filePaths) => {
+    const [command, ...args] = [...commandAndArgs, ...filePaths];
+    executeCommand(cwd, command, ...args);
+  };
+}
+
+const FORMATTERS: Record<string, Formatter> = {
+  eslint: createCommandFormatter(["npx", "eslint", "--fix", "--format=stylish"]),
+  "clang-format": createCommandFormatter(["npx", "clang-format", "-i"]),
   // "typescript-checker": ["npx", "tsc", "--noEmit"],
 };
 
-export function addFormatter(name: string, commandAndArgs: string[]) {
+export function addFormatter(name: string, callbackOrCommandAndArgs: Formatter | string[]) {
   if (name in FORMATTERS) {
     throw new Error(`Cannot add ${name} as it's already there`);
+  } else if (Array.isArray(callbackOrCommandAndArgs)) {
+    FORMATTERS[name] = createCommandFormatter(callbackOrCommandAndArgs);
   } else {
-    FORMATTERS[name] = commandAndArgs;
+    FORMATTERS[name] = callbackOrCommandAndArgs;
   }
 }
 
@@ -43,8 +68,8 @@ export function getFormatterNames() {
 export type FormatterName = string;
 
 export class FormatError extends Error {
-  constructor(formatterName: string, filePaths: string[]) {
-    super(`Failure when running the '${formatterName}' formatter on ${JSON.stringify(filePaths)}`);
+  constructor(formatterName: string, filePaths: string[], cause: Error) {
+    super(`Failure when running the '${formatterName}' formatter on ${JSON.stringify(filePaths)}: ${cause.message}`);
   }
 }
 
@@ -55,17 +80,11 @@ export function format(formatterName: FormatterName, cwd: string, filePaths: str
   } else {
     debug(chalk.dim("Running formatter '%s' on %d files"), formatterName, filePaths.length);
     if (formatterName in FORMATTERS) {
-      const [command, ...args] = FORMATTERS[formatterName].concat(filePaths);
-      console.log(`Running '${formatterName}' formatter`, chalk.dim(command, ...args));
-      const result = cp.spawnSync(command, args, {
-        cwd,
-        encoding: "utf8",
-        stdio: "inherit",
-        shell: true,
-      });
-      if (result.error) throw result.error;
-      if (result.status) {
-        throw new FormatError(formatterName, filePaths);
+      console.log(`Running '${formatterName}' formatter`);
+      try {
+        FORMATTERS[formatterName](cwd, filePaths);
+      } catch (err) {
+        throw new FormatError(formatterName, filePaths, err instanceof Error ? err : new Error(String(err)));
       }
     }
   }
