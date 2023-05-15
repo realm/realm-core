6ad6055c03c02eecc44eeff85cb7f569ebc22da9
Nicola Cabiddu <nicola.cabiddu@mongodb.com>
2023-05-15 16:59:53 +0100

    format fixes

--- test/object-store/c_api/c_api.cpp
+++ test/object-store/c_api/c_api.cpp
@@ -4909,16 +4909,17 @@ TEST_CASE("C API: nested collections", "[c_api]") {
         REQUIRE(value.type == RLM_TYPE_DICTIONARY);
         auto dict = cptr_checked(realm_get_dictionary(obj1.get(), foo_any_col_key));
         checked(realm_dictionary_insert(dict.get(), rlm_str_val("Hello"), rlm_str_val("world"), nullptr, nullptr));
-        //dict -> list
+        // dict -> list
         realm_dictionary_insert_collection(dict.get(), rlm_str_val("Godbye"), RLM_COLLECTION_TYPE_LIST);
         auto list = cptr_checked(realm_dictionary_get_list(dict.get(), rlm_str_val("Godbye")));
         realm_list_insert(list.get(), 0, rlm_str_val("Hello"));
         realm_list_insert(list.get(), 0, rlm_str_val("42"));
         realm_list_insert(list.get(), 0, rlm_int_val(42));
-        //dict -> dict
+        // dict -> dict
         realm_dictionary_insert_collection(dict.get(), rlm_str_val("Hi"), RLM_COLLECTION_TYPE_DICTIONARY);
         auto dict2 = cptr_checked(realm_dictionary_get_dictionary(dict.get(), rlm_str_val("Hi")));
-        checked(realm_dictionary_insert(dict2.get(), rlm_str_val("Nested-Hello"), rlm_str_val("Nested-World"), nullptr, nullptr));
+        checked(realm_dictionary_insert(dict2.get(), rlm_str_val("Nested-Hello"), rlm_str_val("Nested-World"),
+                                        nullptr, nullptr));
         size_t size;
         checked(realm_dictionary_size(dict.get(), &size));
         REQUIRE(size == 3);
@@ -4932,11 +4933,11 @@ TEST_CASE("C API: nested collections", "[c_api]") {
         auto list = cptr_checked(realm_get_list(obj1.get(), foo_any_col_key));
         realm_list_insert(list.get(), 0, rlm_str_val("Hello"));
         realm_list_insert(list.get(), 1, rlm_str_val("World"));
-        //list -> dict
+        // list -> dict
         realm_list_insert_collection(list.get(), 1, RLM_COLLECTION_TYPE_DICTIONARY);
         auto dict = cptr_checked(realm_list_get_dictionary(list.get(), 1));
         checked(realm_dictionary_insert(dict.get(), rlm_str_val("Hello"), rlm_str_val("world"), nullptr, nullptr));
-        //list -> list
+        // list -> list
         realm_list_insert_collection(list.get(), 2, RLM_COLLECTION_TYPE_LIST);
         auto list2 = cptr_checked(realm_list_get_list(list.get(), 2));
         realm_list_insert(list2.get(), 0, rlm_str_val("Nested-Hello"));
