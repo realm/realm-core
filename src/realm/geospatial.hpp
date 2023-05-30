ea21ed388ccbe4b0bc8c4dc90fc6edf7e405dba3
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2023-05-30 10:30:43 +0200

    Added default constructor for GeoPoint

--- src/realm/geospatial.hpp
+++ src/realm/geospatial.hpp
@@ -39,7 +39,7 @@ class TableRef;
 class Geospatial;
 
 struct GeoPoint {
-    GeoPoint() = delete;
+    GeoPoint() {}
     GeoPoint(double lon, double lat)
         : longitude(lon)
         , latitude(lat)
