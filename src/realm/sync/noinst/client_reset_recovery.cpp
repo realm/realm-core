36e24efc23b0053cdf4b4f74351f791db9cf039b
James Stone <james.stone@mongodb.com>
2023-06-19 15:06:13 -0700

    lint

--- src/realm/sync/noinst/client_reset_recovery.cpp
+++ src/realm/sync/noinst/client_reset_recovery.cpp
@@ -719,9 +719,9 @@ void RecoverLocalChangesetsHandler::operator()(const Instruction::AddTable& inst
             else {
                 ColKey pk_col = table->get_primary_key_column();
                 REALM_ASSERT_EX(pk_col, class_name);
-                m_replication->add_class_with_primary_key(table->get_key(), table->get_name(), DataType(pk_col.get_type()),
-                                                          table->get_column_name(pk_col), pk_col.is_nullable(),
-                                                          table->get_table_type());
+                m_replication->add_class_with_primary_key(table->get_key(), table->get_name(),
+                                                          DataType(pk_col.get_type()), table->get_column_name(pk_col),
+                                                          pk_col.is_nullable(), table->get_table_type());
             }
         }
 
