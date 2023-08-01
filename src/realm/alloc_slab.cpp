2065a51af67c253a2e7b3c8bdf05ae703726e4e0
Finn Schiermer Andersen <finn.schiermer.andersen@gmail.com>
2023-08-01 15:33:54 +0200

    check for races between mmap changes and alloc/free

--- src/realm/alloc_slab.cpp
+++ src/realm/alloc_slab.cpp
@@ -558,7 +558,7 @@ void SlabAlloc::do_free(ref_type ref, char* addr)
                 // We can do that just by adding the size
                 if (prev->first + prev->second == ref) {
                     prev->second += size;
-                    return; // Done!
+                    return;                                     // Done!
                 }
                 m_free_read_only.emplace_hint(next, ref, size); // Throws
             }
@@ -867,7 +867,7 @@ ref_type SlabAlloc::attach_file(const std::string& path, Config& cfg, util::Writ
 
         top_ref = validate_header(header, footer, size, path, cfg.encryption_key != nullptr); // Throws
         m_attach_mode = cfg.is_shared ? attach_SharedFile : attach_UnsharedFile;
-        m_data = map_header.get_addr(); // <-- needed below
+        m_data = map_header.get_addr();                                                       // <-- needed below
 
         if (cfg.session_initiator && is_file_on_streaming_form(*header)) {
             // Don't compare file format version fields as they are allowed to differ.
@@ -1211,6 +1211,8 @@ void SlabAlloc::update_reader_view(size_t file_size)
         schedule_refresh_of_outdated_encrypted_pages();
         return;
     }
+
+    CriticalSection cs(changes);
 
     const auto old_slab_base = align_size_to_section_boundary(old_baseline);
     bool replace_last_mapping = false;
