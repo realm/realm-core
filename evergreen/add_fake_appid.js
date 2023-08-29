e94223ab30620da5f2d9cfa86bc8b5f3d781705b
Michael Wilkerson-Barker <michael.wilkersonbarker@mongodb.com>
2023-08-29 10:10:57 -0400

    Updated comment in add_fake_appid.js

--- evergreen/add_fake_appid.js
+++ evergreen/add_fake_appid.js
@@ -1,5 +1,8 @@
-// Add a version to the schema change history store so that the drop optimization does not take place
-// This caused issues with this test failing once app deletions starting being done asynchronously
+// Add a fake version to the schema change history store so that the drop optimization does not run.
+// Drop optimization runs asynchronously to drop the schema change history store after PBS sync is
+// terminated or a PBS app is deleted and there is only one PBS app present at the time of sync
+// termination or app deletion.
+// This caused issues with CI tests failing once app deletions starting being done asynchronously
 let dummy_app = db.schema_change_history.insertOne({"_id" : new ObjectId()});
 if (!dummy_app) {
     throw "Could not insert a dummy app!";
