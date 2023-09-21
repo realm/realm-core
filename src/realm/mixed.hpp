b48268d60a5d5d6feaa655a119076ab1306e1303
Jørgen Edelbo <jorgen.edelbo@mongodb.com>
2023-09-21 15:28:13 +0200

    Avoid string index upgrade

--- src/realm/mixed.hpp
+++ src/realm/mixed.hpp
@@ -230,6 +230,8 @@ public:
     bool is_same_type(const Mixed& b) const noexcept;
     // Will use unsigned lexicographical comparison for strings
     int compare(const Mixed& b) const noexcept;
+    // Will compare strings as arrays of signed chars
+    int compare_signed(const Mixed& b) const noexcept;
     friend bool operator==(const Mixed& a, const Mixed& b) noexcept
     {
         return a.compare(b) == 0;
