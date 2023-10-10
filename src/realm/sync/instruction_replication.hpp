396f21ff2ebbeb157e0e56feff2d2eee8a334050
Nicola Cabiddu <nicola.cabiddu@mongodb.com>
2023-10-10 11:11:56 +0100

    disable client reset tests as long as sync part is not ready

--- src/realm/sync/instruction_replication.hpp
+++ src/realm/sync/instruction_replication.hpp
@@ -198,7 +198,7 @@ private:
     bool m_was_short_circuited;
 };
 
-constexpr bool SYNC_SUPPORTS_NESTED_COLLECTIONS = true;
+constexpr bool SYNC_SUPPORTS_NESTED_COLLECTIONS = false;
 
 } // namespace sync
 } // namespace realm
