a1520562d91dd57d87e023b7658b573edd8d936e
Nicola Cabiddu <nicola.cabiddu@mongodb.com>
2023-12-13 16:44:28 +0000

    restore test_table in full

--- test/test_table.cpp
+++ test/test_table.cpp
@@ -3748,7 +3748,7 @@ NONCONCURRENT_TEST(Table_object_random)
 
 
         auto t2 = steady_clock::now();
-        std::cout << "   insertion time: " << duration_cast<nanoseconds>(t2 - t1).count() / nb_rows << "ns/key"
+        std::cout << "   insertion time: " << duration_cast<nanoseconds>(t2 - t1).count() / nb_rows << " ns/key"
                   << std::endl;
 
         CHECK_EQUAL(table->size(), nb_rows);
@@ -3831,7 +3831,7 @@ NONCONCURRENT_TEST(Table_object_random)
 
         auto t2 = steady_clock::now();
 
-        std::cout << "   update time   : " << duration_cast<nanoseconds>(t2 - t1).count() / nb_rows << "ns/key"
+        std::cout << "   update time   : " << duration_cast<nanoseconds>(t2 - t1).count() / nb_rows << " ns/key"
                   << std::endl;
         wt.commit();
     }
@@ -3854,7 +3854,7 @@ NONCONCURRENT_TEST(Table_object_random)
 #endif
         }
         auto t2 = steady_clock::now();
-        std::cout << "   erase time    : " << duration_cast<nanoseconds>(t2 - t1).count() / nb_rows << "ns/key"
+        std::cout << "   erase time    : " << duration_cast<nanoseconds>(t2 - t1).count() / nb_rows << " ns/key"
                   << std::endl;
 
         wt.commit();
@@ -4860,7 +4860,7 @@ TEST(List_Ops)
     test_lists<Optional<bool>>(test_context, sg, type_Bool, true);
     test_lists<Optional<float>>(test_context, sg, type_Float, true);
     test_lists<Optional<double>>(test_context, sg, type_Double, true);
-    // test_lists<Timestamp>(test_context, sg, type_Timestamp, true); // always Optional?
+    test_lists<Timestamp>(test_context, sg, type_Timestamp, true); // always Optional?
     test_lists<Decimal128>(test_context, sg, type_Decimal, true);
     test_lists<Optional<ObjectId>>(test_context, sg, type_ObjectId, true);
     test_lists<Optional<UUID>>(test_context, sg, type_UUID, true);
@@ -5218,7 +5218,7 @@ TEST(Table_Column_DynamicConversions)
     test_dynamic_conversion_combi_sametype<Decimal128>(test_context, sg, type_Decimal);
     test_dynamic_conversion_combi_sametype<UUID>(test_context, sg, type_UUID);
     // lists...:
-    // test_dynamic_conversion_list_combi<int64_t>(test_context, sg, type_Int);
+    test_dynamic_conversion_list_combi<int64_t>(test_context, sg, type_Int);
     test_dynamic_conversion_list_combi<float>(test_context, sg, type_Float);
     test_dynamic_conversion_list_combi<double>(test_context, sg, type_Double);
     test_dynamic_conversion_list_combi<bool>(test_context, sg, type_Bool);
@@ -5226,37 +5226,38 @@ TEST(Table_Column_DynamicConversions)
 
     test_dynamic_conversion_list_combi_sametype<StringData>(test_context, sg, type_String);
     test_dynamic_conversion_list_combi_sametype<BinaryData>(test_context, sg, type_Binary);
-    // test_dynamic_conversion_list_combi_sametype<Timestamp>(test_context, sg, type_Timestamp);
+    test_dynamic_conversion_list_combi_sametype<Timestamp>(test_context, sg, type_Timestamp);
     test_dynamic_conversion_list_combi_sametype<Decimal128>(test_context, sg, type_Decimal);
     test_dynamic_conversion_list_combi_sametype<UUID>(test_context, sg, type_UUID);
 }
 
+/*
+TEST(Table_Column_Conversions)
+{
+    SHARED_GROUP_TEST_PATH(path);
 
-// TEST(Table_Column_Conversions)
-//{
-//     SHARED_GROUP_TEST_PATH(path);
-//
-//     std::unique_ptr<Replication> hist(make_in_realm_history());
-//     DBRef sg = DB::create(*hist, path, DBOptions(crypt_key()));
-//
-//     test_column_conversion<int64_t, Optional<int64_t>>(test_context, sg, type_Int);
-//     test_column_conversion<float, Optional<float>>(test_context, sg, type_Float);
-//     test_column_conversion<double, Optional<double>>(test_context, sg, type_Double);
-//     test_column_conversion<bool, Optional<bool>>(test_context, sg, type_Bool);
-//     test_column_conversion<StringData, StringData>(test_context, sg, type_String);
-//     test_column_conversion<BinaryData, BinaryData>(test_context, sg, type_Binary);
-//     test_column_conversion<Timestamp, Timestamp>(test_context, sg, type_Timestamp);
-//
-//     test_column_conversion_optional<int64_t>(test_context, sg, type_Int);
-//     test_column_conversion_optional<float>(test_context, sg, type_Float);
-//     test_column_conversion_optional<double>(test_context, sg, type_Double);
-//     test_column_conversion_optional<bool>(test_context, sg, type_Bool);
-//
-//     test_column_conversion_sametype<StringData>(test_context, sg, type_String);
-//     test_column_conversion_sametype<BinaryData>(test_context, sg, type_Binary);
-//     test_column_conversion_sametype<Timestamp>(test_context, sg, type_Timestamp);
-// }
+    std::unique_ptr<Replication> hist(make_in_realm_history());
+    DBRef sg = DB::create(*hist, path, DBOptions(crypt_key()));
 
+    test_column_conversion<int64_t, Optional<int64_t>>(test_context, sg, type_Int);
+    test_column_conversion<float, Optional<float>>(test_context, sg, type_Float);
+    test_column_conversion<double, Optional<double>>(test_context, sg, type_Double);
+    test_column_conversion<bool, Optional<bool>>(test_context, sg, type_Bool);
+    test_column_conversion<StringData, StringData>(test_context, sg, type_String);
+    test_column_conversion<BinaryData, BinaryData>(test_context, sg, type_Binary);
+    test_column_conversion<Timestamp, Timestamp>(test_context, sg, type_Timestamp);
+
+    test_column_conversion_optional<int64_t>(test_context, sg, type_Int);
+    test_column_conversion_optional<float>(test_context, sg, type_Float);
+    test_column_conversion_optional<double>(test_context, sg, type_Double);
+    test_column_conversion_optional<bool>(test_context, sg, type_Bool);
+
+    test_column_conversion_sametype<StringData>(test_context, sg, type_String);
+    test_column_conversion_sametype<BinaryData>(test_context, sg, type_Binary);
+    test_column_conversion_sametype<Timestamp>(test_context, sg, type_Timestamp);
+
+}
+*/
 
 TEST(Table_ChangePKNullability)
 {
