9df7df8982ff085d18ca8a81df8549d1e04c31cd
Kirill Burtsev <kirill.burtsev@mongodb.com>
2024-01-31 21:32:22 +0100

    Add object-store based test for progress notifications

--- test/object-store/sync/session/progress_notifications.cpp
+++ test/object-store/sync/session/progress_notifications.cpp
@@ -20,12 +20,24 @@
 
 #include <realm/util/scope_exit.hpp>
 
+#if REALM_ENABLE_AUTH_TESTS
+#include "util/test_file.hpp"
+#include "util/sync/flx_sync_harness.hpp"
+#include "util/sync/sync_test_utils.hpp"
+
+#include <realm/object-store/impl/object_accessor_impl.hpp>
+#include <realm/object-store/sync/async_open_task.hpp>
+
+using namespace realm::app;
+#endif
+
 #include <catch2/catch_all.hpp>
 
 using namespace realm;
 
+using NotifierType = SyncSession::ProgressDirection;
+
 TEST_CASE("progress notification", "[sync][session][progress]") {
-    using NotifierType = SyncSession::ProgressDirection;
     _impl::SyncProgressNotifier progress;
     uint64_t transferred = 0;
     uint64_t transferrable = 0;
@@ -603,3 +615,279 @@ TEST_CASE("progress notification", "[sync][session][progress]") {
         }
     }
 }
+
+#if REALM_ENABLE_AUTH_TESTS
+
+struct TestSetup {
+    TableRef get_table(const SharedRealm& r)
+    {
+        return r->read_group().get_table("class_" + table_name);
+    }
+
+    size_t add_objects(SharedRealm& r, int num = 5)
+    {
+        CppContext ctx(r);
+        for (int i = 0; i < num; ++i) {
+            // use specifically separate transactions for a bit of history
+            r->begin_transaction();
+            Object::create(ctx, r, StringData(table_name), std::any(make_one(i)));
+            r->commit_transaction();
+        }
+        return get_table(r)->size();
+    }
+
+    virtual SyncTestFile make_config() = 0;
+    virtual AnyDict make_one(int64_t idx) = 0;
+
+    std::string table_name;
+};
+
+struct PBS : TestSetup {
+    PBS()
+    {
+        table_name = "Dog";
+    }
+
+    SyncTestFile make_config() override
+    {
+        const auto schema = get_default_schema();
+        return SyncTestFile(session.app(), partition, schema);
+    }
+
+    AnyDict make_one(int64_t /* idx */) override
+    {
+        return AnyDict{
+            {"_id", std::any(ObjectId::gen())}, {"breed", std::string("bulldog")}, {"name", random_string(1024 * 1024)}};
+    }
+
+    TestAppSession session;
+    const std::string partition = random_string(100);
+};
+
+struct FLX : TestSetup {
+    FLX(const std::string& app_id = "flx_sync_progress")
+        : harness(app_id)
+    {
+        table_name = (*harness.schema().begin()).name;
+        harness.do_with_new_user([](std::shared_ptr<SyncUser>&&) {});
+    }
+
+    SyncTestFile make_config() override
+    {
+        auto config = harness.make_test_file();
+        config.sync_config->rerun_init_subscription_on_open = true;
+        config.sync_config->subscription_initializer = [&](SharedRealm&& realm) {
+            auto sub = realm->get_latest_subscription_set().make_mutable_copy();
+            sub.insert_or_assign(Query(get_table(realm)));
+            sub.commit();
+        };
+        return config;
+    }
+
+    AnyDict make_one(int64_t idx) override
+    {
+        return AnyDict{
+            {"_id", ObjectId::gen()},
+            {"queryable_int_field", idx},
+            {"queryable_str_field", random_string(1024 * 1024)},
+        };
+    }
+
+    FLXSyncTestHarness harness;
+};
+
+TEMPLATE_TEST_CASE("sync progress notifications", "[sync][baas][progress]", PBS, FLX)
+{
+    TestType setup;
+    size_t expected_count = 0;
+
+#define VERIFY_REALM(realm_1, realm_2, expected)                                                                     \
+    {                                                                                                                \
+        REQUIRE(expected > 0);                                                                                       \
+        REQUIRE(realm_1);                                                                                            \
+        REQUIRE(realm_2);                                                                                            \
+        REQUIRE(realm_1 != realm_2);                                                                                 \
+        auto table1 = setup.get_table(realm_1);                                                                      \
+        auto table2 = setup.get_table(realm_2);                                                                      \
+        REQUIRE(table1);                                                                                             \
+        REQUIRE(table2);                                                                                             \
+        REQUIRE(table1->size() == expected);                                                                         \
+        REQUIRE(table2->size() == expected);                                                                         \
+    }
+
+    struct Progress {
+        uint64_t xferred, xferable;
+        double estimate;
+    };
+    typedef std::vector<std::vector<Progress>> ProgressValues;
+    ProgressValues progress;
+
+    std::mutex mutex;
+    auto add_callbacks = [&](SharedRealm& realm) {
+        std::scoped_lock lock(mutex);
+        size_t idx = progress.size();
+        progress.resize(idx + 4);
+        for (auto&& stream : {false, true})
+            for (auto&& direction : {NotifierType::download, NotifierType::upload})
+                realm->sync_session()->register_progress_notifier(
+                    [&, i = idx++](uint64_t xferred, uint64_t xferable, double estimate) {
+                        progress[i].emplace_back(Progress{xferred, xferable, estimate});
+                    },
+                    direction, stream);
+    };
+
+    auto dump = [](const ProgressValues& progress, size_t begin = 0, size_t end = -1) {
+        std::ostringstream out;
+        for (size_t i = begin, e = std::min(end, progress.size()); i < e; ++i) {
+            out << (i > begin ? "\n" : "") << progress[i].size() << ": ";
+            for (auto&& p : progress[i])
+                out << "(" << p.xferred << ", " << p.xferable << ", " << std::setprecision(2) << p.estimate << "), ";
+        }
+        return out.str();
+    };
+
+    auto clear = [&](ProgressValues& progress) {
+        std::scoped_lock lock(mutex);
+        for (auto&& values : progress)
+            values.clear();
+    };
+
+#define VERIFY_PROGRESS_EMPTY(progress, begin, end)                                                                  \
+    {                                                                                                                \
+        std::scoped_lock lock(mutex);                                                                                \
+        for (size_t i = begin; i < end; ++i) {                                                                       \
+            INFO(util::format("i = %1, %2", i, dump(progress, i, i + 1)));                                           \
+            auto&& values = progress[i];                                                                             \
+            CHECK(values.size() == 0);                                                                               \
+        }                                                                                                            \
+    }
+
+#define VERIFY_PROGRESS(progress, begin, end)                                                                        \
+    {                                                                                                                \
+        REQUIRE((begin < end && end <= progress.size()));                                                            \
+                                                                                                                     \
+        std::scoped_lock lock(mutex);                                                                                \
+        for (size_t i = begin; i < end; ++i) {                                                                       \
+            auto&& values = progress[i];                                                                             \
+                                                                                                                     \
+            INFO(i);                                                                                                 \
+            REQUIRE(values.size() > 0);                                                                              \
+                                                                                                                     \
+            for (size_t j = 0; j < values.size(); ++j) {                                                             \
+                INFO(util::format("i: %1, j: %2", i, j));                                                            \
+                auto&& p = values[j];                                                                                \
+                CHECK((0 <= p.xferred <= p.xferable));                                                               \
+                CHECK((0 <= p.estimate <= 1.0));                                                                     \
+                                                                                                                     \
+                if (j > 0) {                                                                                         \
+                    auto&& prev = values[j - 1];                                                                     \
+                    CHECK(prev.xferred <= p.xferred);                                                                \
+                    /* CHECK(prev.xferable <= p.xferable); can decrease by design */                                 \
+                    CHECK(prev.estimate <= p.estimate);                                                              \
+                }                                                                                                    \
+            }                                                                                                        \
+                                                                                                                     \
+            auto&& last = values.back();                                                                             \
+            CHECK(last.estimate == 1.0);                                                                             \
+            CHECK(last.xferred == last.xferable);                                                                    \
+        }                                                                                                            \
+    }
+
+    auto wait_for_sync = [](SharedRealm& realm) {
+        realm->sync_session()->resume();
+        wait_for_upload(*realm);
+        wait_for_download(*realm);
+        realm->sync_session()->pause();
+        realm->refresh();
+    };
+
+    auto config1 = setup.make_config();
+    auto realm_1 = Realm::get_shared_realm(config1);
+    realm_1->sync_session()->pause();
+
+    add_callbacks(realm_1);
+    expected_count = setup.add_objects(realm_1);
+    wait_for_sync(realm_1);
+
+    VERIFY_PROGRESS(progress, 0, 4);
+    clear(progress);
+
+    auto config2 = setup.make_config();
+    auto realm_2 = Realm::get_shared_realm(config2);
+    realm_2->sync_session()->pause();
+    add_callbacks(realm_2);
+    wait_for_sync(realm_2);
+
+    VERIFY_REALM(realm_1, realm_2, expected_count);
+
+    VERIFY_PROGRESS_EMPTY(progress, 0, 4);
+    VERIFY_PROGRESS(progress, 4, 8);
+    clear(progress);
+
+    expected_count = setup.add_objects(realm_2);
+    wait_for_sync(realm_2);
+    wait_for_sync(realm_1);
+    VERIFY_REALM(realm_1, realm_2, expected_count);
+
+    VERIFY_PROGRESS_EMPTY(progress, 0, 2);
+    VERIFY_PROGRESS(progress, 2, 4);
+    VERIFY_PROGRESS_EMPTY(progress, 4, 6);
+    VERIFY_PROGRESS(progress, 6, 8);
+    clear(progress);
+
+    realm_2.reset();
+    expected_count = setup.add_objects(realm_1);
+    wait_for_sync(realm_1);
+
+    realm_2 = Realm::get_shared_realm(config2);
+    realm_2->sync_session()->pause();
+    add_callbacks(realm_2);
+    wait_for_sync(realm_2);
+
+    VERIFY_REALM(realm_1, realm_2, expected_count);
+    VERIFY_PROGRESS_EMPTY(progress, 0, 2);
+    VERIFY_PROGRESS(progress, 2, 4);
+    VERIFY_PROGRESS_EMPTY(progress, 4, 8);
+    VERIFY_PROGRESS(progress, 8, 12);
+    clear(progress);
+
+    // check async open task
+    auto config3 = setup.make_config();
+    for (int i = 0; i < 2; ++i) {
+        auto task = Realm::get_synchronized_realm(config3);
+        REQUIRE(task);
+        progress.resize(progress.size() + 1);
+        task->register_download_progress_notifier([&](uint64_t xferred, uint64_t xferable, double estimate) {
+            progress.back().emplace_back(Progress{xferred, xferable, estimate});
+        });
+
+        std::atomic<bool> finished = false;
+        ThreadSafeReference ref;
+        std::exception_ptr err = nullptr;
+        task->start([&](ThreadSafeReference r, std::exception_ptr e) {
+            ref = std::move(r);
+            err = e;
+            finished = true;
+        });
+
+        util::EventLoop::main().run_until([&] {
+            return finished.load();
+        });
+
+        REQUIRE_FALSE(err);
+        REQUIRE(ref);
+        auto realm_3 = Realm::get_shared_realm(std::move(ref));
+        VERIFY_REALM(realm_1, realm_3, expected_count);
+        VERIFY_PROGRESS(progress, progress.size() - 1, progress.size());
+        VERIFY_PROGRESS_EMPTY(progress, 0, progress.size() - 1);
+        clear(progress);
+        realm_3.reset();
+
+        if (i == 0) {
+            expected_count = setup.add_objects(realm_1);
+            wait_for_sync(realm_1);
+            clear(progress);
+        }
+    }
+}
+#endif
