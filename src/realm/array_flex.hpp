23bd4bf26a4bfbf1203fb53a3f907fb2052e2696
Nicola Cabiddu <nicola.cabiddu@mongodb.com>
2024-03-07 15:43:07 +0000

    Revert "lint"
    This reverts commit 7ac0073058ffe10b7f197d26059d0ec0bfa7c451.

--- src/realm/array_flex.hpp
+++ src/realm/array_flex.hpp
@@ -50,8 +50,7 @@ private:
     bool find_all_match(size_t start, size_t end, size_t baseindex, QueryStateBase* state) const;
 
     template <typename Cond, bool = true> // true int64_t other uint64_t
-    inline size_t parallel_subword_find(const Array&, uint64_t, size_t, size_t, uint_least8_t, size_t, 
-                                        size_t) const;
+    inline size_t parallel_subword_find(const Array&, uint64_t, size_t, size_t, uint_least8_t, size_t, size_t) const;
 
     bool find_eq(const Array&, int64_t, size_t, size_t, size_t, QueryStateBase*) const;
     bool find_neq(const Array&, int64_t, size_t, size_t, size_t, QueryStateBase*) const;
