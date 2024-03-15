679fd77e2fcf493ff5843590d9063297408f2cd1
Finn Schiermer Andersen <finn.schiermer-andersen@mongodb.com>
2024-03-15 15:16:24 +0100

    Initial interface for string interner

---
+++ src/realm/string_interner.hpp
@@ -0,0 +1,41 @@
+/*************************************************************************
+ *
+ * Copyright 2016 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **************************************************************************/
+
+#ifndef REALM_STRING_INTERNER_HPP
+#define REALM_STRING_INTERNER_HPP
+
+#include <realm.hpp>
+
+namespace realm {
+
+
+using StringID = size_t;
+
+class StringInterner {
+public:
+    StringInterner();
+    ~StringInterner();
+    StringID intern(StringData);
+    std::optional<StringID> lookup(StringData);
+    int compare(StringID A, StringID B);
+    int compare(StringData, StringID A);
+    std::optional<StringData> get(StringID);
+};
+} // namespace realm
+
+#endif
