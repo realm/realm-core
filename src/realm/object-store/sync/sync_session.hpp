a2034d17331e337a8e4aa5dc000e0935baa2d873
Kirill Burtsev <kirill.burtsev@mongodb.com>
2024-03-22 20:32:33 +0100

    RCORE-2011: New parameter 'progress_estimate' for sync progress notification (#7124)
    * Add new field 'progress_estimate'
    * Add parsing support to protocol codec for repurposed progress estimate field
    * Refactor to forward parsed parameters from download message to session as is
    * Add object-store based test for progress notifications
    * Remove noop progress invocation in object-store api and simplify internals
    * Pass through progress estimate from client impl to notifier and call updates on download during bootstrap
    * Calculate client-side progress from the start of the upload/download

--- src/realm/object-store/sync/sync_session.hpp
+++ src/realm/object-store/sync/sync_session.hpp
@@ -52,14 +52,15 @@ struct SyncClient;
 class SyncProgressNotifier {
 public:
     enum class NotifierType { upload, download };
-    using ProgressNotifierCallback = void(uint64_t transferred_bytes, uint64_t transferrable_bytes);
+    using ProgressNotifierCallback = void(uint64_t transferred_bytes, uint64_t transferrable_bytes,
+                                          double progress_estimate);
 
     uint64_t register_callback(std::function<ProgressNotifierCallback>, NotifierType direction, bool is_streaming);
     void unregister_callback(uint64_t);
 
     void set_local_version(uint64_t);
-    void update(uint64_t downloaded, uint64_t downloadable, uint64_t uploaded, uint64_t uploadable, uint64_t,
-                uint64_t);
+    void update(uint64_t downloaded, uint64_t downloadable, uint64_t uploaded, uint64_t uploadable,
+                uint64_t snapshot_version, double download_estimate = 1.0, double upload_estimate = 1.0);
 
 private:
     mutable std::mutex m_mutex;
@@ -70,6 +71,8 @@ private:
         uint64_t downloadable;
         uint64_t uploaded;
         uint64_t downloaded;
+        double upload_estimate;
+        double download_estimate;
         uint64_t snapshot_version;
     };
 
@@ -415,7 +418,7 @@ private:
     void cancel_pending_waits(util::CheckedUniqueLock, Status) RELEASE(m_state_mutex);
     enum class ShouldBackup { yes, no };
     void update_error_and_mark_file_for_deletion(SyncError&, ShouldBackup) REQUIRES(m_state_mutex, !m_config_mutex);
-    void handle_progress_update(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
+    void handle_progress_update(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, double, double);
     void handle_new_flx_sync_query(int64_t version);
 
     void nonsync_transact_notify(VersionID::version_type) REQUIRES(!m_state_mutex);
