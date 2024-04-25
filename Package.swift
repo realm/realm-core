be35e4fef949ffb765463fb40e9983d135920409
Michael Wilkerson-Barker <michael.wilkersonbarker@mongodb.com>
2024-04-25 00:05:58 -0400

    Next release update versions

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 import Foundation
 
-let versionStr = "14.5.2"
+let versionStr = "14.5.3"
 let versionPieces = versionStr.split(separator: "-")
 let versionCompontents = versionPieces[0].split(separator: ".")
 let versionExtra = versionPieces.count > 1 ? versionPieces[1] : ""
