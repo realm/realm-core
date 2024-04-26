7355462ddcf596489e198da456a3f7a63ad0da94
Jonathan Reams <jbreams@mongodb.com>
2024-04-26 18:29:41 -0400

    fix building without encryption

--- test/object-store/sync/metadata.cpp
+++ test/object-store/sync/metadata.cpp
@@ -59,6 +59,7 @@ std::shared_ptr<Realm> get_metadata_realm()
 using realm::util::adoptCF;
 using realm::util::CFPtr;
 
+#if REALM_ENABLE_ENCRYPTION
 constexpr const char* access_group = "";
 bool can_access_keychain()
 {
@@ -74,6 +75,7 @@ bool can_access_keychain()
     }();
     return can_access_keychain;
 }
+#endif
 
 CFPtr<CFMutableDictionaryRef> build_search_dictionary(CFStringRef account, CFStringRef service)
 {
@@ -607,6 +609,7 @@ TEST_CASE("app metadata: persisted", "[sync][metadata]") {
     }
 }
 
+#if REALM_ENABLE_ENCRYPTION
 TEST_CASE("app metadata: encryption", "[sync][metadata]") {
     test_util::TestDirGuard test_dir(base_path);
 
@@ -731,6 +734,8 @@ TEST_CASE("app metadata: encryption", "[sync][metadata]") {
     }
 #endif // REALM_PLATFORM_APPLE
 }
+
+#endif
 
 #ifndef SWIFT_PACKAGE // The SPM build currently doesn't copy resource files
 TEST_CASE("sync metadata: can open old metadata realms", "[sync][metadata]") {
@@ -890,7 +895,7 @@ TEST_CASE("sync metadata: can open old metadata realms", "[sync][metadata]") {
 }
 #endif // SWIFT_PACKAGE
 
-#if REALM_PLATFORM_APPLE
+#if REALM_PLATFORM_APPLE && REALM_ENABLE_ENCRYPTION
 TEST_CASE("keychain", "[sync][metadata]") {
     if (!can_access_keychain()) {
         return;
