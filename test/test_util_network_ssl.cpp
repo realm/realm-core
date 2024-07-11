dc966f6183bcf6235cca6a0e08d612aff36454a9
Yavor Georgiev <yavor.georgiev@outlook.com>
2024-07-11 18:10:48 +0200

    lint

--- test/test_util_network_ssl.cpp
+++ test/test_util_network_ssl.cpp
@@ -1251,7 +1251,8 @@ TEST_IF(Util_Network_SSL_Certificate_From_Windows_Cert_Store, IsDebuggerPresent(
 
     ssl_context_1.use_certificate_chain_file(ca_dir + "dns-chain.crt.pem");
     ssl_context_1.use_private_key_file(ca_dir + "dns-checked-server.key.pem");
-    ssl_context_2.use_default_verify(); // this will import the Windows certificates in the context's certificate store
+    ssl_context_2
+        .use_default_verify(); // this will import the Windows certificates in the context's certificate store
 
     network::ssl::Stream ssl_stream_1{socket_1, ssl_context_1, network::ssl::Stream::server};
     network::ssl::Stream ssl_stream_2{socket_2, ssl_context_2, network::ssl::Stream::client};
