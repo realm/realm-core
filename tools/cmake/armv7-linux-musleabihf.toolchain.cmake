fe38bb1c08282bea76ebe98ac81d2b302f648e3f
Yavor Georgiev <fealebenpae@gmail.com>
2024-07-19 16:31:23 +0200

    Add toolchain files for cross-compiling to musl-based Linux

--- tools/cmake/armv7-linux-musleabihf.toolchain.cmake
+++ tools/cmake/armv7-linux-musleabihf.toolchain.cmake
@@ -1,7 +1,7 @@
-set(_TRIPLET "armv7-unknown-linux-gnueabihf")
-set(_TOOLCHAIN_MD5 fbf817b1428bb35c93be8e6c15f73d7d)
+set(_TRIPLET "armv7-unknown-linux-musleabihf")
+set(_TOOLCHAIN_MD5 e5df7301cf9138ef5cae6160878cab15)
 include("${CMAKE_CURRENT_LIST_DIR}/linux.toolchain.base.cmake")
-set(REALM_LINUX_LIBC gnu)
+set(REALM_LINUX_LIBC musl)
 
 # Explicitly opt-in to the slower bfd linker over gold, because gold in GCC 11.2 doesn't play nice with R_ARM_REL32 relocations
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=bfd ${CMAKE_EXE_LINKER_FLAGS_INIT}")
