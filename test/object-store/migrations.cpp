realm/realm-core

commit f7c8e97b76ab4de391a15b7e958d707f01a1852f

Fix issue when changing type of primary key
If you change the type of the primary key and call Realm::update_schema()
without a migration function, the new column will not be set as primary
key column.

src/realm/object-store/object_store.cpp
@@ -124,7 +124,8 @@ ColKey add_column(Group& group, Table& table, Property const& property)
     REALM_ASSERT(property.type != PropertyType::LinkingObjects);
 
     if (property.is_primary) {
-        // Primary key columns should have been created when the table was created
+        // Primary key columns should have been created when the table was created.
+        // Unless this is a migration
         if (auto col = table.get_column_key(property.name)) {
             return col;
         }
@@ -139,9 +140,11 @@ ColKey add_column(Group& group, Table& table, Property const& property)
     else {
         auto key =
             table.add_column(to_core_type(property.type), property.name, is_nullable(property.type), collection_type);
-        if (property.requires_index())
+        if (property.is_primary)
+            table.set_primary_key_column(key); // You can end here if this is a migration
+        else if (property.requires_index())
             table.add_search_index(key);
-        if (property.requires_fulltext_index())
+        else if (property.requires_fulltext_index())
             table.add_fulltext_index(key);
         return key;
     }

test/object-store/migrations.cpp
@@ -1137,6 +1137,31 @@ TEST_CASE("migration: Automatic", "[migration]") {
         });
     }
 
+    SECTION("change primary key from string to UUID without migration function") {
+        using namespace std::string_literals;
+        Schema schema{{"Foo",
+                       {
+                           {"_id", PropertyType::String, Property::IsPrimary{true}},
+                       }}};
+        Schema schema2{{"Foo",
+                        {
+                            {"_id", PropertyType::UUID, Property::IsPrimary{true}},
+                        }}};
+        InMemoryTestFile config;
+        config.schema_mode = SchemaMode::Automatic;
+        config.schema = schema;
+        auto realm = Realm::get_shared_realm(config);
+        realm->update_schema(schema2, 2);
+
+        CppContext ctx(realm);
+        std::any values = AnyDict{
+            {"_id", UUID("3b241101-0000-0000-0000-4136c566a964"s)},
+        };
+        realm->begin_transaction();
+        Object::create(ctx, realm, *realm->schema().find("Foo"), values);
+        realm->commit_transaction();
+    }
+
     SECTION("object accessors inside migrations") {
         using namespace std::string_literals;
 

commit 0a13bed1c80a052d74415deb3239317da9e4502a

Do not set primary key column before migration function is called
This is fixing a regression introduced by f7c8e97. The change done in
this commit is reverted. Instead the primary key column is set in the
post migration changes.

src/realm/object-store/object_store.cpp
@@ -140,9 +140,7 @@ ColKey add_column(Group& group, Table& table, Property const& property)
     else {
         auto key =
             table.add_column(to_core_type(property.type), property.name, is_nullable(property.type), collection_type);
-        if (property.is_primary)
-            table.set_primary_key_column(key); // You can end here if this is a migration
-        else if (property.requires_index())
+        if (property.requires_index())
             table.add_search_index(key);
         else if (property.requires_fulltext_index())
             table.add_fulltext_index(key);
@@ -833,7 +831,12 @@ static void apply_post_migration_changes(Group& group, std::vector<SchemaChange>
                                             handle_backlinks_automatically);
         }
         void operator()(RemoveTable) {}
-        void operator()(ChangePropertyType) {}
+        void operator()(ChangePropertyType op)
+        {
+            if (op.new_property->is_primary) {
+                set_primary_key(table(op.object), op.new_property);
+            }
+        }
         void operator()(MakePropertyNullable) {}
         void operator()(MakePropertyRequired) {}
         void operator()(AddProperty) {}

test/object-store/migrations.cpp
@@ -1137,6 +1137,55 @@ TEST_CASE("migration: Automatic", "[migration]") {
         });
     }
 
+    SECTION("change primary key from int to string") {
+        using namespace std::string_literals;
+        Schema schema{{"Foo",
+                       {
+                           {"_id", PropertyType::Int, Property::IsPrimary{true}},
+                           {"value", PropertyType::String},
+                       }}};
+        Schema schema2{{"Foo",
+                        {
+                            {"_id", PropertyType::String, Property::IsPrimary{true}},
+                            {"value", PropertyType::String},
+                        }}};
+        TestFile config;
+        config.schema_mode = SchemaMode::Automatic;
+        config.schema = schema;
+        config.schema_version = 1;
+        {
+            auto realm = Realm::get_shared_realm(config);
+
+            CppContext ctx(realm);
+            std::any value1 = AnyDict{
+                {"_id", INT64_C(1)},
+                {"value", "foo"s},
+            };
+            std::any value2 = AnyDict{
+                {"_id", INT64_C(2)},
+                {"value", "bar"s},
+            };
+            realm->begin_transaction();
+            auto s = realm->schema().find("Foo");
+            Object::create(ctx, realm, *s, value1);
+            Object::create(ctx, realm, *s, value2);
+            realm->commit_transaction();
+        }
+        config.schema = schema2;
+        config.schema_version = 2;
+        config.migration_function = [](SharedRealm old_realm, SharedRealm realm, Schema&) {
+            Results r{Class(old_realm, &*old_realm->schema().find("Foo"))};
+            auto sz = r.size();
+            auto t = ObjectStore::table_for_object_type(realm->read_group(), "Foo");
+            for (size_t i = 0; i < sz; i++) {
+                Obj o = r.get(i);
+                auto new_obj = t->get_object(o.get_key());
+                new_obj.set("_id", util::to_string(o.get<Int>("_id")));
+            }
+        };
+        auto realm = Realm::get_shared_realm(config);
+    }
+
     SECTION("change primary key from string to UUID without migration function") {
         using namespace std::string_literals;
         Schema schema{{"Foo",
@@ -1153,6 +1202,7 @@ TEST_CASE("migration: Automatic", "[migration]") {
         auto realm = Realm::get_shared_realm(config);
         realm->update_schema(schema2, 2);
 
+        // Make sure you can actually create an object with UUID as primary key
         CppContext ctx(realm);
         std::any values = AnyDict{
             {"_id", UUID("3b241101-0000-0000-0000-4136c566a964"s)},