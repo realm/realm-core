cb5f45ab91a26e79a95f2f61e84fa14a039027e5
jedelbo <572755+jedelbo@users.noreply.github.com>
2025-01-28 09:42:13 +0000

    Prepare for release 14.13.5

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 import Foundation
 
-let versionStr = "14.13.4"
+let versionStr = "14.13.5"
 let versionPieces = versionStr.split(separator: "-")
 let versionCompontents = versionPieces[0].split(separator: ".")
 let versionExtra = versionPieces.count > 1 ? versionPieces[1] : ""
